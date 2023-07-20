e0d0100acfb0984c8c933517f3683a9a05361fdd
Conway <yawnoc@pm.me>
2023-07-21 00:50:48 +0800

    Add 丫 entry data

--- entries/aa.cmd
+++ entries/aa.cmd
@@ -25,6 +25,27 @@ _ngat_ [[\C ngat; \J ngat]].
 
 ### 丫
 
+$$
+; Reading
+  , [[\C aa1; \J aa1]]
+; Reading variations
+  , [[none]]
+; Williams entry
+  ,{w}
+    A colloquial word.
+    =={w}
+    - An interrogative particle ;
+    - also one indicating that the affirmation is indisputable ;
+    - a mere final answering tone, having no meaning ;
+    - _(2)h(o\) (1)(a/)_ well, very well ;
+    - _(1)(a/) wai(3)_ poor, necessitous ;
+    - _(5)k(')(u") (2)k(o\)m w(a/)(6) (1)(a/)_, he said so ;
+    - _mat(7) (5)y(e/) (1)(a/)_, what ?
+    - _(2)k(o\)m ts(o\)(6) (a/)_, no, this is the way to do it.
+    ==
+; Conway's commentary
+$$
+
 ### 吖
 
 ### 呀1
