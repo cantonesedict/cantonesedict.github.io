8617bc94ad64168c3e741f0d324deb7a6d73d804
Conway <yawnoc@pm.me>
2023-07-20 22:56:58 +0800

    Create /entries/aa

---
+++ entries/aa.cmd
@@ -0,0 +1,19 @@
+%%%
+
+# (A/) [[\C aa; \J aa]]
+
+--
+<small>\
+Words in _a_ [[\C a; \J a]] or _(a/)_ [[\C aa; \J aa]],
+are often heard beginning with _ng_ [[\C ng; \J ng]],
+as _ng(a/)_ [[\C ngaa; \J ngaa]],
+_ngai_ [[\C ngai; \J ngai]],
+_ngat_ [[\C ngat; \J ngat]].
+</small>
+--
+
+##{#1} [[\C aa1; \J aa1]]
+
+##{#2} [[\C aa2; \J aa2]]
+
+##{#3} [[\C aa3; \J aa3]]
