71e94f8356e822f928d5e1b2a2d1cc5def56d4fa
Conway <yawnoc@pm.me>
2023-07-22 00:45:42 +0800

    Add 哎 ai1 entry

--- entries/ai.cmd
+++ entries/ai.cmd
@@ -12,3 +12,38 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 #{.williams} Ai [[ai]]
+
+
+##{#1 .williams} (1)Ai [[陰平; ai1]]
+
+###+ 哎1 [[U+54CE]] _(1)ai_ [[ai1]]
+
+$$
+A
+  , 𫫃 = ⿰口挨; [[唉]]
+V
+  , [[aai1: longer syllable for particle sense]]
+W
+  ,
+    [[Page~2: entry for 𫫃 = ⿰口挨]]
+    ==
+    - A particle of surprise or pain;
+    - to beg, to ask;
+    - _(1)ai (1)y(a/)_,
+        [[哎吔; ai1 yaa1]]
+        whew! halloo! oh, dear!
+    - _(1)ai, mat(7) (5)n(i/) (2)k(o\)m ts(o\)(6)_
+        [[哎、乜你噉做; ai1, mat1 nei5 gam2 zou6]]
+        oh! why did you do so?
+    - _(1)ai (4)k(')au (5)n(i/)_
+        [[哎求你; ai1 kau4 nei5]],
+        I intreat you.
+    ==
+C
+  ,
+    =={.modern}
+    - Williams has U+2BAC3 𫫃 = ⿰口挨, which has poor font support.
+      I have grafted its entry to 哎, which does not appear in Williams.
+    - [Jyutping suggests using 哎 as the canonical character for ai1 or aai1.][jpp]
+    ==
+$$
