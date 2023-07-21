10ed9b4111b55a757238562e9395f2c0c23c99e2
Conway <yawnoc@pm.me>
2023-07-22 01:24:35 +0800

    Rename /entries/ title to include 'Index'

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
-* %title --> Entries by Jyutping slug
+* %title --> Index of entries by Jyutping slug
 * %date-modified --> 2023-07-22
 * %copyright-prior-years -->
 
