c7268b4d762423cc274c25250a8b2684e12b1e48
Conway <yawnoc@pm.me>
2023-07-21 13:32:45 +0800

    Make lang=zh-Hant automatic

--- entries/aa.cmd
+++ entries/aa.cmd
@@ -22,7 +22,7 @@ _ngat_ [[\J ngat; \C ngat]].
 --
 
 
-##{#1} (1)(A/) [[.陰平; \J aa1; \C aa1]]
+##{#1} (1)(A/) [[陰平; \J aa1; \C aa1]]
 
 ### 丫 (U+4E2B)
 
@@ -44,25 +44,25 @@ W
     - also one indicating that the affirmation is indisputable;
     - a mere final answering tone, having no meaning;
     - _(2)h(o\) (1)(a/)_
-        [[.好吖; \J hou2 aa1; \C hou2 aa1]]
+        [[好吖; \J hou2 aa1; \C hou2 aa1]]
         well, very well;
     - _(1)(a/) wai(3)_
         [[?; \J aa1 wai3; \C aa1 wai3]]
         poor, necessitous;
     - _(5)k(')(u") (2)k(o\)m w(a/)(6) (1)(a/)_
-        [[.佢噉話吖; \J keoi5 gam2 waa6 aa1; \C k'_(u") kam2 waa6 aa1]],
+        [[佢噉話吖; \J keoi5 gam2 waa6 aa1; \C k'_(u") kam2 waa6 aa1]],
         he said so;
     - _mat(7) (5)y(e/) (1)(a/)_
-        [[.乜嘢吖; \J mat1 je5 aa1; \C mat7 ye5 aa1]],
+        [[乜嘢吖; \J mat1 je5 aa1; \C mat7 ye5 aa1]],
         what?
     - _(2)k(o\)m ts(o\)(6) (a/)_
-        [[.噉做吖; \J gam2 zou6 aa1; \C kam2 tsou6 aa1]],
+        [[噉做吖; \J gam2 zou6 aa1; \C kam2 tsou6 aa1]],
         no, this is the way to do it.
     ==
 C
   ,{m}
     ==
-    - [Jyutping suggests .吖 as the canonical character for aa1.][jpp]
+    - [Jyutping suggests 吖 as the canonical character for aa1.][jpp]
     ==
 $$
 
@@ -71,14 +71,14 @@ $$
 ### 鴉 (U+9D09)
 
 
-##{#2} (2)(A/) [[.陰上; \J aa2; \C aa2]]
+##{#2} (2)(A/) [[陰上; \J aa2; \C aa2]]
 
 ### 啞 (U+555E)
 
 ### 瘂 (U+7602)
 
 
-##{#3} (A/)(3) [[.陰去; \J aa3; \C aa3]]
+##{#3} (A/)(3) [[陰去; \J aa3; \C aa3]]
 
 ### 亞 (U+4E9E)
 
