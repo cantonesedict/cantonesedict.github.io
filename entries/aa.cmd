6729181f8aed1ab6225562382f471eb0d4b411ad
Conway <yawnoc@pm.me>
2023-07-21 18:42:18 +0800

    Remove J prefix

--- entries/aa.cmd
+++ entries/aa.cmd
@@ -11,20 +11,20 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 %%%
 
-#{.williams} (A/) [[\J aa]]
+#{.williams} (A/) [[aa]]
 
 --{.williams}
-Words in _a_ [[\J a]] or _(a/)_ [[\J aa]],
-are often heard beginning with _ng_ [[\J ng]],
-as _ng(a/)_ [[\J ngaa]],
-_ngai_ [[\J ngai]],
-_ngat_ [[\J ngat]].
+Words in _a_ [[a]] or _(a/)_ [[aa]],
+are often heard beginning with _ng_ [[ng]],
+as _ng(a/)_ [[ngaa]],
+_ngai_ [[ngai]],
+_ngat_ [[ngat]].
 --
 
 
-##{#1 .williams} (1)(A/) [[陰平; \J aa1]]
+##{#1 .williams} (1)(A/) [[陰平; aa1]]
 
-### 丫1 [[U+4E2B]] _(1)(a/)_ [[\J aa1]]
+### 丫1 [[U+4E2B]] _(1)(a/)_ [[aa1]]
 
 $$
 W
@@ -33,22 +33,22 @@ W
     - A fork, a crotch;
     - the parting of two branches, fingers, or tines;
     - _(1)(a/) (4)t(')au_
-      [[丫頭; \J aa1 tau4]]
+      [[丫頭; aa1 tau4]]
       or
       _(1)(a/) (4)w(a/)n_
-      [[丫鬟; \J aa1 waan4]],
+      [[丫鬟; aa1 waan4]],
       a slave girl;
     - _(1)(a/) kok(8) kai(3)_
-      [[丫角髻; \J aa1 gok3 gai3]]
+      [[丫角髻; aa1 gok3 gai3]]
       a little boy;
     - _(1)(a/) (1)ch(')(a/)_
-      [[丫杈; \J aa1 caa1]],
+      [[丫杈; aa1 caa1]],
       a fork, a rest for clothes' sticks;
     - _sh(u")(6) (1)(a/)_
-      [[樹丫; \J syu6 aa1]]
+      [[樹丫; syu6 aa1]]
       fork of a tree;
     - _(1)s(a/)m (1)(a/) l(o\)(6)_
-      [[三丫路; \J saam1 aa1 lou6]]
+      [[三丫路; saam1 aa1 lou6]]
       meeting of three ways, a trivium.
     ==
 C
@@ -58,13 +58,13 @@ C
     ==
 $$
 
-### 吖1 [[U+5416]] _(1)(a/)_ [[\J aa1]]
+### 吖1 [[U+5416]] _(1)(a/)_ [[aa1]]
 
 $$
 A
   , [[$呀1; $啊1]]
 V
-  , [[\J a1: shorter syllable]]
+  , [[a1: shorter syllable]]
 W
   ,
     [[(Page~1)]] <br>
@@ -74,19 +74,19 @@ W
     - also one indicating that the affirmation is indisputable;
     - a mere final answering tone, having no meaning;
     - _(2)h(o\) (1)(a/)_
-        [[好吖; \J hou2 aa1]]
+        [[好吖; hou2 aa1]]
         well, very well;
     - _(1)(a/) wai(3)_
-        [[?; \J aa1 wai3]]
+        [[?; aa1 wai3]]
         poor, necessitous;
     - _(5)k(')(u") (2)k(o\)m w(a/)(6) (1)(a/)_
-        [[佢噉話吖; \J keoi5 gam2 waa6 aa1]],
+        [[佢噉話吖; keoi5 gam2 waa6 aa1]],
         he said so;
     - _mat(7) (5)y(e/) (1)(a/)_
-        [[乜嘢吖; \J mat1 je5 aa1]],
+        [[乜嘢吖; mat1 je5 aa1]],
         what?
     - _(2)k(o\)m ts(o\)(6) (a/)_
-        [[噉做吖; \J gam2 zou6 aa1]],
+        [[噉做吖; gam2 zou6 aa1]],
         no, this is the way to do it.
     ==
 C
@@ -96,13 +96,13 @@ C
     ==
 $$
 
-### 呀1 [[U+5440]] _(1)(a/)_ [[\J aa1]]
+### 呀1 [[U+5440]] _(1)(a/)_ [[aa1]]
 
 $$
 A
   , [[$吖1; $啊1]]
 V
-  , [[\J a1: shorter syllable]]
+  , [[a1: shorter syllable]]
 W
   ,
     [[(Page~1)]] <br>
@@ -111,12 +111,12 @@ W
     - An interjection used in answers, denoting surprise or alarm;
     - alas! dreadful.
     ==
-    Read _(4)ng(a/)_ [[\J ngaa4]]: [[see [`ngaa#呀4`]]]. <## TODO ##>
+    Read _(4)ng(a/)_ [[ngaa4]]: [[see [`ngaa#呀4`]]]. <## TODO ##>
 C
   ,
     =={.modern}
     - Williams does not provide a separate entry
-      under _(4)ng(a/)_ [[\J ngaa4]].
+      under _(4)ng(a/)_ [[ngaa4]].
     ==
 S
   ,
@@ -128,13 +128,13 @@ S
     ==
 $$
 
-###+ 啊1 [[U+554A]] _(1)(a/)_ [[\J aa1]]
+###+ 啊1 [[U+554A]] _(1)(a/)_ [[aa1]]
 
 $$
 A
   , [[$吖1; $呀1]]
 V
-  , [[\J a1: shorter syllable]]
+  , [[a1: shorter syllable]]
 W
   ,
     [[Not present]]
@@ -150,7 +150,7 @@ S
     ==
 $$
 
-### 鴉1 [[U+9D09]] _(1)(a/)_ [[\J aa1]]
+### 鴉1 [[U+9D09]] _(1)(a/)_ [[aa1]]
 
 $$
 W
@@ -158,13 +158,13 @@ W
     ==
     - A raven with a white streak on its breast;
     - _(5)l(o\) (1)(a/)_
-      [[老鴉; \J lou5 aa1]],
+      [[老鴉; lou5 aa1]],
       a raven or crow;
     - _(4)t(')(o\) (1)(a/)_
-      [[塗鴉; \J tou4 aa1]],
+      [[塗鴉; tou4 aa1]],
       written roughly;
     - _(1)(a/) p(')(i/)n(3) (4)nai_
-      [[鴉片坭; \J aa1 pin3 nai4]],
+      [[鴉片坭; aa1 pin3 nai4]],
       opium.
     ==
 C
@@ -176,9 +176,9 @@ C
 $$
 
 
-##{#2 .williams} (2)(A/) [[陰上; \J aa2]]
+##{#2 .williams} (2)(A/) [[陰上; aa2]]
 
-### 啞2 [[U+555E]] _(2)(a/)_ [[\J aa2]]
+### 啞2 [[U+555E]] _(2)(a/)_ [[aa2]]
 
 $$
 A
@@ -190,18 +190,18 @@ W
     - dumb;
     - to keep silent, for which the next [[$瘂2]] is used;
     - _(2)(a/) mai(6)_
-      [[啞謎; \J aa2 mai6]]
+      [[啞謎; aa2 mai6]]
       an enigma;
     - _(4)ts(')ing (4)n(a/)n (2)(a/) (2)yan_
-      [[情難啞忍; \J cing4 naan4 aa2 yan2]],
+      [[情難啞忍; cing4 naan4 aa2 yan2]],
       hard to bear it patiently.
     ==
-    Also read _ak(7)_ [[\J ak1]]: [[see [`ak#啞1`]]]. <## TODO ##>
+    Also read _ak(7)_ [[ak1]]: [[see [`ak#啞1`]]]. <## TODO ##>
 C
   ,
     =={.modern}
     - Williams does not provide a separate entry
-      under _ak(7)_ [[\J ak1]].
+      under _ak(7)_ [[ak1]].
     ==
 S
   ,
@@ -210,7 +210,7 @@ S
     ==
 $$
 
-### 瘂2 [[U+7602]] _(2)(a/)_ [[\J aa2]]
+### 瘂2 [[U+7602]] _(2)(a/)_ [[aa2]]
 
 $$
 A
@@ -224,26 +224,26 @@ W
     - wheezing;
     - nape of the neck;
     - _(2)(a/) (2)tsai_
-      [[瘂仔; \J aa2 zai2]],
+      [[瘂仔; aa2 zai2]],
       a dumb boy;
     - _(2)(a/) (2)hau (4)m(o\) (4)(i/)n_
-      [[瘂口無言; \J aa2 hau2 mou4 jin4]],
+      [[瘂口無言; aa2 hau2 mou4 jin4]],
       will not speak, sulky;
     - _(2)(a/) tuk(9)_
-      [[瘂毒; \J aa2 duk6]]
+      [[瘂毒; aa2 duk6]]
       dumb people are revengeful;
     - _(1)sheng t(a/)i(3) (2)(a/)_
-      [[聲帶瘂; \J seng1 daai3 aa2]],
+      [[聲帶瘂; seng1 daai3 aa2]],
       the sound is indistinct.
     ==
 $$
 
 
-##{#3 .williams} (A/)(3) [[陰去; \J aa3]]
+##{#3 .williams} (A/)(3) [[陰去; aa3]]
 
-### 亞3 [[U+4E9E]] _(a/)(3)_ [[\J aa3]]
+### 亞3 [[U+4E9E]] _(a/)(3)_ [[aa3]]
 
-### 呀3 [[U+5440]] _(a/)(3)_ [[\J aa3]]
+### 呀3 [[U+5440]] _(a/)(3)_ [[aa3]]
 
 $$
 S
@@ -256,7 +256,7 @@ S
     ==
 $$
 
-###+ 啊3 [[U+554A]] _(a/)(3)_ [[\J aa3]]
+###+ 啊3 [[U+554A]] _(a/)(3)_ [[aa3]]
 
 $$
 S
@@ -266,14 +266,14 @@ S
     ==
 $$
 
-### 婭3 [[U+5A6D]] _(a/)(3)_ [[\J aa3]]
+### 婭3 [[U+5A6D]] _(a/)(3)_ [[aa3]]
 
-### 阿3 [[U+963F]] _(a/)(3)_ [[\J aa3]]
+### 阿3 [[U+963F]] _(a/)(3)_ [[aa3]]
 
 
-##{#4 .modern} (4)(A/) [[陽平; \J aa4]]
+##{#4 .modern} (4)(A/) [[陽平; aa4]]
 
-###+ 呀4 [[U+5440]] _(4)(a/)_ [[\J aa4]]
+###+ 呀4 [[U+5440]] _(4)(a/)_ [[aa4]]
 
 $$
 S
@@ -287,9 +287,9 @@ S
 $$
 
 
-##{#6 .modern} (A/)(6) [[陽去; \J aa6]]
+##{#6 .modern} (A/)(6) [[陽去; aa6]]
 
-###+ 呀6 [[U+5440]] _(a/)(6)_ [[\J aa6]]
+###+ 呀6 [[U+5440]] _(a/)(6)_ [[aa6]]
 
 $$
 S
