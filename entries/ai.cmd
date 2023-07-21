62efd5de34ca4494e7b34e04ff8d15f56b43ee83
Conway <yawnoc@pm.me>
2023-07-22 01:13:58 +0800

    Clarify corrections with square brackets

--- entries/ai.cmd
+++ entries/ai.cmd
@@ -35,7 +35,7 @@ W
     - _(1)ai, mat(7) (5)n(i/) (2)k(o\)m ts(o\)(6)_
         [[哎、乜你噉做; ai1, mat1 nei5 gam2 zou6]]
         oh! why did you do so?
-    - _(1)ai (4)k(')au (5)n(i/)_
+    - _(1)ai [(4)k(')au] (5)n(i/)_
         [[哎求你; ai1 kau4 nei5]],
         I intreat you.
     ==
@@ -44,6 +44,8 @@ C
     =={.modern}
     - Williams has U+2BAC3 𫫃 = ⿰口挨, which has poor font support.
       I have grafted its entry to 哎, which does not appear in Williams.
+    - Corrected _(1)k(')au_ [[kau1]] to _(4)k(')au_ [[kau4]]
+      in the "I intreat you" example.
     - [Jyutping suggests using 哎 as the canonical character for ai1 or aai1.][jpp]
     ==
 $$
@@ -63,7 +65,7 @@ W
     - _(2)ai (5)l(o\) (2)tsai_
         [[矮老仔; ai2 lou5 zai2]],
         a pigmy, a little old man;
-    - _(2)p(i/) (1)ch(e/)ung (2)ai tang(3) (2)tsai (5)n(i/) (5)ts(')o_
+    - _(2)p(i/) (1)ch(e/)ung (2)ai tang(3) (2)tsai (5)n(i/) [(5)ts(')o]_
         [[畀張矮凳仔你坐; bei2 zoeng1 ai2 dang3 zai2 nei5 co5]]
         get a low stool for you to sit on---to delude one;
     - _(2)ai tak(7) tsai(6)_
