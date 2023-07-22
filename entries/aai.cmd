b03b5248678fd68568c7f15dfa6f99167d09deb2
Conway <yawnoc@pm.me>
2023-07-22 16:55:29 +0800

    Add 挨 (U+6328) ꜀ái (aai1)

--- entries/aai.cmd
+++ entries/aai.cmd
@@ -12,3 +12,54 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 #{.williams} (A/)i [[aai]]
+
+
+##{#1 .williams} (1)(A/)i [[陰平; aai1]]
+
+### 挨1 [[U+6328]] _(1)(a/)i_ [[aai1]]
+
+$$
+W
+  ,
+    [[Page~2]] <br>
+    ==
+    - To lean upon, to trust to;
+    - to push away, to carry on the back;
+    - to place alongside;
+    - to strike;
+    - to force, to crowd, as with the elbows;
+    - to graft;
+    - next, near;
+    - passable;
+    - to lie down, to recline;
+    - _(1)(a/)i (4)m(a/)i (1)n(i/) p(i/)n(6)_
+        [[挨埋呢便; aai1 maai4 ni1 bin6]]
+        lean it against here;
+        help here a little;
+    - _(5)yau tak(7) (1)(a/)i pang(6)_
+        [[有得挨憑; jau4 dak1 aai1 bang6]]
+        has some dependance or resource;
+    - _(5)yau t(a/)i(6) (1)sh(a/)n (1)(a/)i_
+        [[有大山挨; jau5 daai6 saan1 aai1]],
+        he has powerful friends;
+    - _(1)(a/)i (5)h(a/) tik(7)_
+        [[挨吓滴; aai1 haa5 dik1]]
+        lie down a little;
+    - _(1)(a/)i tak(7) h(u")(3)_
+        [[挨得去; aai1 dak1 heoi3]]
+        it will answer;
+    - _(1)(a/)i (4)m(u/)n (1)(a/)i (u/)(6)_
+        [[挨門挨戶; aai1 mun4 aai1 wu6]]
+        to gad and stare, as a virago;
+        to go from door to door to examine;
+    - _(1)(a/)i (2)(i/)_
+        [[挨倚; aai1 ji2]],
+        to mutually rely upon;
+    - _(1)(a/)i l(a/)i(6)_
+        [[挨賴; aai1 laai6]]
+        to trust for help;
+    - _(1)(a/)i (5)m(a/)n_
+        [[挨晚; aai1 maan5-1]],
+        towards evening.
+    ==
+$$
