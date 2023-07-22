ef8793f99ef2d00074edf6da02835031e5866526
Conway <yawnoc@pm.me>
2023-07-22 13:26:38 +0800

    Add modernisation section

--- index.cmd
+++ index.cmd
@@ -14,13 +14,46 @@ OrdinaryDictionaryReplacement: #.properties-override
 #{.modern} Cantonese Dictionary
 
 --{.modern}
-A modern edition of
-[''A Tonic Dictionary of the Chinese Language in the Canton Dialect''][tonic] (1856),
-or 《英華分韻撮要》,
+A modern edition of 《英華分韻撮要》
+[''A Tonic Dictionary of the Chinese Language in the Canton Dialect''][tonic] (1856)
 by [Samuel Wells Williams].
 --
 
 [Samuel Wells Williams]: https://en.wikipedia.org/wiki/Samuel_Wells_Williams
+
+
+##{#modern .modern} Modernisation
+
+--{.modern}
+The version of ''Tonic Dictionary'' used as a base reference is
+[ark:/13960/t0ps1ph66 at archive.org][tonic].
+--
+--{.modern}
+Modernisation is a solo effort by Conway, consisting of:
+--
+++{.modern}
+1. Transcribing its entries <span class="williams">(black serif text)</span>;
+
+1. Collating the entries by Jyutping
+   (noting that there are phonological differences between
+   the Ts'ing Dynasty Cantonese recorded by Williams
+   and modern Hong Kong Cantonese);
+
+1. Inserting characters and Jyutping (purple sans-serif text)
+   in the romanisation-only examples, where practicable;
+
+1. Inserting remarks (purple sans-serif text)
+   and cross-reference links (blue);
+
+1. Adding entries for some characters that were not recorded by Williams.
+++
+--{.modern}
+No effort has been made to alter or censor the Williams text
+for the sensitivities of 21st century readers.
+--
+
+
+##{#contents .modern} Contents
 
 --{.modern}
 Work in progress.
