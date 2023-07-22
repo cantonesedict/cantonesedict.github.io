bdc5f3d8eee7fcccfe027413abb12d1b3177b40d
Conway <yawnoc@pm.me>
2023-07-22 23:16:27 +0800

    Add /intro Section 1 Paragraph 1

--- intro.cmd
+++ intro.cmd
@@ -12,3 +12,44 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 #{.williams} %title
+
+--{.modern}
+[[From [Page~ix of the original][ix] onwards.]]
+--
+
+[ix]: https://archive.org/details/tonicdictionaryo00will/page/n14/mode/1up
+
+
+##{#canton} 1. The Canton Dialect
+
+--{.williams}
+The dialect of the Chinese language spoken most accurately in
+the city of Canton [[廣州]], and hence usually called the Canton Dialect, is
+understood throughout the districts in the western half of the province
+of Kw(a/)ngtung [[廣東]], with only unimportant exceptions. Though the
+variations from the speech of the citizens of Canton itself and from
+each other, heard in these districts are numerous, still the general
+characteristics of the dialect in its idiom and pronunciation, are
+preserved, and the people find little difficulty in talking with each
+other. The people from the district of Sinhwui 新會 lying south-
+westerly from Canton, exhibit the most remarkable peculiarities
+in pronunciation, and it is a puzzle to the scholars in the city
+how they should have originated. The Canton dialect differs so
+much from that spoken at the extreme east of the province, in
+the department of Ch(a/)uchau 潮州, and in most parts of the adjoining
+prefectures of Ki(a/)ying~chau [[嘉應州]] and Hwaichau~f(u/) [[惠州府]], that their
+inhabitants are not intelligible to citizens of the capital. This
+dialect, usually called the Ti(e/)chi(u/) from the local sound of 潮州,
+and the Canton, constitute the speech of probably six sevenths of
+the nineteen millions reckoned as the population of the whole province;
+still this estimate is based on no very reliable data, owing to
+the difficulty of actual examination. The patois known as the _k(a/)k-k(a/)_ [[客家]]
+approaches very near to the Canton, and people from Kw(a/)ngs(i/) [[廣西]]
+are easily understood in this city. Both the Canton and Ti(e/)chi(u/)
+dialects differ so materially in idiom and pronunciation from the
+general language of China, called the _kw(a/)n hw(a/)_ 官話 court or
+mandarin dialect, that those speaking only the latter do not understand
+them; and when they come to Canton as officers, are always
+obliged to employ interpreters to explain to them the depositions
+made in their courts by the common people.
+--
