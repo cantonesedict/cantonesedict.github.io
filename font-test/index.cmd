116fe76d14bef967268f9ca8f8fe4db872f79931
Conway <yawnoc@pm.me>
2023-07-28 21:58:10 +0800

    Add ideographic tone marks table

--- font-test/index.cmd
+++ font-test/index.cmd
@@ -16,3 +16,43 @@ OrdinaryDictionaryReplacement: #.no-black-serif-note
 %%%
 
 #{.modern} %title
+
+##{.modern} Ideographic tone marks: four tones
+
+--{.modern}
+These tone marks are combining characters,
+and should appear as circles on one of the four corners of a character
+(representing the four tones of Middle Chinese).
+--
+
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Code point
+    ; Description
+    ; Reference image
+    ; Your browser
+|:
+  //
+    , U+302A
+    , Classical marker for level tone (平聲)
+    ,{.font-test}
+    ,{.font-test} 平〪
+  //
+    , U+302B
+    , Classical marker for rising tone (上聲)
+    ,{.font-test}
+    ,{.font-test} 上〫
+  //
+    , U+302C
+    , Classical marker for departing tone (去聲)
+    ,{.font-test}
+    ,{.font-test} 去〬
+  //
+    , U+302D
+    , Classical marker for entering tone (入聲)
+    ,{.font-test}
+    ,{.font-test} 入〭
+''
+||||
