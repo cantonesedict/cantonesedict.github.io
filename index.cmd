740de04d81e7d1841d0af37ef178bce88f570df3
Conway <yawnoc@pm.me>
2023-07-29 13:31:10 +0800

    Move /intro to /intro/

--- index.cmd
+++ index.cmd
@@ -82,7 +82,7 @@ for the sensitivities of 21st-century readers.**
   -{.williams} [\[English Title Page\]](/title/english)
   -{.williams} [Preface.](/preface)
   -{.williams} [\[Chinese Title Page\]](/title/chinese)
-  -{.williams} [Introduction.](/intro)
+  -{.williams} [Introduction.](/intro/)
   ==
 
 - Front matter (modern)
