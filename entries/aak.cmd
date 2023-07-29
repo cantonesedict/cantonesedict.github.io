b8d6644992e8e89f1753547a4d1056039764d83c
Conway <yawnoc@pm.me>
2023-07-30 00:10:55 +0800

    Add 握 (U+63E1) ak꜆ (ak1) etc.

--- entries/aak.cmd
+++ entries/aak.cmd
@@ -12,3 +12,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 #{.williams} (A/)k [[aak]]
+
+
+##{#1 .williams} (A/)k(7) [[aak1 高陰入]]
+
+###+ 握1 [[U+63E1]] _(a/)k(7)_ [[aak1]]
+
+$$
+V
+  , [[ak1: literary]]
+W
+  , [[Not present]]
+C
+  ,
+    =={.modern}
+    - Most modern speakers prefer this longer syllable
+      over the shorter _ak(7)_ [[ak1]].
+      Nevertheless, see [`ak#握1`] for the canonical entry.
+    ==
+S
+  ,
+    =={.modern}
+    - [`ak#握1`]
+    ==
+$$
--- entries/ak.cmd
+++ entries/ak.cmd
@@ -39,3 +39,32 @@ S
     - [`aa#啞2`]
     ==
 $$
+
+### 握1 [[U+63E1]] _ak(7)_ [[ak1]]
+
+$$
+V
+  , [[aak1: longer syllable]]
+W
+  ,
+    [[Page~3]] <br>
+    ==
+    - To grasp, to hold within the hand;
+    - a little, as much as the palm can hold;
+    - _ak(7) teng(6)_ [[ak1 deng6 握定]] to grasp firmly;
+    - _ak(7) (2)shau_ [[ak1 sau2 握手]], to shake hands;
+    - _ak(7) (4)ch(')(i/)_ [[ak1 ci4 握持]], to hold in the hand;
+    - _ak(7) i(u/)(3)_ [[ak1 yiu3 握要]] necessary.
+    ==
+C
+  ,
+    =={.modern}
+    - Most modern speakers prefer the longer syllable _(a/)k(7)_ [[aak1]];
+      see [`aak#握1`].
+    ==
+S
+  ,
+    =={.modern}
+    - [`aak#握1`]
+    ==
+$$
