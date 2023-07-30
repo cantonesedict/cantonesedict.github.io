287b93b0160044644f9b7d12e1167a1e92faddcf
Conway <yawnoc@pm.me>
2023-07-30 23:40:14 +0800

    Add zaak

--- entries/index.cmd
+++ entries/index.cmd
@@ -65,6 +65,7 @@ Work in progress.
   ==
   - [`zaa`]
   - [`zaai`]
+  - [`zaak`]
   - [`zai`]
   - [`zak`]
   ==
---
+++ entries/zaak.cmd
@@ -0,0 +1,18 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+< /res/rules/links.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> zaak
+* %date-modified --> 2023-07-30
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[From Page~12|n51 of the original onwards. Work in progress.]]
+--
+
+#{.williams} Ch(a/)k. [[zaak]]
