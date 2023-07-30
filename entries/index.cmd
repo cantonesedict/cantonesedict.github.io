04f154eac1b1ba30eb0ccdf97fdad1968b24cbdf
Conway <yawnoc@pm.me>
2023-07-31 00:16:59 +0800

    Add zau

--- entries/index.cmd
+++ entries/index.cmd
@@ -89,6 +89,7 @@ Work in progress.
   - [`zang`]
   - [`zap`]
   - [`zat`]
+  - [`zau`]
   ==
 ====
 </nav>
---
+++ entries/zau.cmd
@@ -0,0 +1,18 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+< /res/rules/links.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> zau
+* %date-modified --> 2023-07-30
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[From Page~25|n64 (Chau) and Page~560 (Tsau) of the original onwards. Work in progress.]]
+--
+
+#{.williams} Chau. Tsau. [[zau]]
