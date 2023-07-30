e48bb9644ad3d372554c73d142ddf41a90beef46
Conway <yawnoc@pm.me>
2023-07-31 00:37:27 +0800

    Add ci

---
+++ entries/ci.cmd
@@ -0,0 +1,18 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+< /res/rules/links.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> ci
+* %date-modified --> 2023-07-30
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[From Page~10*|n81 (Ch(')(i/)) and Page~615 (Ts(')z') of the original onwards. Work in progress.]]
+--
+
+#{.williams} Ch(')(i/). Ts(')z'. [[ci]]
