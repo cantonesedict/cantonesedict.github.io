bbf54c87e23924d52f252d550e301fc1030dcbc9
Conway <yawnoc@pm.me>
2023-08-02 00:10:54 +0800

    Rewrite home page version explainer

--- index.cmd
+++ index.cmd
@@ -6,7 +6,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Home
-* %date-modified --> 2023-07-29
+* %date-modified --> 2023-08-01
 * %copyright-prior-years -->
 * %modern-colour --> crimson
 
@@ -20,7 +20,7 @@ OrdinaryDictionaryReplacement: #.no-black-serif-note
 
 --{.modern}
 A modern edition of 《英華分韻撮要》
-[''A Tonic Dictionary of the Chinese Language in the Canton Dialect''][tonic] (1856)
+[''A Tonic Dictionary of the Chinese Language in the Canton Dialect'' (1856)]<n2>
 by [Samuel Wells Williams].
 --
 
@@ -30,17 +30,47 @@ by [Samuel Wells Williams].
 ##{#modern .modern} Modernisation
 
 --{.modern}
-The version of ''Tonic Dictionary'' used as the base reference is
-[`ark:/13960/t0ps1ph66` at archive.org][tonic],
-which is a scan of the copy held at
-the Library of the University of Illinois at Urbana-Champaign.
+The version of ''Tonic Dictionary'' used as the master reference is
+[`ark:/13960/s20jtk4f14j` at archive.org]<n2>,
+sourced from [a Google Books scan][google] of
+[83600-B held at <span lang="de">ÖNB Hauptabt. Heldenplatz</span>][onb].
 --
 --{.modern}
-Where there be missing pages or damage to the base reference version,
-a secondary version is used a subsidiary reference,
-namely [`ark:/13960/s20jtk4f14j` at archive.org][tonic2],
-from a [scan by Google](https://books.google.com/books?id=X8JhunQfImoC).
+While it would have been more preferable to use
+[`ark:/13960/t0ps1ph66` at archive.org][uiuc]
+from the Library of the University of Illinois at Urbana-Champaign
+(being both a higher-quality colour scan and not watermarked by Google),
+that version has severe paging issues:
 --
+++{.modern}
+1.
+  Pages~xxxiv and~xxxv are missing (they should appear between [n38] and [n39]).
+1.
+  Pages are duplicated:
+  ==
+  - Pages~14 through~40 appear twice:
+    once at [n53] through [n79], and again at [n85] through [n111].
+  - Pages~9* through~13* appear twice:
+    once at [n80] through [n84], and again at [9] through [13].
+  ==
+  It is unclear whether this is an error on the part of the book printer,
+  the scanner, or both.
+++
+
+[google]: https://books.google.com/books?id=X8JhunQfImoC
+[onb]: https://search.onb.ac.at/permalink/f/128lc6g/ONB_alma21317554760003338
+[uiuc]: https://archive.org/details/tonicdictionaryo00will/page/n6/mode/1up
+[n38]: https://archive.org/details/tonicdictionaryo00will/page/n38/mode/1up
+[n39]: https://archive.org/details/tonicdictionaryo00will/page/n39/mode/1up
+[n53]: https://archive.org/details/tonicdictionaryo00will/page/n53/mode/1up
+[n79]: https://archive.org/details/tonicdictionaryo00will/page/n79/mode/1up
+[n80]: https://archive.org/details/tonicdictionaryo00will/page/n80/mode/1up
+[n84]: https://archive.org/details/tonicdictionaryo00will/page/n84/mode/1up
+[n85]: https://archive.org/details/tonicdictionaryo00will/page/n85/mode/1up
+[n111]: https://archive.org/details/tonicdictionaryo00will/page/n111/mode/1up
+[9]: https://archive.org/details/tonicdictionaryo00will/page/9/mode/1up
+[13]: https://archive.org/details/tonicdictionaryo00will/page/13/mode/1up
+
 --{.modern}
 The modernisation is a ongoing solo effort by Conway, consisting of:
 --
