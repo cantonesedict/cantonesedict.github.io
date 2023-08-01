0289a870b1f63c3c0acba7083174afd894288246
Conway <yawnoc@pm.me>
2023-08-02 00:47:34 +0800

    Update English Title Page for new links

--- title/english.cmd
+++ title/english.cmd
@@ -6,15 +6,14 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> English Title Page: A Tonic Dictionary of the Chinese Language in the Canton Dialect.
-* %date-modified --> 2023-07-29
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From [the English title page of the original][n6].]]
+[[From [the English title page of the original]<n10>.]]
 --
-[n6]: https://archive.org/details/tonicdictionaryo00will/page/n6/mode/1up
 
 
 #{.williams} A Tonic Dictionary of the Chinese Language in the Canton Dialect.
@@ -22,16 +21,19 @@ OrdinaryDictionaryReplacement: #.properties-override
 --{.williams .title-information}
 _(1)Ying (4)W(a/) (1)Fan Wan[(6)] Ts(')(u")t(8) I(u/)(3)._
 --
+<ins class="modern">
+(Remarks:
 =={.modern}
-- [[This is the Williams romanisation of the Chinese title, 英華分韻撮要
-  ying1 waa4 fan1 wan6 cyut3 yiu3.]]
+- This is the Williams romanisation of the Chinese title, 英華分韻撮要
+  ying1 waa4 fan1 wan6 cyut3 yiu3.
 
-- [[Corrected _Wan(3)_ (wan3) to _Wan(6)_ (wan6).
+- Corrected _Wan(3)_ (wan3) to _Wan(6)_ (wan6).
   The reading of 韻 in 陽去 (tone~6) is that according to the 分韻 itself,
-  which has followed the expected reflex from Middle Chinese 王問切.]]
+  which has followed the expected reflex from Middle Chinese 王問切.
 
-- [[The reading preferred by most modern speakers is in 陽上 (tone~5).]]
+- The reading preferred by most modern speakers is in 陽上 (tone~5).)
 ==
+</ins>
 
 --{.williams .title-information}
 _By S.~Wells Williams._
@@ -46,7 +48,7 @@ __Canton:__ Printed at the Office of the Chinese Repository.
 --
 
 ||{.image-container s="margin: 2em 0"}
-  [![A scan of the English title page of the 1856 Tonic Dictionary.][scan]][n6]
+  [![A scan of the English title page of the 1856 Tonic Dictionary.][scan]]<n10>
 ||
 
 [scan]: english.jpg
