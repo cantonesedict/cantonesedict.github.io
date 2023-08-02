f8b2d2576c043767dd06f6bc433045fe6d6247bb
Conway <yawnoc@pm.me>
2023-08-02 22:19:48 +0800

    Update caap for new links

--- entries/caap.cmd
+++ entries/caap.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caap
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~23|n62 of the original onwards. Work in progress.]]
+[[From Page~23|n66 of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(')(a/)p. [[caap]]
