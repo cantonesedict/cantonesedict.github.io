9a06ecec3569655fd557af19923884c69276b57f
Conway <yawnoc@pm.me>
2023-08-02 21:50:10 +0800

    Update zak for new links

--- entries/zak.cmd
+++ entries/zak.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zak
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~12|n51 (Chak) and Page~550 (Tsak) of the original onwards. Work in progress.]]
+[[From Page~12|n55 (Chak) and Page~550 (Tsak) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Chak. Tsak. [[zak]]
