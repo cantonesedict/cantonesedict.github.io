6797c2dd2de8d9ca2308042c42102aa77a8850be
Conway <yawnoc@pm.me>
2023-08-02 22:10:58 +0800

    Update zang for new links

--- entries/zang.cmd
+++ entries/zang.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zang
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~20|n59 (Chang) and Page~556 (Tsang) of the original onwards. Work in progress.]]
+[[From Page~20|n63 (Chang) and Page~556 (Tsang) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Chang. Tsang. [[zang]]
