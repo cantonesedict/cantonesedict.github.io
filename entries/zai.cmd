7b33d070c212332ffc4e2d4faad6d48667a9f881
Conway <yawnoc@pm.me>
2023-08-02 21:44:43 +0800

    Update zai for new links

--- entries/zai.cmd
+++ entries/zai.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zai
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~9|n48 (Chai) and Page~548 (Tsai) of the original onwards. Work in progress.]]
+[[From Page~9|n52 (Chai) and Page~548 (Tsai) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Chai. Tsai. [[zai]]
