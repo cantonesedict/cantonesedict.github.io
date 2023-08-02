08745031bfd0ca08f83e95e011c7fe33c9e908ee
Conway <yawnoc@pm.me>
2023-08-02 22:07:55 +0800

    Update caan for new links

--- entries/caan.cmd
+++ entries/caan.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caan
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~20|n59 (Ch(')(a/))n and Page~555 (Ts(')(a/)n) of the original onwards. Work in progress.]]
+[[From Page~20|n63 (Ch(')(a/))n and Page~555 (Ts(')(a/)n) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(')(a/)n. Ts(')(a/)n. [[caan]]
