d71355e0cc362d4cf0cf2ac08804c1f8baa10212
Conway <yawnoc@pm.me>
2023-08-02 21:54:13 +0800

    Update caak for new links

--- entries/caak.cmd
+++ entries/caak.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caak
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~13|n52 (Ch(')(a/)k) and Page~551 (Ts(')(a/)k) of the original onwards. Work in progress.]]
+[[From Page~13|n56 (Ch(')(a/)k) and Page~551 (Ts(')(a/)k) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(')(a/)k. Ts(')(a/)k. [[caak]]
