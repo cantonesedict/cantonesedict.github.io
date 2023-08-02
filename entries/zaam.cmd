d0f166e5e7eb4b2fa44dee22b2b575a99eca4ce2
Conway <yawnoc@pm.me>
2023-08-02 21:58:25 +0800

    Update zaam for new links

--- entries/zaam.cmd
+++ entries/zaam.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zaam
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~15|n54 (Ch(a/))m and Page~553 (Ts(a/)m) of the original onwards. Work in progress.]]
+[[From Page~15|n58 (Ch(a/))m and Page~553 (Ts(a/)m) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(a/)m. Ts(a/)m. [[zaam]]
