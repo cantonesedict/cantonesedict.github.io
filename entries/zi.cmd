0543359e7486bc0655631e06aa6ee80f2f9016a6
Conway <yawnoc@pm.me>
2023-08-02 22:49:10 +0800

    Update zi for new links

--- entries/zi.cmd
+++ entries/zi.cmd
@@ -1,18 +1,17 @@
 < /res/rules/boilerplate.cmdr
 < /res/rules/syntax.cmdr
-< /res/rules/links.cmdr
 
 OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zi
-* %date-modified --> 2023-07-30
+* %date-modified --> 2023-08-02
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~37|n76 (Ch(i/)) and Page~610 (Tsz') of the original onwards. Work in progress.]]
+[[From Page~37|n80 (Ch(i/)) and Page~610 (Tsz') of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(i/). Tsz'. [[zi]]
