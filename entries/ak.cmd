c545c5c5e486ac093a0053516dfb2444da133829
Conway <yawnoc@pm.me>
2023-08-03 22:05:06 +0800

    Switch character heading IDs to tone first

--- entries/ak.cmd
+++ entries/ak.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> ak
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-03
 * %copyright-prior-years -->
 
 %%%
