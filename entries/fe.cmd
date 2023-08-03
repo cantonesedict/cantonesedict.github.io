8c7b5909291cc1be73ddc58ea9a9c1eb9fc64f63
Conway <yawnoc@pm.me>
2023-08-03 23:45:37 +0800

    Add fe

---
+++ entries/fe.cmd
@@ -0,0 +1,17 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> fe
+* %date-modified --> 2023-08-03
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[Not present in the original ''Tonic Dictionary''. Work in progress.]]
+--
+
+#{.modern} [[fe]]
