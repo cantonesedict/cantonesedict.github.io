58235f9e3734d3f1c5025fcf4b94cfe973621813
Conway <yawnoc@pm.me>
2023-08-06 00:58:52 +0800

    Update faat for appendix

--- entries/faat.cmd
+++ entries/faat.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> faat
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~50 of the original onwards. Work in progress.]]
+[[From Page~50 & Page~714 of the original onwards. Work in progress.]]
 --
 
 #{.williams} F(a/)t. [[faat]]
