27b93b3bead810999f934bab05606f1ec54faf82
Conway <yawnoc@pm.me>
2023-08-06 00:49:29 +0800

    Update zik for appendix

--- entries/zik.cmd
+++ entries/zik.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zik
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~13* (Chik) and Page~568 (Tsik) of the original onwards. Work in progress.]]
+[[From Page~13* & Page~713 (Chik) and Page~568 (Tsik) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Chik. Tsik. [[zik]]
