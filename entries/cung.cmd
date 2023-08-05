7641b0d8d7326b3ac5217fedf273f4db79f34e77
Conway <yawnoc@pm.me>
2023-08-06 00:51:07 +0800

    Update cung for appendix

--- entries/cung.cmd
+++ entries/cung.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> cung
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~40* (Ch(')ung) and Page~608 (Ts(')ung) of the original onwards. Work in progress.]]
+[[From Page~40* & Page~713 (Ch(')ung) and Page~608 (Ts(')ung) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Ch(')ung. Ts(')ung. [[cung]]
