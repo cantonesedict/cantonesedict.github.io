e7e6618b9b3a20755f64da92639b769e90b6401b
Conway <yawnoc@pm.me>
2023-08-06 01:06:24 +0800

    Update fui for appendix

--- entries/fu.cmd
+++ entries/fu.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fu
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
