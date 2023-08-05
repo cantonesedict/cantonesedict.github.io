43b8835ed88ff51b78623cca302cb6ea10cbee00
Conway <yawnoc@pm.me>
2023-08-06 01:03:53 +0800

    Update fo for appendix

--- entries/fik.cmd
+++ entries/fik.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fik
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
--- entries/fo.cmd
+++ entries/fo.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fo
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~53 of the original onwards. Work in progress.]]
+[[From Page~53 & Page~714 of the original onwards. Work in progress.]]
 --
 
 #{.williams} Fo. [[fo]]
