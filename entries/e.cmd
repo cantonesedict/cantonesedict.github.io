c9115cb01cacea0adefcb3d9034ca31ce7b0a118
Conway <yawnoc@pm.me>
2023-08-06 00:53:53 +0800

    Update e for appendix

--- entries/e.cmd
+++ entries/e.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> e
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~42 of the original onwards. Work in progress.]]
+[[From Page~42 & Page~713 of the original onwards. Work in progress.]]
 --
 
 #{.williams} (E/). [[e]]
