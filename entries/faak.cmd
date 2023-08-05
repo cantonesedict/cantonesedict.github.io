bd6096f27fdc4112f62ede9fe06ecd7bef1835cb
Conway <yawnoc@pm.me>
2023-08-06 00:56:24 +0800

    Update faak for appendix

--- entries/faak.cmd
+++ entries/faak.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> faak
-* %date-modified --> 2023-08-04
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[Not present in the original ''Tonic Dictionary''. Work in progress.]]
+[[From Page~713 of the original onwards. Work in progress.]]
 --
 
-#{.modern} F(a/)k. [[faak]]
+#{.williams} F(a/)k. [[faak]]
