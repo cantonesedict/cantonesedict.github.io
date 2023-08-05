2f01077c6cb05f9b3d2ab480cb303d15888520ca
Conway <yawnoc@pm.me>
2023-08-06 00:58:20 +0800

    Update faan for appendix

--- entries/faan.cmd
+++ entries/faan.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> faan
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~47 of the original onwards. Work in progress.]]
+[[From Page~47 & Page~714 of the original onwards. Work in progress.]]
 --
 
 #{.williams} F(a/)n. [[faan]]
