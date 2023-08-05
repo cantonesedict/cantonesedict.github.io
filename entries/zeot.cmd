c774699592baa5f693a5714ef4a20e4bbfe91642
Conway <yawnoc@pm.me>
2023-08-06 00:51:36 +0800

    Update zeot for appendix

--- entries/zeot.cmd
+++ entries/zeot.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zeot
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~41 (Chut) and Page~609 (Tsut) of the original onwards. Work in progress.]]
+[[From Page~41 & Page~713 (Chut) and Page~609 (Tsut) of the original onwards. Work in progress.]]
 --
 
 #{.williams} Chut. Tsut. [[zeot]]
