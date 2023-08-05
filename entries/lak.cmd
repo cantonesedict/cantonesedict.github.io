cf448d721d492403aaca84aa2debd7f94ea6f8c7
Conway <yawnoc@pm.me>
2023-08-05 15:57:59 +0800

    Fix /entries/l* dates

--- entries/lak.cmd
+++ entries/lak.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> lak
-* %date-modified --> 2023-08-04
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
