8f85a87d35d494e025043401e5f880d9e5ca3dc8
Conway <yawnoc@pm.me>
2023-08-06 00:57:43 +0800

    Update fan for appendix

--- entries/fan.cmd
+++ entries/fan.cmd
@@ -11,7 +11,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 --{.modern}
-[[From Page~45 of the original onwards. Work in progress.]]
+[[From Page~45 & Page~713 of the original onwards. Work in progress.]]
 --
 
 #{.williams} Fan. [[fan]]
