e59ba68c953b6d4f52ff181a1839f418121679df
Conway <yawnoc@pm.me>
2023-08-05 22:23:01 +0800

    Add sok

---
+++ entries/sok.cmd
@@ -0,0 +1,17 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> sok
+* %date-modified --> 2023-08-05
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[From Page~451 (Shok) and Page~472 (Sok) of the original onwards. Work in progress.]]
+--
+
+#{.williams} Shok. Sok. [[sok]]
