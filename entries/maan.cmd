0c978ce0907d84a4769fc7f6126a5bd1b94312eb
Conway <yawnoc@pm.me>
2023-08-06 01:30:27 +0800

    Update for appendix Page 723

--- entries/maan.cmd
+++ entries/maan.cmd
@@ -11,7 +11,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 --{.modern}
-[[From Page~276 of the original onwards. Work in progress.]]
+[[From Page~276 & Page~723 of the original onwards. Work in progress.]]
 --
 
 #{.williams} M(a/)n. [[maan]]
--- entries/ng.cmd
+++ entries/ng.cmd
@@ -11,7 +11,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 %%%
 
 --{.modern}
-[[From Page~313 of the original onwards. Work in progress.]]
+[[From Page~313 & Page~723 of the original onwards. Work in progress.]]
 --
 
 #{.williams} 'Ng. [[ng]]
