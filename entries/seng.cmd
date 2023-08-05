b8847f6b38eb42b8a47b3b21282e1eaa86ce0637
Conway <yawnoc@pm.me>
2023-08-05 21:51:30 +0800

    Add seng

---
+++ entries/seng.cmd
@@ -0,0 +1,17 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> seng
+* %date-modified --> 2023-08-05
+* %copyright-prior-years -->
+
+%%%
+
+--{.modern}
+[[From Page~412 (Seng) and Page~433 (Sheng) of the original onwards. Work in progress.]]
+--
+
+#{.williams} Seng. Sheng. [[seng]]
