0cb434cc81e164a75b256e883dfd7fc6903d0abf
Conway <yawnoc@pm.me>
2023-08-06 01:07:02 +0800

    Update fun for appendix

--- entries/fun.cmd
+++ entries/fun.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fun
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~64 of the original onwards. Work in progress.]]
+[[From Page~64 & Page~715 of the original onwards. Work in progress.]]
 --
 
 #{.williams} F(u/)n. [[fun]]
