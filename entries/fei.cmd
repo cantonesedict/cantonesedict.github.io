b4871806747957cfc4fde1ff76e7aef7d6c2fe44
Conway <yawnoc@pm.me>
2023-08-06 01:00:23 +0800

    Update fei for appendix

--- entries/fei.cmd
+++ entries/fei.cmd
@@ -5,13 +5,13 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fei
-* %date-modified --> 2023-08-03
+* %date-modified --> 2023-08-05
 * %copyright-prior-years -->
 
 %%%
 
 --{.modern}
-[[From Page~52 of the original onwards. Work in progress.]]
+[[From Page~52 & Page~714 of the original onwards. Work in progress.]]
 --
 
 #{.williams} F(i/). [[fei]]
