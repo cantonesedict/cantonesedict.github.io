317ad14303fc682578d8a17564dc208c520bf817
Conway <yawnoc@pm.me>
2023-08-06 18:17:01 +0800

    Add 揞 ꜂òm (am2)

--- entries/am.cmd
+++ entries/am.cmd
@@ -67,7 +67,7 @@ S
 $$
 
 
-##{#2 .williams} (2)Am. [[am2 陰上]]
+##{#2 .williams} (2)Am. (2)(O\)m. [[am2 陰上]]
 
 ### 唵2 | _(2)am_ [[am2]]
 
@@ -87,5 +87,92 @@ C
     =={.modern}
     - "[A] word used by Budhists":
       A transliteration of the Sanskrit <span lang="sa">ॐ</span> (_Om_).
+    ==
+$$
+
+### 掩2 | _(2)am_ [[am2]]
+
+$$
+U
+  , [[U+63A9]]
+A
+  , [[$揞2]]
+W
+  ,
+    [[Page~3]] <br>
+    A colloquial word. [[**See $揞am2 for the canonical entry.**]]
+C
+  ,
+    =={.modern}
+    - Williams has 掩 "cover" twice: once as 掩 _(2)am_ [[am2]] on Page~3,
+      and once as 掩 _(1)(o\)m_ [[am1]] as a variant of $揞2 _(2)(o\)m_ [[am2]]
+      on Page~341.
+    ==
+$$
+
+### 揞2 | _(2)(o\)m_ [[am2]]
+
+$$
+U
+  , [[U+63DE]]
+A
+  , $掩2
+W
+  ,
+    [[Page~3, appearing as 掩 _(2)am_]] <br>
+    A colloquial word.
+    ==
+    - To cover with the hand;
+    - to conceal from;
+    - _(2)am (4)m(a/)i (1)sh(e/)ung (5)ng(a/)n_,
+        [[]]
+        to cover both eyes;
+    - _(2)am ch(u")(6)_
+        [[]]
+        hide it;
+    - _(2)am tik(7) p(u/)i(6) (2)shau_,
+        [[]]
+        give him a little sop, cover his palm.
+    ==
+    [[Page~341]] <br>
+    ==
+    - To cover, to hide, to screen, by putting something on;
+    - to, extinguish, to suppress;
+    - _(2)(o\)m ch(u")(6) (2)hau_
+        [[]],
+        to put the hand over the mouth;
+    - _(1)ch(e/) (2)(o\)m pat(7) ch(u")(6)_
+        [[]]
+        can not be hid;
+    - _(2)(o\)m ch(u")(6) (4)t(')au hok(8)_
+        [[]]
+        to lean the head on the hand;
+    - _(2)p(i/) (2)shau (2)(o\)m ch(u")(6)_
+        [[]]
+        cover it with your hand;
+    - _(2)(o\)m mat(9) (4)lo (2)k(u/)_
+        [[]],
+        "muffle the gong," _i.~e._ keep it quiet;
+    - _(2)(o\)m (1)fung (4)k(')am_
+        [[]],
+        to play a piano;
+    - _(2)(o\)m mit(9)_
+        [[]]
+        to put out, as fire;
+        to quash;
+    - _(2)(o\)m p(a/)k(8) (4)mai (4)mung_
+        [[]],
+        blindman's buff;
+    - _(2)(o\)m ch(u")(6) (5)k(')(u") (1)sh(e/)ung (5)ng(a/)n_
+        [[]],
+        blindfold his eyes;
+    - _(2)(o\)m mak(9)_
+        [[]]
+        to feel the pulse;
+    - _(2)(o\)m p(u/)i(6) (2)shau_
+        [[]]
+        to give hush-money, to bribe.
+    - The first character [[掩]] is read _(2)(i/)m_ [[jim2]],
+      but also used with this meaning in the _p(')ing shing_ [[平聲]] [[jim1]].
     ==
 $$
