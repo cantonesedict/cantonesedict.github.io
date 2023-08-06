a3919579f0fd82c93ea4b5ddaafae36aa2959ab5
Conway <yawnoc@pm.me>
2023-08-06 18:42:32 +0800

    Add Add 揞 ꜂òm (am2) characters

--- entries/am.cmd
+++ entries/am.cmd
@@ -125,13 +125,13 @@ W
     - To cover with the hand;
     - to conceal from;
     - _(2)am (4)m(a/)i (1)sh(e/)ung (5)ng(a/)n_,
-        [[]]
+        [[am2 maai4 soeng1 ngaan5 揞埋雙眼]]
         to cover both eyes;
     - _(2)am ch(u")(6)_
-        [[]]
+        [[am2 zyu6 揞住]]
         hide it;
     - _(2)am tik(7) p(u/)i(6) (2)shau_,
-        [[]]
+        [[am2 dik1 bui6 sau2 揞滴背手]]
         give him a little sop, cover his palm.
     ==
     [[Page~341]] <br>
@@ -139,40 +139,45 @@ W
     - To cover, to hide, to screen, by putting something on;
     - to, extinguish, to suppress;
     - _(2)(o\)m ch(u")(6) (2)hau_
-        [[]],
+        [[am2 zyu6 hau2 揞住口]],
         to put the hand over the mouth;
     - _(1)ch(e/) (2)(o\)m pat(7) ch(u")(6)_
-        [[]]
+        [[ze1 am2 bat1 zyu6 遮揞不住]]
         can not be hid;
     - _(2)(o\)m ch(u")(6) (4)t(')au hok(8)_
-        [[]]
+        [[am2 zyu6 tau4 hok3 揞住頭殼]]
         to lean the head on the hand;
     - _(2)p(i/) (2)shau (2)(o\)m ch(u")(6)_
-        [[]]
+        [[bei2 sau2 am2 zyu6 畀手揞住]]
         cover it with your hand;
     - _(2)(o\)m mat(9) (4)lo (2)k(u/)_
-        [[]],
+        [[am2 mat6 lo4 gu2 揞密鑼鼓]],
         "muffle the gong," _i.~e._ keep it quiet;
     - _(2)(o\)m (1)fung (4)k(')am_
-        [[]],
+        [[am2 fung1 kam4 揞風琴]],
         to play a piano;
     - _(2)(o\)m mit(9)_
-        [[]]
+        [[am2 mit6 揞滅]]
         to put out, as fire;
         to quash;
     - _(2)(o\)m p(a/)k(8) (4)mai (4)mung_
-        [[]],
+        [[am2 baak3 mai4 mung4 揞？迷濛]],
         blindman's buff;
     - _(2)(o\)m ch(u")(6) (5)k(')(u") (1)sh(e/)ung (5)ng(a/)n_
-        [[]],
+        [[am2 zyu6 keoi5 soeng1 ngaan5 揞住佢雙眼]],
         blindfold his eyes;
     - _(2)(o\)m mak(9)_
-        [[]]
+        [[am2 mak6 揞脈]]
         to feel the pulse;
     - _(2)(o\)m p(u/)i(6) (2)shau_
-        [[]]
+        [[am2 bui6 sau2 揞背手]]
         to give hush-money, to bribe.
     - The first character [[掩]] is read _(2)(i/)m_ [[jim2]],
-      but also used with this meaning in the _p(')ing shing_ [[平聲]] [[jim1]].
+      but also used with this meaning in the _p(')ing shing_ [[平聲]] [[am1]].
+    ==
+C
+  ,
+    =={.modern}
+    - 揞背手: Now rarely heard; 揞口費 is more common for "hush money".
     ==
 $$
