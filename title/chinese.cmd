cc3f59c935966d5b572b77a753e04471c87a622d
Conway <yawnoc@pm.me>
2023-08-07 21:57:12 +0800

    Rewrite Chinese title <ins>

--- title/chinese.cmd
+++ title/chinese.cmd
@@ -5,36 +5,45 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Chinese Title Page: 《英華分韻撮要》
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-07
 * %copyright-prior-years -->
 
 %%%
 
---{.modern}
-[[From [the Chinese title page of the original]<n9>.]]
+<ins class="modern">
 --
+(From [the Chinese title page of the original]<n9>.)
+--
+</ins>
 
 
 #{.williams} 《英華分韻撮要》
 
---{.modern}
-[[lit. "Anglo--Chinese Rime-Divided Handpicked Essentials"]]
+<ins class="modern">
 --
+(lit. "Anglo--Chinese Rime-Divided Handpicked Essentials")
+--
+</ins>
 
 --{.williams .title-information}
 咸豐丙辰年鐫
 --
 
---{.modern}
-[[lit. Engraved Ping-shan Year of H(a/)m-fung = 1856]]
---
 <ins class="modern">
+--
+(lit. Engraved Ping-shan Year of H(a/)m-fung = 1856)
+--
+--
 (Remarks:
+--
 ==
 - 丙辰 Ping-shan: year 53/60 of the [sexagenary cycle]
 - 咸豐 H(a/)m-fung: [Emperor Hsien-fêng], penultimate of the Ts(')ing Dynasty
-- Fun fact: 咸豐年 is now a Cantonese phrase meaning "ages ago")
+- Fun fact: 咸豐年 is now a Cantonese phrase meaning "ages ago"
 ==
+--
+End of remarks.)
+--
 </ins>
 
 [sexagenary cycle]: https://en.wikipedia.org/wiki/Sexagenary_cycle
@@ -44,20 +53,22 @@ OrdinaryDictionaryReplacement: #.properties-override
 衛三畏廉士甫編譯
 --
 
---{.modern}
-[[lit. Compiled [by] Mr Samuel Wells Williams]]
+<ins class="modern">
 --
+(lit. Compiled [by] Mr Samuel Wells Williams)
+--
+</ins>
 
 --{.williams .title-information}
 羊城中和行梓行
 --
 
---{.modern}
-[[lit. Printed Chung-wo Hong of [the] City of Rams = Canton]]
+<ins class="modern">
 --
+(lit. Printed Chung-wo Hong of [the] City of Rams = Canton)
+--
+</ins>
 
 ||{.image-container s="margin: 2em 0"}
-  [![A scan of the Chinese title page of the 1856 Tonic Dictionary.][scan]]<n9>
+  [![A scan of the Chinese title page of the 1856 Tonic Dictionary.](chinese.png)]<n9>
 ||
-
-[scan]: chinese.png
