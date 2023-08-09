0a103235d6b329e2c57f6d1bcce1156e56ce8f9a
Conway <yawnoc@pm.me>
2023-08-09 22:03:11 +0800

    Gather entry CMD names

--- autoupdate.py
+++ autoupdate.py
@@ -6,9 +6,20 @@
 Automatically update CMD files.
 """
 
+import os
+
+
+def gather_entry_cmd_names():
+    return sorted([
+        os.path.join(directory_path, file_name)
+        for directory_path, _, file_names in os.walk('entries/')
+        for file_name in file_names
+        if file_name.endswith('.cmd') and file_name != 'index.cmd'
+    ])
+
 
 def main():
-    pass
+    entry_cmd_names = gather_entry_cmd_names()
 
 
 if __name__ == '__main__':
