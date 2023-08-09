ca4d0e1ddfa84b1d4508ab18e6fcc89bf12b3ee4
Conway <yawnoc@pm.me>
2023-08-09 23:04:15 +0800

    Implement gather toned characters

--- autoupdate.py
+++ autoupdate.py
@@ -8,26 +8,53 @@ Automatically update CMD files.
 
 import os
 import re
+import sys
 
 
-def gather_toned_syllables(cmd_content):
-    return [
-        match.group('syllable')
-        for match in re.finditer(
-            pattern=r'^\#\# \{ \#(?P<tone> [1-6] ) .* \[\[ (?P<syllable> [a-z]+(?P=tone) ) .* \]\]$',
+def gather_tone_syllable_list(cmd_content):
+    return re.findall(
+        pattern=r'^ \#\# \{ \#(?P<tone> [1-6] ) .* \[\[ (?P<syllable> [a-z]+ )(?P=tone) .* \]\] $',
+        string=cmd_content,
+        flags=re.MULTILINE | re.VERBOSE,
+    )
+
+
+def gather_toned_characters_from_tone(cmd_content, tone_syllable_list):
+    return {
+        tone: re.findall(
+            pattern=fr'^ \#{{3}} [+]? [ ] (?P<toned_character> \S {tone} ) .* \[\[ {syllable}{tone} \]\] $',
             string=cmd_content,
             flags=re.MULTILINE | re.VERBOSE,
         )
-    ]
+        for tone, syllable in tone_syllable_list
+    }
+
+
+def check_gathered_syllables(entry_cmd_name, tone_syllable_list):
+    cmd_syllable = re.sub(
+        pattern=r'entries/(?P<syllable>[a-z]+)\.cmd',
+        repl=r'\g<syllable>',
+        string=entry_cmd_name,
+    )
+
+    gathered_syllables = set(s for _, s in tone_syllable_list)
+    if gathered_syllables and gathered_syllables != {cmd_syllable}:
+        print(
+            f'Error in `{entry_cmd_name}`: gathered_syllables {gathered_syllables} != `{{{cmd_syllable}}}`',
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
 
 def update_entry(entry_cmd_name):
     with open(entry_cmd_name, 'r', encoding='utf-8') as old_cmd_file:
         old_cmd_content = old_cmd_file.read()
 
-    toned_syllables = gather_toned_syllables(old_cmd_content)
-    if toned_syllables:
-        print(toned_syllables)
+    tone_syllable_list = gather_tone_syllable_list(old_cmd_content)
+    check_gathered_syllables(entry_cmd_name, tone_syllable_list)
+    toned_characters_from_tone = gather_toned_characters_from_tone(old_cmd_content, tone_syllable_list)
+    if toned_characters_from_tone:
+        print(toned_characters_from_tone)
 
 
 def update_entries(entry_cmd_names):
