ac63e93006c3cbd814b7ad5d9f7b1c0649147bc2
Conway <yawnoc@pm.me>
2023-08-11 00:09:20 +0800

    Revise hei

--- entries/hei.cmd
+++ entries/hei.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> hei
-* %date-modified --> 2023-08-06
+* %date-modified --> 2023-08-10
 * %copyright-prior-years -->
 
 %%%
-
---{.modern}
-[[From Page~79 of the original onwards. **Work in progress.**]]
---
 
 #{.williams} H(i/). [[hei]]
 
@@ -22,26 +18,57 @@ OrdinaryDictionaryReplacement: #.properties-override
 ==
 </nav>
 
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+- [hei3](#3)
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - H(i/). [[Page~79]]
+  ==
+WP
+  ==
+  - H(i/) <span class="modern">= [hiː]</span>
+  ==
+MP
+  ==
+  - hei = [hei]
+  ==
+$$
+
 
 ##{#3 .williams} H(i/)(3). [[hei3 陰去]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $屭3
+==
+</nav>
+<## /tone-3-characters ##>
 
 ###+ 屭3 | _h(i/)(3)_ [[hei3]]
 
 $$
 U
-  , [[U+5C6D]]
+  U+5C6D
 V
-  , [[ai3: for colloquial senses]]
+  ==
+  -{.modern} hei3 (for colloquial senses)
+  ==
 W
-  , [[Not present]]
+  [[Not present]]
 C
-  ,
-    =={.modern}
-    - 【屭】 Of great strength. See 贔屭 under $贔bei6. [[TODO]]
-    ==
+  ==
+  - 【屭 hei3】 Of great strength. See 贔屭 under $贔bei6. [[TODO]]
+  ==
 S
-  ,
-    =={.modern}
-    - $屭ai3
-    ==
+  =={.modern}
+  - $屭ai3
+  ==
 $$
