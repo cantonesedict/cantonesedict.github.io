4ca5abd730d79e2b502c4a12f69158adceda38a6
Conway <yawnoc@pm.me>
2023-08-11 23:04:28 +0800

    Mark am done

--- entries/am.cmd
+++ entries/am.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Am. (O\)m. [[am]]
 
-<nav class="sideways">
-=={.modern}
-- [[Work in progress]]
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entries by Jyutping slug
-* %date-modified --> 2023-08-10
+* %date-modified --> 2023-08-11
 * %copyright-prior-years -->
 
 OrdinaryDictionaryReplacement: #.no-black-serif-note
@@ -61,7 +61,7 @@ RegexDictionaryReplacement: #.incipit-navigation-links
 - $aau (work in progress)
 - $ai
 - $ak
-- $am (work in progress)
+- $am
 - $ang (work in progress)
 - $ap (work in progress)
 - $at (work in progress)
