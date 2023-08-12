eeacb7a02b705a2fd767fd015e8e7e3b2f83abc7
Conway <yawnoc@pm.me>
2023-08-12 18:15:08 +0800

    Add 鶯 ꜀ang (ang1)

--- entries/ang.cmd
+++ entries/ang.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [ang1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -42,4 +43,51 @@ S
   ==
   - $ngang [[TODO]]
   ==
+$$
+
+
+##{#1 .williams} (1)Ang. [[ang1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $鶯1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 鶯1 | _(1)ang_ [[ang1]]
+
+$$
+R
+  鳥 + 10
+U
+  U+9DAF
+W
+  ====
+  - [[Page~4 (1)鶯]]
+    ==
+    - The nightingale, or a gay species of thrush
+      that nestles on the willow and sings well;
+    - variegated plumes;
+    - _(4)wong (1)ang_
+        [[wong4 ang1 黃鶯]],
+        the mango bird;
+    - _(1)ang (1)ko_
+        [[ang1 go1 鶯歌]],
+        a parrot;
+    - _(1)ang (1)ko p(i/)(6)_
+        [[ang1 go1 bei6 鶯歌鼻]]
+        a Roman nose.
+    ==
+    --
+    A colloquial word;
+    --
+    ==
+    - a knot in a stick of wood;
+    - _p(')ek(8) kau(6) (4)ch(')(a/)i (1)ang_
+        [[pek3 gau6 caai4 ang1 劈嚿柴鶯]],
+        split this knot for me.
+    ==
+  ====
 $$
