b2d371d348f6b127c9fd21020f20bc6b31fe51d1
Conway <yawnoc@pm.me>
2023-08-12 23:49:56 +0800

    Add 鴨 áp꜆ (aap3)

--- entries/aap.cmd
+++ entries/aap.cmd
@@ -20,6 +20,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [aap3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -40,5 +41,57 @@ MP
 S
   ==
   - $ngaap
+  ==
+$$
+
+
+##{#3 .williams} (A/)p(8). [[aap3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $鴨3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 鴨3 | _(a/)p(8)_ [[aap3]]
+
+$$
+R
+  鳥 + 5
+U
+  U+9D28
+V
+  ==
+  - aap3-2 (colloquial rising-tone noun)
+  ==
+W
+  ====
+  - [[Page~5 鴨(8)]]
+    ==
+    - A duck, a mallard;
+    - _(2)h(i/)n (a/)p(8)_
+        [[hin2 aap3-2 蜆鴨]]
+        a wild duck;
+    - _l(a/)p(9) (a/)p(8)_
+        [[laap6 aap3-2 臘鴨]]
+        dried ducks;
+    - _(2)ch(')(a/)u (a/)p(8) p(')in(3)_
+        [[caau2 aap3 pin3-2 炒鴨片]]
+        fried duck cutlet;
+    - _(a/)p(8) (4)mi(u/)_
+        [[aap3 miu4 鴨苗]],
+        ducklings;
+    - _p(u/)i(6) (a/)p(8) (2)tsai_
+        [[bui6 aap3 zai2 焙鴨仔]],
+        to hatch ducks artificially.
+    ==
+  ====
+C
+  ==
+  - 【鴨】 (aap3-2):
+    Slang for male prostitute.
+    Compare $雞gai1 "chicken" for female prostitute.
   ==
 $$
