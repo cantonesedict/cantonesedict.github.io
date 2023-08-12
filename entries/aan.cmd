e5f93cf8d428ecfb2c89579d24b4664c860f4eb5
Conway <yawnoc@pm.me>
2023-08-12 17:38:48 +0800

    Add 晏 án꜄ (aan3)

--- entries/aan.cmd
+++ entries/aan.cmd
@@ -18,6 +18,11 @@ OrdinaryDictionaryReplacement: #.properties-override
 ==
 </nav>
 <## tones ##>
+<nav class="sideways">
+=={.modern}
+- [aan3](#3)
+==
+</nav>
 <## /tones ##>
 
 $$
@@ -36,5 +41,66 @@ MP
 S
   ==
   - $ngaan
+  ==
+$$
+
+
+##{#3 .williams} (A/)n(3). [[aan3 陰去]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $晏3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 晏3 | _(a/)n(3)_ [[aan3]]
+
+$$
+R
+  日 + 6
+U
+  U+664F
+W
+  ====
+  - [[Page~4, 晏(3)]]
+    ==
+    - Evening, sunset;
+    - a serene, clear, sky;
+    - tardy, behindhand;
+    - peaceful, gentle;
+    - rich, full, said of furs;
+    - a tiffin;
+    - _(2)h(i/) (1)shan (a/)n(3)_
+        [[hei2 san1 aan3 起身晏]]
+        to rise late;
+    - _sh(e/)ung(6) (a/)n(3)_
+        [[soeng6 aan3 上晏]]
+        forenoon;
+    - _fan(3) (a/)n(3) k(a/)u(3)_
+        [[fan3 aan3 gaau3 瞓晏覺]]
+        to take a siesta;
+    - _fong(3) (a/)n(3) hok(9)_
+        [[fong3 aan3 hok6 放晏學]]
+        recess in school;
+    - _shik(9) (a/)n(3)_
+        [[sik6 aan3 食晏]]
+        to eat lunch;
+    - _(a/)n(3) chau(3)_
+        [[aan3 zau3 晏晝]]
+        noon;
+    - _ts(o\)(6) (a/)n(3)_
+        [[zou6 aan3 做晏]]
+        to get tiffin;
+    - _k(o\)m(3)(a/)n(3)_
+        [[gam3 aan3 咁晏]]
+        so late!
+    ==
+  ====
+C
+  ==
+  - Williams' "tiffin": British for "lunch".
+  - Williams' 上晏: Now rarely heard; 上晝 is more common.
   ==
 $$
