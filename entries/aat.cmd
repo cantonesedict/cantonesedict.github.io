d9e3c4c27f0fc4b29a17ebd645a1ee9f96af5f43
Conway <yawnoc@pm.me>
2023-08-13 12:35:57 +0800

    Add 遏 Át꜆ (aat3)

--- entries/aat.cmd
+++ entries/aat.cmd
@@ -20,6 +20,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [aat3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -41,4 +42,43 @@ S
   ==
   - $ngaat
   ==
+$$
+
+
+##{#3 .williams} (A/)t(8) [[aat3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $遏3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 遏3 | _(A/)t(8)_ [[aat3]]
+
+$$
+R
+  辵 + 9
+U
+  U+904F
+W
+  ====
+  - [[Page~5 遏(8)]]
+    ==
+    - To stop, to bring to a standstill;
+    - unforeseen obstacle;
+    - to reach, to arrive;
+    - to cut off, to put a stop to;
+    - _(2)cho (a/)t(8)_
+        [[zo2 aat3 阻遏]]
+        to stop one doing, to restrain;
+    - _(a/)t(8) tek(9)_
+        [[aat3 dek6 遏糴]]
+        to hold on to rice;
+    - _(a/)t(8) yuk(9) (4)n(a/)n_
+        [[aat3 juk6 naan4 遏慾難]],
+        hard to curb one's lusts.
+    ==
+  ====
 $$
