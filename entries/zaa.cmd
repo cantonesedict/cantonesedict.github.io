7ff94b156511092bf0739dad0c85166bcc003d86
Conway <yawnoc@pm.me>
2023-08-15 22:13:49 +0800

    Do zaa lead

--- entries/zaa.cmd
+++ entries/zaa.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zaa
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-08-15
 * %copyright-prior-years -->
 
 %%%
-
---{.modern}
-[[From Page~7 (Ch(a/)) and Page~547 (Ts(a/)) of the original onwards.]]
---
 
 #{.williams} Ch(a/). Ts(a/). [[zaa]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - [[Work in progress]]
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ch(a/). [[Page~7]]
+  - Ts(a/). [[Page~547]]
+  ==
+WP
+  ==
+  - Ch(a/) <span class="modern">= [tɕaː]</span>
+  - Ts(a/) <span class="modern">= [tsaː]</span>
+  ==
+MP
+  ==
+  - zaa = [tsaː], [tɕaː]
+  ==
+$$
