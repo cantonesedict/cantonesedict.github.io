c4ec7aaa15c68b087031bdb63712493b1de95a23
Conway <yawnoc@pm.me>
2023-09-10 00:50:57 +0800

    Add 岔汊 ꜀Chʻá (caa1)

--- entries/caa.cmd
+++ entries/caa.cmd
@@ -47,10 +47,39 @@ $$
 <## tone-1-characters ##>
 <nav class="sideways">
 =={.modern}
+- $岔1
 - $差1
+- $汊1
 ==
 </nav>
 <## /tone-1-characters ##>
+
+### 岔1 | _(1)Ch(')(a/)_ [[caa1]]
+
+$$
+R
+  山 + 4
+U
+  U+5C94
+W
+  ====
+  - [[Page~8 (1)岔]]
+    ==
+    - [...] roads diverging;
+    - _(1)s(a/)m (1)ch(')(a/) l(o\)(6) (2)hau_
+        [[saam1 caa1 lou6 hau2 三岔路口]],
+        a trivium;
+    - [[see $汊caa1 for "branching water" sense]]
+    ==
+  ====
+C
+  ==
+  - Williams does not provide separate entries for 岔 and $汊1,
+    probably following Kangxi, which has 與汊同 under 岔.
+    I have split things up accordingly.
+  - Williams' 三岔路口: Also written 三叉路口.
+  ==
+$$
 
 ### 差1 | _(1)Ch(')(a/)_ [[caa1]]
 
@@ -94,5 +123,31 @@ S
   ==
   - $差caai1 (TODO)
   - $差ci1 (TODO)
+  ==
+$$
+
+### 汊1 | _(1)Ch(')(a/)_ [[caa1]]
+
+$$
+R
+  水 + 3
+U
+  U+6C4A
+W
+  ====
+  - [[Page~8 (1)汊]]
+    ==
+    - Streams [...] diverging;
+    - [[see $岔caa1 for "branching road" sense]]
+    - _(1)s(a/)m (1)ch(')(a/) (4)ho_
+        [[saam1 caa1 ho4 三汊河]],
+        a river in Li(a/)utung [[遼東]].
+    ==
+  ====
+C
+  ==
+  - Williams does not provide separate entries for $岔1 and 汊,
+    probably following Kangxi, which has 與汊同 under 岔.
+    I have split things up accordingly.
   ==
 $$
