be551b33a15e245bc83089312a52a9d96c763e0e
Conway Li <conway.li@rgbassurance.com.au>
2023-09-29 15:02:13 +1000

    Add 擠 ꜀Chai ꜀Tsai (zai1)

--- entries/zai.cmd
+++ entries/zai.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zai1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -40,4 +41,65 @@ MP
   ==
   - zai = [tsɐi], [tɕɐi]
   ==
+$$
+
+
+##{#1 .williams} (1)Chai (1)Tsai [[zai1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $擠1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 擠1 | _(1)Chai_ _(1)Tsai_ [[zai1]]
+
+$$
+R
+  手 + 14
+U
+  U+64E0
+W
+  ====
+  - [[Page~9|n52 (1)擠 _(1)Chai_ (zai1)]]
+    --
+    A colloquial word.
+    --
+    ==
+    - To place, to put down, to put by;
+    - _(1)chai (4)m(a/)i_
+        [[zai1 maai4 擠埋]],
+        put it aside;
+    - _(1)chai (1)p(i/)n ch(')(u")(3)_
+        [[zai1 bin1 cyu3 擠邊處]]
+        where shall I put it?
+    - _(1)chai (1)p(i/)n ch(')(u")(3) (1)t(o\) (2)h(o\)_
+        [[zai1 bin1 cyu3 dou1 hou2 擠邊處都好]],
+        put [it] down anywhere.
+    ==
+  - [[Page~548 (1)擠 _(1)Tsai_ (zai1)]]
+    ==
+    - To push, to push over, to upset;
+    - to press upon, to crowd;
+    - _(1)tsai (2)yung_
+        [[zai1 jung2 擠湧]],
+        to crowd and press upon.
+    ==
+  ====
+C
+  ====
+  - 【擠】 (zai1):
+    ++
+    1. To put, or put down.
+       ==
+       - 擠喺度 (zai1 hai2 dou6): "Put [it] over here."
+       ==
+    1. Crowded.
+       ==
+       - 人多擠迫 (jan4 do1 zai1 bik1): "Lots of people [and] crowded."
+       ==
+    ++
+  ====
 $$
