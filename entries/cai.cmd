e9b68a97f879271d8bf9536c412a20f6ad96a5ff
Conway <yawnoc@pm.me>
2023-10-08 18:47:48 +0800

    Add 妻 ꜀Tsʻai (cai1), 妻 Tsʻai꜄ (cai3)

--- entries/cai.cmd
+++ entries/cai.cmd
@@ -21,6 +21,8 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [cai1](#1)
+- [cai3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +40,94 @@ MP
   ==
   - cai = [tsʰɐi], [tɕʰɐi]
   ==
+$$
+
+
+##{#1 .williams} (1)Ts(')ai [[cai1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $妻1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 妻1 | _(1)Ts(')ai_ [[cai1]]
+
+$$
+R
+  女 + 5
+U
+  U+59BB
+W
+  ====
+  - [[Page~549 (1)妻]]
+    ==
+    - A wife, a partner, a spouse, a consort who is takea with legal ceremonies;
+    - _(1)ts(')ai (2)tsz'_
+        [[cai1 zi2 妻子]]
+        a wife;
+    - _ling(6) (1)ts(')ai_
+        [[ling6 cai1 令妻]],
+        your wife;
+    - _h(a/)k(8) (1)ts(')ai_
+        [[haak3 cai1 客妻]],
+        a courtesan;
+    - _l(o\)(6) (4)t(')au (1)ts(')ai_
+        [[lou6 tau4 cai1 路頭妻]],
+        a wayside wife, one taken while sojourning elsewhere;
+    - _(4)p(')ing (1)ts(')ai_
+        [[ping4 cai1 平妻]],
+        equal to a wife, _i.~e._ a concubine;
+    - _[(2)]ts(')(u") (1)ts(')ai_
+        [[ceoi2 cai1 娶妻]]
+        to take a wife;
+    - _(4)(i/)n (1)ts(')ai_
+        [[jin4 cai1 賢妻]]
+        my good wife, a virtuous wife;
+    - _(1)ki(u/) (1)ts(')ai (5)m(i/) ts(')(i/)p(8)_
+        [[giu1 cai1 mei5 cip3 嬌妻美妾]],
+        an accomplished wife aud handsome concubine.
+    ==
+    --
+    Also read _ts(')ai(3)_ [[cai3]]; [[**see $妻cai3**]].
+    --
+  ====
+C
+  ==
+  - Williams' 娶妻:
+    Changed Middle Chinese _ts(')(u")(3)_ [[ceoi3]]
+    to common reading _(2)ts(')(u")_ [[ceoi2]].
+  ==
+$$
+
+
+##{#3 .williams} Ts(')ai(3) [[cai3 陰去]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $妻3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 妻3 | _Ts(')ai(3)_ [[cai3]]
+
+$$
+R
+  女 + 5
+U
+  U+59BB
+W
+  ====
+  - [[Page~549 (1)妻]]
+    --
+    Also read _ts(')ai(3)_ [[cai3]];
+    --
+    ==
+    - to give a daughter as wife to one.
+    ==
+  ====
 $$
