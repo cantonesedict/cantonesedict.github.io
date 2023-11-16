03facb0b2fe09429c5f3e3b1ee12de4e79dedc97
Conway <yawnoc@pm.me>
2023-11-16 23:00:03 +0800

    Mark cai done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entries by Jyutping
-* %date-modified --> 2023-10-07
+* %date-modified --> 2023-11-16
 * %copyright-prior-years -->
 
 OrdinaryDictionaryReplacement: #.no-black-serif-note
@@ -124,7 +124,7 @@ RegexDictionaryReplacement: #.incipit-navigation-links
 - $caap (work in progress)
 - $caat (work in progress)
 - $caau (work in progress)
-- $cai (work in progress)
+- $cai
 - $cak (work in progress)
 - $cam (work in progress)
 - $can (work in progress)
