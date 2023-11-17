ebf75d397a30c45d7be66cd7577de4f557073f9f
Conway <yawnoc@pm.me>
2023-11-18 00:28:27 +0800

    Add 齋 ꜀Chái (zaai1)

--- entries/zaai.cmd
+++ entries/zaai.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zaai1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +39,123 @@ MP
   ==
   - zaai = [tsaːi], [tɕaːi]
   ==
+$$
+
+
+##{#1 .williams} (1)Ch(a/)i [[zaai1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $齋1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 齋1 | _(1)Ch(a/)i_ [[zaai1]]
+
+$$
+R
+  齊 + 3
+U
+  U+9F4B
+A
+  ==
+  - $斋1 [[TODO]]
+  ==
+W
+  ====
+  - [[Page~10|n53 (1)齋]]
+    ==
+    - To respect, to reverence;
+    - to abstain from;
+    - pure, reverential, serious;
+    - a retiring room, a closet;
+    - _(2)t(a/) (1)ch(a/)i_
+        [[daa2 zaai1 打齋]],
+        to get a soul out of tartarus;
+    - _(2)t(a/) (1)ch(a/)i hok(9)_
+        [[daa2 zaai1 hok6-2 打齋鶴]]
+        storks for the soul to ride to heaven;
+        _met._ a pander;
+    - _t(a/)i(6) sz'(6) (1)ch(a/)i_
+        [[daai6 si6 zaai1 大士齋]],
+        offerings before the purveyor of hungry spirits;
+    - _(4)h(a/)n (1)ch(a/)i_
+        [[haan4 zaai1 閒齋]],
+        a closet;
+    - _(1)sh(u") (1)ch(a/)i_
+        [[syu1 zaai1 書齋]],
+        a library;
+    - _(1)ts(')ing (1)ch(a/)i_
+        [[cing1 zaai1 清齋]],
+        an entire fast;
+    - _(2)lo (1)ch(a/)i_
+        [[lo2 zaai1 攞齋]],
+        to beg vegetables;
+    - _(1)ch(a/)i k(a/)i(3)_
+        [[zaai1 gaai3 齋戒]]
+        to fast from animal food;
+    - _(1)ch(a/)i tsi(u/)[(3)]_
+        [[zaai1 ziu3 齋醮]]
+        to fast at All-souls;
+    - _shik(9) (1)ch(a/)i_
+        [[sik6 zaai1 食齋]],
+        to fast on vegetables;
+    - _(4)lo hon(3) (1)ch(a/)i_
+        [[lo4 hon3 zaai1 羅漢齋]],
+        all sorts of vegetables;
+    - _(1)ch(a/)i (1)k(u")_
+        [[zaai1 geoi1 齋居]],
+        to live at ease;
+    - _(1)ch(a/)i (1)chong_
+        [[zaai1 zong1 齋莊]],
+        respectful, reverential.
+    ==
+    --
+    A colloquial word.
+    --
+    ==
+    - A final particle denoting that it was said, that it is so;
+    - _mat(7) (4)yan w(a/)(6) (1)ch(a/)i_
+        [[mat1 jan4 waa6 zaai1 乜人話齋]],
+        somebody has said;
+    - _(2)h(o\) (5)ts(')z' (5)k(')(u") w(a/)(6) (1)ch(a/)i_
+        [[hou2 ci5 keoi5 waa6 zaai1 好似佢話齋]],
+        just as he said.
+    ==
+  ====
+C
+  ==
+  - Williams' 齋戒: Properly, abstinence from all pleasures (meat, alcohol, sex).
+  - Williams' 齋醮: Corrected _tsi(u/)(6)_ [[ziu6]] to _tsi(u/)(3)_ [[ziu3]].
+  - Williams' 羅漢齋: The name of a dish, commonly called Buddha's Delight.
+  ==
+E
+  ====
+  - 【齋】 (zaai1):
+    ++
+    1. Vegetarian food.
+       ==
+       - 初一十五食齋 (co1 jat1 sap6 ng5 sik6 zaai1):
+         "Eating vegetarian [on the] first [and] fifteenth [of the lunar month]."
+       - 齋口唔齋心 (zaai1 hau2 m4 zaai1 sam1):
+         "Vegetarian [in] mouth [but] not vegetarian [at] heart"
+         (said of vegetarians that eat imitation meat).
+       ==
+    1. Plain, pure, etc.
+       ==
+       - 齋啡 (zaai1 fe1): "Straight coffee."
+       - 齋腸 (zaai1 coeng4-2): "Plain [rice-noodle] roll."
+       - 齋噏 (zaai1 ngap1): "Purely blabbering."
+       ==
+    ++
+  - 【話齋】 (waa6 zaai1):
+    ++
+    1. Emphatic form of "[as he] said".
+      ==
+      - 好似佢話齋 (hou2 ci5 keoi5 waa6 zaai1): "As he said."
+      ==
+    ++
+  ====
 $$
