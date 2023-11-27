75e4c57a354cd4707ab76fb428ae7e016152a268
Conway <yawnoc@pm.me>
2023-11-27 21:45:10 +0800

    Add 差 ꜀Chʻái (caai1)

--- entries/caa.cmd
+++ entries/caa.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caa
-* %date-modified --> 2023-10-07
+* %date-modified --> 2023-11-26
 * %copyright-prior-years -->
 
 %%%
@@ -193,7 +193,7 @@ E
   ====
 S
   ==
-  - $差caai1 (TODO)
+  - $差caai1
   - $差ci1 (TODO)
   ==
 $$
--- entries/caai.cmd
+++ entries/caai.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [caai1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,75 @@ WP
 MP
   ==
   - caai = [tsʰaːi], [tɕʰaːi]
+  ==
+$$
+
+
+##{#1 .williams} (1)Ch(')(a/)i [[caai1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $差1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 差1 | _(1)Ch(')(a/)i_ [[caai1]]
+
+$$
+R
+  工 + 7
+U
+  U+5DEE
+W
+  ====
+  - [[Page~11|n54 (1)差]]
+    ==
+    - To commission, to send on a message;
+    - to manage, to act;
+    - a messenger, an envoy;
+    - a legate, a commissioner;
+    - _(1)ch(')(a/)i sz'(3)_
+        [[caai1 si3 差使]]
+        a bailiff;
+    - _(1)ch(')(a/)i yik(9)_
+        [[caai1 jik6 差役]]
+        a policeman, a lictor;
+    - _(1)ch(')(a/)i sz'(6)_
+        [[caai1 si6 差事]]
+        an errand;
+    - _k(a/)i(3) (1)ch(')(a/)i_
+        [[gaai3 caai1 解差]],
+        a criminal's guard;
+    - _(2)f(u/) (1)ch(')(a/)i_
+        [[fu2 caai1 苦差]],
+        an unpleasant duty;
+    - _ch(')ut(7) (1)ch(')(a/)i_
+        [[ceot1 caai1 出差]],
+        sent as legate;
+    - _(2)p(')(a/)u (1)king (1)ch(')(a/)i_
+        [[paau2 ging1 caai1 跑京差]],
+        a courier with dispatches.
+    ==
+  ====
+E
+  ====
+  - 【差】 (caai1):
+    ++
+    1. An officer.
+       ==
+       - 差人 (caai1 jan4): "[Police] officer."
+       - 差佬 (caai1 lou2): "Copper."
+       - 差婆 (caai1 po4): "Copperess."
+       - 當差 (dong1 caai1): "Serve as [a] copper."
+       - 郵差 (jau4 caai1): "Post officer."
+       ==
+    ++
+  ====
+S
+  ==
+  - $差caa1
+  - $差ci1 (TODO)
   ==
 $$
