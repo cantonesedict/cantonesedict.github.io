c75a9f4aad4bf84a5e8c9ebf101071560fb50ba9
Conway <yawnoc@pm.me>
2023-12-13 23:23:24 +0800

    Add 仄 Chak꜆ (zak1)

--- entries/zak.cmd
+++ entries/zak.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zak1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,46 @@ WP
 MP
   ==
   - zak = [tsɐk̚], [tɕɐk̚]
+  ==
+$$
+
+
+##{#1 .williams} Chak(7) Tsak(7) [[zak1 高陰入]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $仄1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 仄1 | _Chak(7)_ [[zak1]]
+
+$$
+R
+  人 + 2
+U
+  U+4EC4
+W
+  ====
+  - [[Page~12|n55 仄(7)]]
+    ==
+    - Inclined, slanting, leaning, oblique;
+    - the name of the three deflected tones [[上聲, 去聲, 入聲]];
+    - _(1)k(')ing chak(7)_
+        [[king1 zak1 傾仄]]
+        tipped over, upset;
+    - _chak(7) (1)[shing]_
+        [[zak1 sing1 仄聲]]
+        the oblique tones;
+    - _chak(7) lau(6)_
+        [[zak1 lau6 仄陋]]
+        vile, erring.
+    ==
+  ====
+C
+  ==
+  - Williams' 仄聲: Corrected _(1)sheng_ [[seng1]] to _(1)shing_ [[sing1]].
   ==
 $$
