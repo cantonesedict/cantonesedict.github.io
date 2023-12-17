b6c802b99500b67287dd36df6621b43793293fba
Conway <yawnoc@pm.me>
2023-12-17 22:09:19 +0800

    Add 責 Chák꜆ (zaak3)

--- entries/zaak.cmd
+++ entries/zaak.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zaak3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,63 @@ WP
 MP
   ==
   - zaak = [tsaːk̚], [tɕaːk̚]
+  ==
+$$
+
+
+##{#3 .williams} Ch(a/)k(8) [[zaak3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $責3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 責3 | _Ch(a/)k(8)_ [[zaak3]]
+
+$$
+R
+  貝 + 4
+U
+  U+8CAC
+W
+  ====
+  - [[Page~12|n55 責(8)]]
+    ==
+    - To reprove, to reprimand, to blame;
+    - to punish, to fine;
+    - to sustain, to be responsible for, to have charge of;
+    - [[see $矺zaak3 for "weight/crush" senses (TODO)]]
+    - to ask, to demand;
+    - _ch(a/)k(8) fat(9)_
+        [[zaak3 fat6 責罰]]
+        to fine;
+    - [[...]]
+    - _ch(a/)k(8) (4)[sh(e/)ung]_
+        [[zaak3 soeng4 責償]],
+        to compel repayment;
+    - _ch(a/)k(8) [fat](9)_
+        [[zaak3 fat6 責罰]]
+        to punish;
+    - _ch(a/)k(8) man(6)_
+        [[zaak3 man6 責問]]
+        to subject to the question;
+    - _ch(a/)k(8) (4)shing_
+        [[zaak3 sing4 責成]],
+        to charge with;
+    - _ch(a/)k(8) yam(6) sham(6) chung(6)_
+        [[zaak3 jam6 sam6 zung6 責任甚重]]
+        onerous duties devolve on him;
+    - [[...]].
+    ==
+  ====
+C
+  ==
+  - Williams' 責償: Changed improper reading _(4)ch(')(e/)ung_ [[coeng4]]
+    to _(4)sh(e/)ung_ [[soeng4]].
+  - Williams' 責罰 (second occurrence): Changed _f(a/)t(9)_ [[faat6]]
+    to modern _fat(9)_ [[fat6]].
   ==
 $$
