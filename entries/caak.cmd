3b1fdeeb98ec1ed1dcbe2bad48f6dbecdd17ec3b
Conway <yawnoc@pm.me>
2023-12-22 23:44:39 +0800

    Do caak lead

--- entries/caak.cmd
+++ entries/caak.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caak
-* %date-modified --> 2023-08-02
+* %date-modified --> 2023-12-22
 * %copyright-prior-years -->
 
 %%%
-
---{.modern}
-[[From Page~13|n56 (Ch(')(a/)k) and Page~551 (Ts(')(a/)k) of the original onwards.]]
---
 
 #{.williams} Ch(')(a/)k. Ts(')(a/)k. [[caak]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - [[Work in progress]]
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ch(')(a/)k. [[Page~13|n56]]
+  - Ts(')(a/)k. [[Page~551]]
+  ==
+WP
+  ==
+  - Ch(')(a/)k <span class="modern">= [tɕʰaːk̚]</span>
+  - Ts(')(a/)k <span class="modern">= [tsʰaːk̚]</span>
+  ==
+MP
+  ==
+  - caak = [tsʰaːk̚], [tɕʰaːk̚]
+  ==
+$$
