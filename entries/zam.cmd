10d30eb606dd5bf7a77aae0f5ce9a0382177bd3b
Conway <yawnoc@pm.me>
2023-12-24 23:05:17 +0800

    Add 針 ꜀Cham (zam1) etc.

--- entries/zam.cmd
+++ entries/zam.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zam1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -40,4 +41,82 @@ MP
   ==
   - zam = [tsɐm], [tɕɐm]
   ==
+$$
+
+
+##{#1 .williams} (1)Cham [[zam1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $針1
+- $鍼1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 針1 | _(1)Cham_ [[zam1]]
+
+$$
+R
+  金 + 2
+U
+  U+91DD
+A
+  ==
+  - $鍼1
+  ==
+W
+  ====
+  - [[Page~14|n57 (1)針(1)鍼]]
+    ==
+    - A needle;
+    - a cauterizing probe;
+    - to prick;
+    - to use needles in acupuncture;
+    - leaves of the pine;
+    - _(1)ch(')(u")n (1)cham_
+        [[cyun1 zam1 穿針]],
+        to thread a needle;
+    - _[(2)]k(u/) (4)ch(')ui (1)cham_
+        [[gu1 ceoi4 zam1 鼓錘針]],
+        a pin;
+    - _(1)cham p(i/)(6) s(e/)uk[(8)] t(')(i/)t[(8)]_
+        [[zam1 bei6 soek3 tit3 針鼻削鐵]]
+        mean, stingy;
+    - _(1)cham (2)ch(i/)_
+        [[zam1 zi2 針黹]],
+        to embroider;
+    - _(4)hon (2)sh(u") (1)cham_
+        [[hon4 syu2 zam1 寒暑針]]
+        a thermometer;
+    - _(5)ng(a/)n (1)cham_
+        [[ngaan5 zam1 眼針]],
+        a needle;
+    - _(1)man (1)cham_
+        [[man1 zam1 蚊針]],
+        a musquito's sting.
+    ==
+  ====
+C
+  ==
+  - Williams' 鼓錘針: Corrected _(1)k(u/)_ [[gu1]] to _(2)k(u/)_ [[gu2]].
+  - Williams' 針鼻削鐵: Corrected _s(e/)uk(9) t(')(i/)t(9)_ [[soek6 tit6]] to _s(e/)uk(8) t(')(i/)t(8)_ [[soek3 tit3]].
+  ==
+$$
+
+### 鍼1 | _(1)Cham_ [[zam1]]
+
+$$
+R
+  金 + 9
+U
+  U+937C
+W
+  ====
+  - [[Page~14|n57 (1)針(1)鍼]]
+    --
+    [[Alternative form of 針. **See $針zam1 for the canonical entry.**]]
+    --
+  ====
 $$
