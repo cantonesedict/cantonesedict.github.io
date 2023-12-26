218940509a7e34e2b4148297c57328f1624582fc
Conway <yawnoc@pm.me>
2023-12-26 22:17:15 +0800

    Add 沈沉 ꜁Chʻam (cam4)

--- entries/cam.cmd
+++ entries/cam.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [cam4](#4)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,96 @@ WP
 MP
   ==
   - cam = [tsʰɐm], [tɕʰɐm]
+  ==
+$$
+
+
+##{#4 .williams} (4)Ch(')am (4)Ts(')am [[cam4 陽平]]
+
+<## tone-4-characters ##>
+<nav class="sideways">
+=={.modern}
+- $沈4
+- $沉4
+==
+</nav>
+<## /tone-4-characters ##>
+
+### 沈4 | _(4)Ch(')am_ [[cam4]]
+
+$$
+R
+  水 + 4
+U
+  U+6C89
+W
+  ====
+  - [[Page~15|n58 (4)沉(4)沈]]
+    --
+    [[Alternative form of 沉. **See $沉cam4 for the canonical entry.**]]
+    --
+  ====
+S
+  ==
+  - $沈sam2 (TODO)
+  ==
+$$
+
+### 沉4 | _(4)Ch(')am_ [[cam4]]
+
+$$
+R
+  水 + 4
+U
+  U+6C88
+A
+  ==
+  - $沈4
+  ==
+W
+  ====
+  - [[Page~15|n58 (4)沉(4)沈]]
+    ==
+    - To sink;
+    - to immerse;
+    - to put under water, to be lost;
+    - to quash, to suppress;
+    - muddy, deep, dull, as colors;
+    - a lake;
+    - _shat(9) (2)shau (4)ch(')am chui(6)_
+        [[sat6 sau2 cam4 zeoi6 實首沉墜]]
+        garrulous;
+    - _(4)ch(')am (4)lun_
+        [[cam4 leon4 沉淪]],
+        lost, ruined;
+    - _(4)ch(')am m(u/)t(9)_
+        [[cam4 mut6 沉沒]]
+        to lose in the water;
+    - _(4)ch(')am (1)(u")n_
+        [[cam4 jyun1 沉淵]],
+        lost in the depths;
+    - _(4)ch(')am on(3)_
+        [[cam4 on3 沉案]]
+        to quash a case;
+    - _(4)cham nik[(7)]_
+        [[cam4 nik1 沉溺]]
+        drowned, in doting on;
+    - _(4)ch(')am [(5)ch(')ung]_
+        [[cam4 cung5 沉重]]
+        very sick;
+    - _shap(9) (4)ch(')am (2)kau (4)p(')(o\)_
+        [[sap6 cam4 gau2 pou4 十沉九浮]],
+        the odds are against him;
+    - _(4)ch(')am (1)h(e/)ung_
+        [[cam4 hoeng1 沉香]],
+        aloes.
+    ==
+  ====
+C
+  ==
+  - Williams' 沉溺:
+    Changed Middle Chinese _nik(9)_ [[nik6]]
+    to common reading _nik(7)_ [[nik1]].
+  - Williams' 沉重: Corrected _chung(6)_ [[zung6 去聲]] to _(5)ch(')ung_ [[cung5 上聲]].
   ==
 $$
