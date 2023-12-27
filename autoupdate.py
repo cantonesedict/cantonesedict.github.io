cd1cdb9bdd55410396c54084b181630b3e2bf621
Conway <yawnoc@pm.me>
2023-12-27 23:24:44 +0800

    Rewrite as Updator to gather names on init

--- autoupdate.py
+++ autoupdate.py
@@ -11,51 +11,49 @@ import re
 import sys
 
 
-class PageUpdator:
-    @staticmethod
-    def gather_page_cmd_names():
-        return sorted([
+class Updator:
+    def __init__(self):
+        self.cmd_names = sorted([
             os.path.join(directory_path, file_name)
             for directory_path, _, file_names in os.walk('entries/')
             for file_name in file_names
             if file_name.endswith('.cmd') and file_name != 'index.cmd'
         ])
 
-    @staticmethod
-    def update_all(page_cmd_names):
-        for page_cmd_name in page_cmd_names:
-            PageUpdator._update(page_cmd_name)
+    def update_all(self):
+        for cmd_name in self.cmd_names:
+            Updator._update(cmd_name)
 
     @staticmethod
-    def _update(page_cmd_name):
-        with open(page_cmd_name, 'r', encoding='utf-8') as old_cmd_file:
+    def _update(cmd_name):
+        with open(cmd_name, 'r', encoding='utf-8') as old_cmd_file:
             old_cmd_content = old_cmd_file.read()
 
-        PageUpdator._check_jyutping_heuristic(page_cmd_name, old_cmd_content)
-        tone_syllable_list = PageUpdator._gather_tone_syllable_list(old_cmd_content)
-        PageUpdator._check_syllables(page_cmd_name, tone_syllable_list)
-        toned_characters_from_tone = PageUpdator._gather_toned_characters_from_tone(old_cmd_content, tone_syllable_list)
+        Updator._check_jyutping_heuristic(cmd_name, old_cmd_content)
+        tone_syllable_list = Updator._gather_tone_syllable_list(old_cmd_content)
+        Updator._check_syllables(cmd_name, tone_syllable_list)
+        toned_characters_from_tone = Updator._gather_toned_characters_from_tone(old_cmd_content, tone_syllable_list)
 
         new_cmd_content = old_cmd_content
-        new_cmd_content = PageUpdator._update_page_tones_navigation(new_cmd_content, tone_syllable_list)
-        new_cmd_content = PageUpdator._update_page_character_navigations(new_cmd_content, toned_characters_from_tone)
+        new_cmd_content = Updator._update_page_tones_navigation(new_cmd_content, tone_syllable_list)
+        new_cmd_content = Updator._update_page_character_navigations(new_cmd_content, toned_characters_from_tone)
 
         if new_cmd_content == old_cmd_content:
             return
 
-        with open(page_cmd_name, 'w', encoding='utf-8') as new_cmd_file:
+        with open(cmd_name, 'w', encoding='utf-8') as new_cmd_file:
             new_cmd_file.write(new_cmd_content)
 
     @staticmethod
-    def _check_jyutping_heuristic(page_cmd_name, cmd_content):
+    def _check_jyutping_heuristic(cmd_name, cmd_content):
         bad_jyutping_runs = [
             run
             for run in re.findall(pattern=r'\b[a-z]+[1-9]\b', string=cmd_content, flags=re.VERBOSE)
-            if PageUpdator._is_bad_jyutping_heuristic(run)
+            if Updator._is_bad_jyutping_heuristic(run)
         ]
         if bad_jyutping_runs:
             print(
-                f'Error in `{page_cmd_name}`: bad Jyutping in {bad_jyutping_runs}',
+                f'Error in `{cmd_name}`: bad Jyutping in {bad_jyutping_runs}',
                 file=sys.stderr,
             )
             sys.exit(1)
@@ -76,17 +74,17 @@ class PageUpdator:
         )
 
     @staticmethod
-    def _check_syllables(page_cmd_name, tone_syllable_list):
+    def _check_syllables(cmd_name, tone_syllable_list):
         cmd_syllable = re.sub(
             pattern=r'entries/(?P<syllable>[a-z]+)\.cmd',
             repl=r'\g<syllable>',
-            string=page_cmd_name,
+            string=cmd_name,
         )
 
         gathered_syllables = set(s for _, s in tone_syllable_list)
         if gathered_syllables and gathered_syllables != {cmd_syllable}:
             print(
-                f'Error in `{page_cmd_name}`: gathered_syllables {gathered_syllables} != `{{{cmd_syllable}}}`',
+                f'Error in `{cmd_name}`: gathered_syllables {gathered_syllables} != `{{{cmd_syllable}}}`',
                 file=sys.stderr,
             )
             sys.exit(1)
@@ -113,7 +111,7 @@ class PageUpdator:
     def _update_page_tones_navigation(cmd_content, tone_syllable_list):
         return re.sub(
             pattern=r'(?<=<## tones ##>\n).*?(?=<## /tones ##>\n)',
-            repl=PageUpdator._tones_navigation_cmd_content(tone_syllable_list),
+            repl=Updator._tones_navigation_cmd_content(tone_syllable_list),
             string=cmd_content,
             flags=re.DOTALL | re.MULTILINE,
         )
@@ -123,7 +121,7 @@ class PageUpdator:
         for tone, toned_characters in toned_characters_from_tone.items():
             cmd_content = re.sub(
                 pattern=fr'(?<=<## tone-{tone}-characters ##>\n).*?(?=<## /tone-{tone}-characters ##>\n)',
-                repl=PageUpdator._character_navigation_cmd_content(toned_characters),
+                repl=Updator._character_navigation_cmd_content(toned_characters),
                 string=cmd_content,
                 flags=re.DOTALL | re.MULTILINE,
             )
@@ -183,9 +181,10 @@ def gather_page_cmd_content(page_cmd_names):
 
 
 def main():
-    page_cmd_names = PageUpdator.gather_page_cmd_names()
-    PageUpdator.update_all(page_cmd_names)
+    updator = Updator()
+    updator.update_all()
 
+    page_cmd_names = updator.cmd_names  # TODO: purge me
     cmd_content_from_name = gather_page_cmd_content(page_cmd_names)
     page_count = len(page_cmd_names)
     wip_count = sum('work in progress' in cmd_content.lower() for cmd_content in cmd_content_from_name.values())
