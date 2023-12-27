2c76e0398bf200cf5fcfc727382728fdf8d31d2a
Conway <yawnoc@pm.me>
2023-12-27 21:47:16 +0800

    Add 斬 ꜂Chám (zaam2)

--- entries/zaam.cmd
+++ entries/zaam.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zaam2](#2)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,67 @@ WP
 MP
   ==
   - zaam = [tsaːm], [tɕaːm]
+  ==
+$$
+
+
+##{#2 .williams} (2)Ch(a/)m [[zaam2 陰上]]
+
+<## tone-2-characters ##>
+<nav class="sideways">
+=={.modern}
+- $斬2
+==
+</nav>
+<## /tone-2-characters ##>
+
+### 斬2 | _(2)Ch(a/)m_ [[zaam2]]
+
+$$
+R
+  斤 + 7
+U
+  U+65AC
+W
+  ====
+  - [[Page~15|n58 (2)斬]]
+    ==
+    - To sunder, to cut in two;
+    - to cut off;
+    - to execute, to decapitate;
+    - capital;
+    - to root up;
+    - to break off, as [intercourse];
+    - faded, forgotten, vanished;
+    - _(2)ch(a/)m (1)shui_
+        [[zaam2 zeoi1 斬衰]],
+        mourning for parents;
+    - _(2)ch(a/)m (5)t(')(u")n_
+        [[zaam2 tyun5 斬斷]],
+        to cut in two, to break off, as a business;
+    - _(2)ch(a/)m (4)t(')au_
+        [[zaam2 tau4-2 斬頭]],
+        to decollate;
+    - _(1)i(u/) (2)ch(a/)m_
+        [[jiu1 zaam2 腰斬]],
+        to divide a criminal in the middle;
+    - _(2)ch(a/)m (1)teng ts(i/)t(9) t(')(i/)t(8)_
+        [[zaam2 deng1 zit6 tit3 斬釘截鐵]]
+        to cut an iron nail---decided, sincere;
+    - _(1)k(a/)m (2)ch(a/)m_
+        [[gaam1 zaam2 監斬]],
+        to superintend an execution;
+    - _(2)ch(a/)m (1)hoi_
+        [[zaam2 hoi1 斬開]],
+        to cut open;
+    - _(2)ch(a/)m [fat](9)_
+        [[zaam2 fat6 斬伐]]
+        to subjugate; to prune.
+    ==
+  ====
+C
+  ==
+  - Williams' 腰斬: Now used figuratively for midway cancellation or termination.
+  - Williams' 斬伐: Changed _f(a/)t(9)_ [[faat6]] to modern _fat(9)_ [[fat6]].
   ==
 $$
