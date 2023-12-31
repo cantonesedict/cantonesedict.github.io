b0fdaea68f31483bf9096185f5cfaa2c3a339b7d
Conway <yawnoc@pm.me>
2024-01-01 00:22:00 +0800

    Add 攙 ꜀Chʻám (caam1) etc.

--- entries/caam.cmd
+++ entries/caam.cmd
@@ -5,8 +5,8 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caam
-* %date-modified --> 2023-12-31
-* %copyright-prior-years -->
+* %date-modified --> 2024-01-01
+* %copyright-prior-years --> 2023--
 
 %%%
 
@@ -21,6 +21,8 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [caam1](#1)
+- [caam4](#4)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +41,93 @@ WP
 MP
   ==
   - caam = [tsʰaːm], [tɕʰaːm]
+  ==
+$$
+
+
+##{#1 .williams} <ins class="modern">(1)Ch(')(a/)m</ins> (1)Ts(')(a/)m [[caam1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $攙1
+==
+</nav>
+<## /tone-1-characters ##>
+
+###+ 攙1 | _(1)Ch(')(a/)m_ [[caam1]]
+
+$$
+R
+  手 + 17
+U
+  U+6519
+V
+  ==
+  - caam4 (uncommon)
+  ==
+F
+  [[Not present]]
+W
+  ====
+  - [[Page~16|n59 (4)攙 _(4)Ch(')(a/)m_ (caam4); here normalised to _(1)Ch(')(a/)m_ (caam1)]]
+    ==
+    - To stab;
+    - to sustain, to support, to lead;
+    - to supply awant, to make up;
+    - to divide with another;
+    - to pull out;
+    - _[(1)]ch(')(a/)m fan(6) (4)lai (1)t(')(i/)m_
+        [[caam1 fan6 lai4 tim1 攙分嚟添]],
+        make another share for him;
+    - _[(1)]ch(')(a/)m (4)f(u/)_
+        [[caam1 fu4 攙扶]],
+        to uphold.
+    ==
+  ====
+C
+  ==
+  - The uncommon reading caam4 is that given by Williams.
+    Whilst caam4 is not unattested in Middle Chinese (being expected from 士咸切),
+    the canonical reading is caam1, expected from 楚銜切
+    (which is the primary reading recorded in Kangxi).
+    It is possible that Williams has mistakenly parsed 楚銜切 as 陽平 rather than 陰平.
+  ==
+S
+  ==
+  - $攙caam4
+  ==
+$$
+
+
+##{#4 .williams} (4)Ch(')(a/)m (4)Ts(')(a/)m [[caam4 陽平]]
+
+<## tone-4-characters ##>
+<nav class="sideways">
+=={.modern}
+- $攙4
+==
+</nav>
+<## /tone-4-characters ##>
+
+### 攙4 | _(4)Ch(')(a/)m_ [[caam4]]
+
+$$
+R
+  手 + 17
+U
+  U+6519
+F
+  [[Not present]]
+W
+  ====
+  - [[Page~16|n59 (4)攙]]
+    --
+    [[Reading variation; **see $攙caam1 for the canonical entry**.]]
+    --
+  ====
+S
+  ==
+  - $攙caam1
   ==
 $$
