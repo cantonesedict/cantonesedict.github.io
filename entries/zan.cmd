f033847ecfcd53067d00516cfb92c22ddf3a2ad3
Conway <yawnoc@pm.me>
2024-01-03 21:26:05 +0800

    Add 真 ꜀Chan (zan1)

--- entries/zan.cmd
+++ entries/zan.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zan1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,105 @@ WP
 MP
   ==
   - zan = [tsɐn], [tɕɐn]
+  ==
+$$
+
+
+##{#1 .williams} (1)Chan [[zan1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $眞1
+- $真1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 眞1 | _(1)Chan_ [[zan1]]
+
+$$
+R
+  目 + 5
+U
+  U+771E
+F
+  ==
+  - [[F1.79 眞]] 實也正也精也純也
+  ==
+W
+  ====
+  - [[Page~16|n59 (1)眞(1)真]]
+    --
+    [[Alternative form of 真. **See $真zan1 for the canonical entry.**]]
+    --
+  ====
+$$
+
+### 真1 | _(1)Chan_ [[zan1]]
+
+$$
+R
+  目 + 5
+U
+  U+771F
+A
+  ==
+  - $眞1
+  ==
+F
+  ==
+  - [[F1.79 眞]] 實也正也精也純也
+  ==
+W
+  ====
+  - [[Page~16|n59 (1)眞(1)真]]
+    ==
+    - True, sincere, unfeigned, genuine, pure;
+    - in fact, truly, indeed, no mistake;
+    - a likeness;
+    - actual, not secondary;
+    - spiritual, subtle, pure, unmixed;
+    - _(1)chan (4)yan_
+        [[zan1 jan4 真人]],
+        a sprite, a phantom of a man[;]
+    - _(1)chan ko(3) (5)ts(')z'_
+        [[zan1 go3 ci5 真個似]]
+        in fact, it was like;
+    - _(2)s(e/) (1)chan_
+        [[se2 zan1 寫真]],
+        to draw portraits;
+    - _(1)chan hai(6)_
+        [[zan1 hai6 真係]]
+        it is so;
+    - _(1)chan (1)tsong ching(3) [f(a/)n](6)_
+        [[zan1 zong1 zing3 faan6-2 真贓正犯]]
+        this thing proves him a thief;
+    - _(4)'m (1)ch(i/) (1)chan (2)k(a/)_
+        [[m4 zi1 zan1 gaa2 唔知真假]],
+        I don't know whether it is so or not;
+    - _(1)chan (1)sam_
+        [[zan1 sam1 真心]],
+        true-hearted;
+    - _(1)chan f(u/)i(3) h(i/)(3)_
+        [[zan1 fui3 hei3 真晦氣]]
+        low-spirited;
+    - _(1)chan ching(3)_
+        [[zan1 zing3 真正]]
+        truly, indeed;
+    - _(1)chan (2)kong_
+        [[zan1 kong2 真講]],
+        it ts really so.
+    ==
+  ====
+C
+  ==
+  - Williams' 真人:
+    The classical meaning of 真 is, Kangxi quotes, 《說文》仙人變形而登天也,
+    "The immortal man changing form and ascending to Heaven".
+    In modern non-Taoist usage this sense is now obsolete,
+    and 真人 now simply means "real person".
+  - Williams' 真贓正犯:
+    Corrected _fan(6)_ [[fan6]] to _f(a/)n(6)_ [[faan6]].
   ==
 $$
