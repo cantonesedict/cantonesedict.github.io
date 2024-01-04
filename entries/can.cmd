7e16f9d60ef2a1865bd5671a160fe6efa12c7882
Conway <yawnoc@pm.me>
2024-01-04 13:01:18 +0800

    Add 陳 ꜁Chʻan (can4)

--- entries/can.cmd
+++ entries/can.cmd
@@ -22,6 +22,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <nav class="sideways">
 =={.modern}
 - [can1](#1)
+- [can4](#4)
 ==
 </nav>
 <## /tones ##>
@@ -64,7 +65,7 @@ U
   U+55D4
 F
   ==
-  [[F1.81 嗔]] 怒也
+  - [[F1.81 嗔]] 怒也
   ==
 W
   ====
@@ -105,7 +106,7 @@ U
   U+778B
 F
   ==
-  [[F1.81 瞋]] 怒而張目也
+  - [[F1.81 瞋]] 怒而張目也
   ==
 W
   ====
@@ -122,5 +123,65 @@ C
   - Fan Wan and Williams both print this as ⿰目眞,
     but no separate code point to 瞋~U+778B exists for this form
     (blame Han unification inconsistency).
+  ==
+$$
+
+
+##{#4 .williams} (4)Ch(')an [[can4 陽平]]
+
+<## tone-4-characters ##>
+<nav class="sideways">
+=={.modern}
+- $陳4
+==
+</nav>
+<## /tone-4-characters ##>
+
+### 陳4 | _(4)Ch(')an_ [[can4]]
+
+$$
+R
+  阜 + 8
+U
+  U+9673
+F
+  ==
+  - [[F1.82 陳]] 列也張也敷也告也又姓
+  ==
+W
+  ====
+  - [[Page~18|n61 (4)陳]]
+    ==
+    - To arrange, to set in order, to spread out;
+    - to say, to reply;
+    - to state, to express;
+    - to memorialize;
+    - a long time, an old thing;
+    - many, all;
+    - a feudal state near the present capital of Hon(a/)n [[河南]];
+    - _tsoi(6) (4)ch(')an [ts(')(u")t](9) (4)l(e/)ung_
+        [[zoi6 can4 zyut6 loeng4 在陳絕糧]],
+        to lack food in Ch(')in---to be in straits;
+    - _(1)p(')(o\) (4)ch(')an_
+        [[pou1 can4 鋪陳]],
+        to seat or rank in order;
+    - _(4)ch(')an ch(')(i/)t(8)_
+        [[can4 cit3 陳設]]
+        to arrange in order;
+    - _(4)sh(e/)ung (2)pi(u/) (4)ch(')an (4)ts(')ing_
+        [[soeng5 biu2 can4 cing4 上表陳情]],
+        to state one's thoughts to H.~I.~M.;
+    - _(4)ch(')an (4)p(')(i/) k(o\)m(3) kau(6)_
+        [[can4 pei4 gam3 gau6 陳皮咁舊]]
+        old as a dried orange skin, faded.
+    ==
+  ====
+C
+  ==
+  - Williams' 在陳絕糧: Corrected _ts(')(u")t(9)_ [[cyut6]] to _ts(u")t(9)_ [[zyut6]].
+  ==
+S
+  ==
+  - $陳zan6
   ==
 $$
