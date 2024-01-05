457eefe50522c5e618b478fa5de05c8c1566f1a1
Conway <yawnoc@pm.me>
2024-01-05 16:54:14 +0800

    Add 2-stroke radical headings

---
+++ radicals/2.cmd
@@ -0,0 +1,92 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 2-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 2-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⼆]
+- [⼇]
+- [⼈]
+- [⼉]
+- [⼊]
+- [⼋]
+- [⼌]
+- [⼍]
+- [⼎]
+- [⼏]
+- [⼐]
+- [⼑]
+- [⼒]
+- [⼓]
+- [⼔]
+- [⼕]
+- [⼖]
+- [⼗]
+- [⼘]
+- [⼙]
+- [⼚]
+- [⼛]
+- [⼜]
+==
+</nav>
+
+## ⼆ (two)
+
+## ⼇ (lid)
+
+## ⼈ (man)
+
+## ⼉ (legs)
+
+## ⼊ (enter)
+
+## ⼋ (eight)
+
+## ⼌ (down box)
+
+## ⼍ (cover)
+
+## ⼎ (ice)
+
+## ⼏ (table)
+
+## ⼐ (open box)
+
+## ⼑ (knife)
+
+## ⼒ (power)
+
+## ⼓ (wrap)
+
+## ⼔ (spoon)
+
+## ⼕ (right open box)
+
+## ⼖ (hiding enclosure)
+
+## ⼗ (ten)
+
+## ⼘ (divination)
+
+## ⼙ (seal)
+
+## ⼚ (cliff)
+
+## ⼛ (private)
+
+## ⼜ (again)
