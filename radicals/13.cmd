d3c2e38bd08252f6f3bf1014365d1ac5fa59b6bd
Conway <yawnoc@pm.me>
2024-01-05 21:56:25 +0800

    Add /radicals/13

---
+++ radicals/13.cmd
@@ -0,0 +1,55 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 13-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 13-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⿌]
+- [⿍]
+- [⿎]
+- [⿏]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⿌ (frog)
+
+<## radical-⿌-characters ##>
+<## /radical-⿌-characters ##>
+
+
+## ⿍ (tripod)
+
+<## radical-⿍-characters ##>
+<## /radical-⿍-characters ##>
+
+
+## ⿎ (drum)
+
+<## radical-⿎-characters ##>
+<## /radical-⿎-characters ##>
+
+
+## ⿏ (rat)
+
+<## radical-⿏-characters ##>
+<## /radical-⿏-characters ##>
