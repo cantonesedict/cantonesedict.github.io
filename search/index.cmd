5c4ad820fca4a4a84471696f94cf0d8228f8706e
Conway <yawnoc@pm.me>
2024-01-05 23:13:54 +0800

    Create bare search page

---
+++ search/index.cmd
@@ -0,0 +1,44 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Search by character or code point
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> search by Chinese character or by Unicode code point
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+||||{.modern .filter-container}
+<label for="filter">Search:</label>
+<input type="text" id="filter" onkeyup="performSearch()" placeholder="(up to 16~chars / 1~code point)">
+<noscript>
+||{.modern}
+**JavaScript is required for search to work.**
+||
+</noscript>
+||||
+
+u<````````
+<script>
+</script>
+````````>
+
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Character
+    ; Code point
+    ; Character entry links
+|:
+''
+||||
