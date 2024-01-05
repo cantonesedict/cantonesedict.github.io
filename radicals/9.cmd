9ab9c7e9a51305ce7c1210225da58623c2a1b401
Conway <yawnoc@pm.me>
2024-01-05 21:48:01 +0800

    Add /radicals/9

---
+++ radicals/9.cmd
@@ -0,0 +1,138 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 9-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 9-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⾯]
+- [⾰]
+- [⾱]
+- [⾲]
+- [⾳]
+- [⾴]
+- [⾵]
+- [⾶]
+- [⾷]
+- [⾸]
+- [⾹]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⾯ (face)
+
+<## radical-⾯-characters ##>
+<## /radical-⾯-characters ##>
+
+
+## ⾰ (leather)
+
+<## radical-⾰-characters ##>
+<## /radical-⾰-characters ##>
+
+
+## ⾱ (tanned leather)
+
+<## radical-⾱-characters ##>
+<## /radical-⾱-characters ##>
+
+
+## ⾲ (leek)
+
+<## radical-⾲-characters ##>
+<## /radical-⾲-characters ##>
+
+
+## ⾳ (sound)
+
+<## radical-⾳-characters ##>
+<## /radical-⾳-characters ##>
+
+
+## ⾴ (leaf)
+
+<## radical-⾴-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $頞aat3
+      ==
+      </nav>
+''
+||||
+<## /radical-⾴-characters ##>
+
+
+## ⾵ (wind)
+
+<## radical-⾵-characters ##>
+<## /radical-⾵-characters ##>
+
+
+## ⾶ (fly)
+
+<## radical-⾶-characters ##>
+<## /radical-⾶-characters ##>
+
+
+## ⾷ (eat)
+
+<## radical-⾷-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 17
+    ,
+      <nav class="sideways">
+      ==
+      - $饞caam4
+      ==
+      </nav>
+''
+||||
+<## /radical-⾷-characters ##>
+
+
+## ⾸ (head)
+
+<## radical-⾸-characters ##>
+<## /radical-⾸-characters ##>
+
+
+## ⾹ (fragrant)
+
+<## radical-⾹-characters ##>
+<## /radical-⾹-characters ##>
