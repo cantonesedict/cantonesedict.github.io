64397c1b84825ac2294e27f9b7b8581c9bd249a5
Conway <yawnoc@pm.me>
2024-01-05 22:01:35 +0800

    Add /radicals/16

---
+++ radicals/16.cmd
@@ -0,0 +1,41 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 16-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 16-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⿓]
+- [⿔]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⿓ (dragon)
+
+<## radical-⿓-characters ##>
+<## /radical-⿓-characters ##>
+
+
+## ⿔ (turtle)
+
+<## radical-⿔-characters ##>
+<## /radical-⿔-characters ##>
