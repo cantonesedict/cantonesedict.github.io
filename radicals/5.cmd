1f15ad20172b5cf248111114fb7bd3c66e4adeb4
Conway <yawnoc@pm.me>
2024-01-05 21:31:48 +0800

    Add /radicals/5

---
+++ radicals/5.cmd
@@ -0,0 +1,531 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 5-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 5-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⽞]
+- [⽟]
+- [⽠]
+- [⽡]
+- [⽢]
+- [⽣]
+- [⽤]
+- [⽥]
+- [⽦]
+- [⽧]
+- [⽨]
+- [⽩]
+- [⽪]
+- [⽫]
+- [⽬]
+- [⽭]
+- [⽮]
+- [⽯]
+- [⽰]
+- [⽱]
+- [⽲]
+- [⽳]
+- [⽴]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⽞ (profound)
+
+<## radical-⽞-characters ##>
+<## /radical-⽞-characters ##>
+
+
+## ⽟ (jade)
+
+<## radical-⽟-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $珍zan1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽟-characters ##>
+
+
+## ⽠ (melon)
+
+<## radical-⽠-characters ##>
+<## /radical-⽠-characters ##>
+
+
+## ⽡ (tile)
+
+<## radical-⽡-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 9
+    ,
+      <nav class="sideways">
+      ==
+      - $甄zan1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $甌au1
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $甖aang1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽡-characters ##>
+
+
+## ⽢ (sweet)
+
+<## radical-⽢-characters ##>
+<## /radical-⽢-characters ##>
+
+
+## ⽣ (life)
+
+<## radical-⽣-characters ##>
+<## /radical-⽣-characters ##>
+
+
+## ⽤ (use)
+
+<## radical-⽤-characters ##>
+<## /radical-⽤-characters ##>
+
+
+## ⽥ (field)
+
+<## radical-⽥-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $畛can2
+      ==
+      </nav>
+''
+||||
+<## /radical-⽥-characters ##>
+
+
+## ⽦ (bolt of cloth)
+
+<## radical-⽦-characters ##>
+<## /radical-⽦-characters ##>
+
+
+## ⽧ (sickness)
+
+<## radical-⽧-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $疢can2
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $疹can2
+      - $痄zaa2
+      - $痄zaa3
+      ==
+      </nav>
+  //
+    , 7
+    ,
+      <nav class="sideways">
+      ==
+      - $㾘ang2
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $瘂aa2
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $瘵caai3
+      - $瘵zaai3
+      ==
+      </nav>
+''
+||||
+<## /radical-⽧-characters ##>
+
+
+## ⽨ (dotted tent)
+
+<## radical-⽨-characters ##>
+<## /radical-⽨-characters ##>
+
+
+## ⽩ (white)
+
+<## radical-⽩-characters ##>
+<## /radical-⽩-characters ##>
+
+
+## ⽪ (skin)
+
+<## radical-⽪-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $皻zaa1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽪-characters ##>
+
+
+## ⽫ (dish)
+
+<## radical-⽫-characters ##>
+<## /radical-⽫-characters ##>
+
+
+## ⽬ (eye)
+
+<## radical-⽬-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $眨zaam2
+      - $眞zan1
+      - $真zan1
+      ==
+      </nav>
+  //
+    , 10
+    ,
+      <nav class="sideways">
+      ==
+      - $瞋can1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $䁪zaam2
+      ==
+      </nav>
+''
+||||
+<## /radical-⽬-characters ##>
+
+
+## ⽭ (spear)
+
+<## radical-⽭-characters ##>
+<## /radical-⽭-characters ##>
+
+
+## ⽮ (arrow)
+
+<## radical-⽮-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $矧can2
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $矮ai2
+      ==
+      </nav>
+''
+||||
+<## /radical-⽮-characters ##>
+
+
+## ⽯ (stone)
+
+<## radical-⽯-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 3
+    ,
+      <nav class="sideways">
+      ==
+      - $矺zaak3
+      - $矺zaak6
+      ==
+      </nav>
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $砌cai3
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $砦zaai6
+      - $砸zaak3
+      - $砧zam1
+      ==
+      </nav>
+  //
+    , 9
+    ,
+      <nav class="sideways">
+      ==
+      - $碪zam1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $𥕌 (⿰石斬)caam4
+      - $磧zaak3
+      ==
+      </nav>
+''
+||||
+<## /radical-⽯-characters ##>
+
+
+## ⽰ (spirit)
+
+<## radical-⽰-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $祭zai3
+      ==
+      </nav>
+  //
+    , 7
+    ,
+      <nav class="sideways">
+      ==
+      - $祲cam1
+      - $祲zam1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽰-characters ##>
+
+
+## ⽱ (track)
+
+<## radical-⽱-characters ##>
+<## /radical-⽱-characters ##>
+
+
+## ⽲ (grain)
+
+<## radical-⽲-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $秸aat3
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $穄zai3
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $穧zai1
+      - $穧zai6
+      ==
+      </nav>
+''
+||||
+<## /radical-⽲-characters ##>
+
+
+## ⽳ (cave)
+
+<## radical-⽳-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $窄zaak3
+      ==
+      </nav>
+''
+||||
+<## /radical-⽳-characters ##>
+
+
+## ⽴ (stand)
+
+<## radical-⽴-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $站zaam6
+      ==
+      </nav>
+''
+||||
+<## /radical-⽴-characters ##>
