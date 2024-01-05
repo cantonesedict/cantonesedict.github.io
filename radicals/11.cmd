f7bcc3f6091d50d9be17270dfe72300551d3ff7b
Conway <yawnoc@pm.me>
2024-01-05 21:52:54 +0800

    Add /radicals/11

---
+++ radicals/11.cmd
@@ -0,0 +1,181 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 11-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 11-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⿂]
+- [⿃]
+- [⿄]
+- [⿅]
+- [⿆]
+- [⿇]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⿂ (fish)
+
+<## radical-⿂-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $魫cam2
+      - $魫cam4
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $鮆cai5
+      - $鮓zaa2
+      ==
+      </nav>
+  //
+    , 7
+    ,
+      <nav class="sideways">
+      ==
+      - $鯽zak1
+      ==
+      </nav>
+  //
+    , 9
+    ,
+      <nav class="sideways">
+      ==
+      - $鰂caak6
+      - $鰂zak1
+      ==
+      </nav>
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $鱏cam4
+      - $鱘cam4
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $鱭cai5
+      ==
+      </nav>
+''
+||||
+<## /radical-⿂-characters ##>
+
+
+## ⿃ (bird)
+
+<## radical-⿃-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $鴉aa1
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $鴨aap3
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $鵪am1
+      ==
+      </nav>
+  //
+    , 10
+    ,
+      <nav class="sideways">
+      ==
+      - $鷃aan3
+      - $鶯ang1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $䳺am1
+      - $鷗au1
+      ==
+      </nav>
+''
+||||
+<## /radical-⿃-characters ##>
+
+
+## ⿄ (salt)
+
+<## radical-⿄-characters ##>
+<## /radical-⿄-characters ##>
+
+
+## ⿅ (deer)
+
+<## radical-⿅-characters ##>
+<## /radical-⿅-characters ##>
+
+
+## ⿆ (wheat)
+
+<## radical-⿆-characters ##>
+<## /radical-⿆-characters ##>
+
+
+## ⿇ (hemp)
+
+<## radical-⿇-characters ##>
+<## /radical-⿇-characters ##>
