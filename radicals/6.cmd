ac7e1864c4673bd55a70ff4a05de1577e480a687
Conway <yawnoc@pm.me>
2024-01-05 21:35:59 +0800

    Add /radicals/6

---
+++ radicals/6.cmd
@@ -0,0 +1,560 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> Index of characters with 6-stroke radicals
+* %date-modified --> 2024-01-05
+* %copyright-prior-years -->
+* %meta-description --> index of characters with 6-stroke radicals
+
+OrdinaryDictionaryReplacement: #.no-black-serif-note
+- queue_position: AFTER #.boilerplate.footer
+* %black-serif-note -->
+
+%%%
+
+#{.modern} %title
+
+<nav class="sideways">
+=={.modern}
+- [⽵]
+- [⽶]
+- [⽷]
+- [⽸]
+- [⽹]
+- [⽺]
+- [⽻]
+- [⽼]
+- [⽽]
+- [⽾]
+- [⽿]
+- [⾀]
+- [⾁]
+- [⾂]
+- [⾃]
+- [⾄]
+- [⾅]
+- [⾆]
+- [⾇]
+- [⾈]
+- [⾉]
+- [⾊]
+- [⾋]
+- [⾌]
+- [⾍]
+- [⾎]
+- [⾏]
+- [⾐]
+- [⾑]
+==
+</nav>
+
+--{.modern}
+For all radicals, see [Index of characters by radical](./).
+--
+
+
+## ⽵ (bamboo)
+
+<## radical-⽵-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $笮zaak3
+      ==
+      </nav>
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $策caak3
+      ==
+      </nav>
+  //
+    , 9
+    ,
+      <nav class="sideways">
+      ==
+      - $箴zam1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $篸caam2
+      - $簀zaai3
+      - $簀zaak3
+      - $簀zak1
+      ==
+      </nav>
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $簪zaam1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽵-characters ##>
+
+
+## ⽶ (rice)
+
+<## radical-⽶-characters ##>
+<## /radical-⽶-characters ##>
+
+
+## ⽷ (silk)
+
+<## radical-⽷-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $紾can2
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $緀cai1
+      ==
+      </nav>
+  //
+    , 10
+    ,
+      <nav class="sideways">
+      ==
+      - $縊ai3
+      - $縝can2
+      ==
+      </nav>
+''
+||||
+<## /radical-⽷-characters ##>
+
+
+## ⽸ (jar)
+
+<## radical-⽸-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 10
+    ,
+      <nav class="sideways">
+      ==
+      - $罃ang1
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $罌aang1
+      ==
+      </nav>
+''
+||||
+<## /radical-⽸-characters ##>
+
+
+## ⽹ (net)
+
+<## radical-⽹-characters ##>
+<## /radical-⽹-characters ##>
+
+
+## ⽺ (sheep)
+
+<## radical-⽺-characters ##>
+<## /radical-⽺-characters ##>
+
+
+## ⽻ (feather)
+
+<## radical-⽻-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $翟zaak6
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $翳ai3
+      ==
+      </nav>
+''
+||||
+<## /radical-⽻-characters ##>
+
+
+## ⽼ (old)
+
+<## radical-⽼-characters ##>
+<## /radical-⽼-characters ##>
+
+
+## ⽽ (and)
+
+<## radical-⽽-characters ##>
+<## /radical-⽽-characters ##>
+
+
+## ⽾ (plow)
+
+<## radical-⽾-characters ##>
+<## /radical-⽾-characters ##>
+
+
+## ⽿ (ear)
+
+<## radical-⽿-characters ##>
+<## /radical-⽿-characters ##>
+
+
+## ⾀ (brush)
+
+<## radical-⾀-characters ##>
+<## /radical-⾀-characters ##>
+
+
+## ⾁ (meat)
+
+<## radical-⾁-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $𦠉 (⿰⺼壹)ai3
+      ==
+      </nav>
+''
+||||
+<## /radical-⾁-characters ##>
+
+
+## ⾂ (minister)
+
+<## radical-⾂-characters ##>
+<## /radical-⾂-characters ##>
+
+
+## ⾃ (self)
+
+<## radical-⾃-characters ##>
+<## /radical-⾃-characters ##>
+
+
+## ⾄ (arrive)
+
+<## radical-⾄-characters ##>
+<## /radical-⾄-characters ##>
+
+
+## ⾅ (mortar)
+
+<## radical-⾅-characters ##>
+<## /radical-⾅-characters ##>
+
+
+## ⾆ (tongue)
+
+<## radical-⾆-characters ##>
+<## /radical-⾆-characters ##>
+
+
+## ⾇ (oppose)
+
+<## radical-⾇-characters ##>
+<## /radical-⾇-characters ##>
+
+
+## ⾈ (boat)
+
+<## radical-⾈-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $舴zaak3
+      ==
+      </nav>
+''
+||||
+<## /radical-⾈-characters ##>
+
+
+## ⾉ (stopping)
+
+<## radical-⾉-characters ##>
+<## /radical-⾉-characters ##>
+
+
+## ⾊ (color)
+
+<## radical-⾊-characters ##>
+<## /radical-⾊-characters ##>
+
+
+## ⾋ (grass)
+
+<## radical-⾋-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $茶caa4
+      - $茦caak3
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $菡aam2
+      - $菴am1
+      - $萋cai1
+      ==
+      </nav>
+  //
+    , 9
+    ,
+      <nav class="sideways">
+      ==
+      - $葠cam1
+      ==
+      </nav>
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $蔯can4
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $薺cai5
+      ==
+      </nav>
+  //
+    , 19
+    ,
+      <nav class="sideways">
+      ==
+      - $蘸zaam3
+      ==
+      </nav>
+''
+||||
+<## /radical-⾋-characters ##>
+
+
+## ⾌ (tiger)
+
+<## radical-⾌-characters ##>
+<## /radical-⾌-characters ##>
+
+
+## ⾍ (insect)
+
+<## radical-⾍-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $蚕caam4
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $蚱zaa3
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $蜡caa3
+      - $蜡zaa3
+      ==
+      </nav>
+  //
+    , 13
+    ,
+      <nav class="sideways">
+      ==
+      - $蠆caai3
+      - $蠈caak6
+      ==
+      </nav>
+  //
+    , 14
+    ,
+      <nav class="sideways">
+      ==
+      - $蠐cai4
+      ==
+      </nav>
+  //
+    , 15
+    ,
+      <nav class="sideways">
+      ==
+      - $𧓸 (⿰虫鄭)zaak6
+      ==
+      </nav>
+  //
+    , 18
+    ,
+      <nav class="sideways">
+      ==
+      - $蠶caam4
+      ==
+      </nav>
+''
+||||
+<## /radical-⾍-characters ##>
+
+
+## ⾎ (blood)
+
+<## radical-⾎-characters ##>
+<## /radical-⾎-characters ##>
+
+
+## ⾏ (walk enclosure)
+
+<## radical-⾏-characters ##>
+<## /radical-⾏-characters ##>
+
+
+## ⾐ (clothes)
+
+<## radical-⾐-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $袗can2
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $製zai3
+      ==
+      </nav>
+  //
+    , 16
+    ,
+      <nav class="sideways">
+      ==
+      - $襯can3
+      ==
+      </nav>
+''
+||||
+<## /radical-⾐-characters ##>
+
+
+## ⾑ (west)
+
+<## radical-⾑-characters ##>
+<## /radical-⾑-characters ##>
