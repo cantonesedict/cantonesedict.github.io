080ca924152154057405774b2f117b45c3272dec
Conway <yawnoc@pm.me>
2024-01-05 12:11:37 +0800

    Fix missing space in 碪 zam1 radical + strokes
    (cherry-picked from f5f250920da672cf5120c982106c04d9fb41d1b5)

--- entries/zam.cmd
+++ entries/zam.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zam
-* %date-modified --> 2024-01-04
+* %date-modified --> 2024-01-05
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -185,7 +185,7 @@ $$
 
 $$
 R
-  石+ 9
+  石 + 9
 U
   U+78AA
 F
