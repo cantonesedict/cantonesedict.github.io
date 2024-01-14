c005b663a3e3b3753c636dce2f51cedf78683aa3
Conway <yawnoc@pm.me>
2024-01-14 22:31:30 +0800

    Add 昝 ꜂Tsán (zaan2)

--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2024-01-13
+* %date-modified --> 2024-01-14
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -440,6 +440,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $昃zak1
+      ==
+      </nav>
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $昝zaan2
       ==
       </nav>
   //
