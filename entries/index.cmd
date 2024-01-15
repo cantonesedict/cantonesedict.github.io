bcbcaed1508eed90fcae72df262b2a09022327fe
Conway <yawnoc@pm.me>
2024-01-15 20:54:59 +0800

    Mark zaan done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2024-01-09
+* %date-modified --> 2024-01-15
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -809,7 +809,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $zaai
 - $zaak
 - $zaam
-- $zaan (work in progress)
+- $zaan
 - $zaang (work in progress)
 - $zaap (work in progress)
 - $zaat (work in progress)
--- entries/zaan.cmd
+++ entries/zaan.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ch(a/)n. Ts(a/)n. [[zaan]]
 
-<nav class="sideways">
-=={.modern}
-- [[Work in progress]]
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
