b057d3b75a2d41600c893e1d98b9a1719ac5231a
Conway <yawnoc@pm.me>
2024-01-17 22:09:57 +0800

    Add 箏 ꜀Chang (zang1)

--- entries/zang.cmd
+++ entries/zang.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zang1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -40,4 +41,44 @@ MP
   ==
   - zam = [tsɐŋ], [tɕɐŋ]
   ==
+$$
+
+
+##{#1 .williams} (1)Chang (1)Tsang [[zang1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $箏1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 箏1 | _(1)Chang_ [[zang1]]
+
+$$
+R
+  ⽵ + 8
+U
+  U+7B8F
+F
+  ==
+  - [[F2.29 筝]] 樂器十二絃
+  ==
+W
+  ====
+  - [[Page~20|n63 (1)箏]]
+    ==
+    - A kind of harpsichord with 12~copper strings;
+    - _(1)fung (1)chang_
+        [[fung1 zang1 風箏]],
+        glasses hung to jingle in the wind;
+    - _(2)l(a/) (1)chang_
+        [[laa2 zang1 揦箏]],
+        to thrum the guitar;
+    - _fong(3) (1)fung (1)chang_
+        [[fong3 fung1 zang1 放風箏]],
+        to fly a singing kite.
+    ==
+  ====
 $$
--- radicals/6.cmd
+++ radicals/6.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 6-stroke radicals
-* %date-modified --> 2024-01-16
+* %date-modified --> 2024-01-17
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 6-stroke radicals
 
@@ -80,6 +80,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $策caak3
+      ==
+      </nav>
+  //
+    , 8
+    ,
+      <nav class="sideways">
+      ==
+      - $箏zang1
       ==
       </nav>
   //
