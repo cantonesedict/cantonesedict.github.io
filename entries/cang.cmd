2d98e9f8b7eae39d5d69bf747760f040e93f9908
Conway <yawnoc@pm.me>
2024-01-18 22:47:45 +0800

    Add 層 ꜁Tsʻang (cang4)

--- entries/cang.cmd
+++ entries/cang.cmd
@@ -47,10 +47,58 @@ $$
 <## tone-4-characters ##>
 <nav class="sideways">
 =={.modern}
+- $層4
 - $曾4
 ==
 </nav>
 <## /tone-4-characters ##>
+
+### 層4 | _(4)Ts(')ang_ [[cang4]]
+
+$$
+R
+  ⼫ + 12
+U
+  U+5C64
+F
+  ==
+  - [[F2.31 層]] 重叠也
+  ==
+W
+  ====
+  - [[Page~557 (4)層]]
+    ==
+    - A story of a house;
+    - a layer, a strata, a tier, a stage;
+    - a lamina, a plate, or whatever is piled or laid on;|
+    - still more, added to;
+    - a step, a degree above;
+    - a classifier of stories;
+    - _k(o\) (5)k(')(u") yat(7) (4)ts(')ang_
+        [[gou1 keoi5 jat1 cang4 高佢一層]],
+        one peg higher than it;
+    - _(4)m(o\) (4)ts(')ang_
+        [[mou4 cang4 無層]],
+        no progress, it can't be done;
+    - _(1)ts(')(i/)n (4)ts(')ang (2)ch(i/)_
+        [[cin1 cang4 zi2 千層紙]],
+        foliated mica;
+    - _(5)sh(e/)ung tai(6) (1)s(a/)m (4)ts(')ang_
+        [[soeng5 dai6 saam1 cang4 上第三層]],
+        go up to the third story;
+    - _[(5)ch(')ung] yat(7) (4)ts(')ang_
+        [[cung5 jat1 cang4 重一層]],
+        one degree heavier;
+    - _(5)yau (4)ts(')ang (5)yau ts(')z'(3)_
+        [[jau5 cang4 jau5 ci3 有層有次]]
+        there are proper gradations.
+    ==
+  ====
+C
+  ==
+  - Williams' 重一層: Corrected _chung(6)_ [[zung6 去聲]] to _(5)ch(')ung_ [[cung5 上聲]].
+  ==
+$$
 
 ### 曾4 | _(4)Ts(')ang_ [[cang4]]
 
--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-01-17
+* %date-modified --> 2024-01-18
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -423,6 +423,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $層cang4
+      ==
+      </nav>
   //
     , 21
     ,
