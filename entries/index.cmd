a90b41ce516b22fffb4103b3d5c2a69946b44772
Conway <yawnoc@pm.me>
2024-01-18 22:14:19 +0800

    Mark zang done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2024-01-17
+* %date-modified --> 2024-01-18
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -818,7 +818,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $zak
 - $zam
 - $zan
-- $zang (work in progress)
+- $zang
 - $zap (work in progress)
 - $zat (work in progress)
 - $zau (work in progress)
