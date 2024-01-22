2f9664c48eaef39ce43ee8ade3bc83cde4ee96c0
Conway <yawnoc@pm.me>
2024-01-22 15:58:46 +0800

    Fix alternative form 執 for 蓻 should be modern

--- entries/zap.cmd
+++ entries/zap.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zap
-* %date-modified --> 2024-01-21
+* %date-modified --> 2024-01-22
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -208,7 +208,7 @@ F
 A
   ==
   - $𠽃 (⿰口執)1
-  - $執1
+  -{.modern} $執1
   ==
 W
   ====
