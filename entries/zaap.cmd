ea4cd26e232058d940a96b528d56e34f643895f2
Conway <yawnoc@pm.me>
2024-01-23 15:23:24 +0800

    Add 劄 Cháp꜆ (zaap3)

--- entries/zaap.cmd
+++ entries/zaap.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zaap3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,69 @@ WP
 MP
   ==
   - zaap = [tsaːp̚], [tɕaːp̚]
+  ==
+$$
+
+
+##{#3 .williams} Ch(a/)p(8) [[zaap3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $劄3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 劄3 | _Ch(a/)p(8)_ [[zaap3]]
+
+$$
+R
+  ⼑ + 12
+U
+  U+5284
+F
+  ==
+  - [[F2.66 劄]] ::帋劄又以針[刺]也又柬疏謂之劄子::
+  ==
+W
+  ====
+  - [[Page~22|n65 劄(8)]]
+    ==
+    - To prick, to puncture;
+    - to embroider;
+    - a document, a paper;
+    - a particular sort of paper;
+    - a diploma;
+    - a contract;
+    - to write out, as a list of prices;
+    - a reply from a higher officer;
+    - a remonstrance to the emperor;
+    - _(2)ch(i/) ch(a/)p(8)_
+        [[zi2 zaap3 紙劄]]
+        stationery;
+    - _ch(a/)p(8) (2)tsz'_
+        [[zaap3 zi2 劄子]]
+        a remonstrance;
+    - _k(a/)m(3) ch(a/)p(8)_
+        [[gaam3 zaap3 監劄]],
+        a diploma bought by a _kiensang_ [[監生]];
+    - _ch(a/)p(8) k(a/)(3) (4)ts(')(i/)n_
+        [[zaap3 gaa3 cin4 劄價錢]],
+        write the prices;
+    - _ch(a/)p(8) lok(9) p(o\)(6)_
+        [[zaap3 lok6 bou6-2 劄落簿]]
+        put it to account;
+    - _ch(a/)p(8) (1)t(a/)n_
+        [[zaap3 daan1 劄單]],
+        a contract for a purchase.
+    ==
+    --
+    The word _chop_, used in Canton is derived from this.
+    --
+  ====
+C
+  ==
+  - Fan Wan entry: Corrected 剌 to 刺.
   ==
 $$
--- radicals/2.cmd
+++ radicals/2.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 2-stroke radicals
-* %date-modified --> 2024-01-21
+* %date-modified --> 2024-01-23
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 2-stroke radicals
 
@@ -325,6 +325,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $剷caan2
+      ==
+      </nav>
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $劄zaap3
       ==
       </nav>
   //
