ca64dadab9e1065df2ac1ad725e854fc4a228ce1
Conway <yawnoc@pm.me>
2024-01-23 16:02:08 +0800

    Add 閘 Cháp꜇ (zaap6)

--- entries/zaap.cmd
+++ entries/zaap.cmd
@@ -22,6 +22,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <nav class="sideways">
 =={.modern}
 - [zaap3](#3)
+- [zaap6](#6)
 ==
 </nav>
 <## /tones ##>
@@ -104,5 +105,67 @@ W
 C
   ==
   - Fan Wan entry: Corrected 剌 to 刺.
+  ==
+$$
+
+
+##{#6 .williams} Ch(a/)p(9) Ts(a/)p(9) [[zaap6 陽入]]
+
+<## tone-6-characters ##>
+<nav class="sideways">
+=={.modern}
+- $閘6
+==
+</nav>
+<## /tone-6-characters ##>
+
+### 閘6 | _Ch(a/)p(9)_ [[zaap6]]
+
+$$
+R
+  ⾨ + 5
+U
+  U+9598
+F
+  ==
+  - [[F2.66 閘]] 閘門
+  ==
+W
+  ====
+  - [[Page~22|n65 閘(9)]]
+    ==
+    - A guarded gate, a barrier, a barricade;
+    - dam;
+    - a barrier like a turnstile or a stockade;
+    - a lock, a flood-gate;
+    - to shut a gate;
+    - _ch(a/)p(9) ts(a/)p(9)_
+        [[zaap6 zaap6 閘？]]
+        a guard-house at a gate;
+    - _(1)kw(a/)n ch(a/)p(9)_
+        [[gwaan1 zaap6 關閘]]
+        to shut the gate;
+        the Barrier at Macao;
+    - _(2)shui ch(a/)p(9)_
+        [[seoi2 zaap6 水閘]]
+        a waste-weir, a mill-race or sluice with gates;
+    - _ch(a/)p(9) (1)f(u/)_
+        [[zaap6 fu1 閘夫]]
+        a gatekeeper;
+    - _(1)hon ch(a/)p(9)_
+        [[hon1 zaap6 看閘]]
+        to guard the gate;
+    - _ch(a/)p(9) (4)m(u/)n_
+        [[zaap6 mun4 閘門]],
+        a street gate;
+    - _(5)sh(e/)ung ch(a/)p(9)_
+        [[soeng5 zaap6 上閘]]
+        put in the bars.
+    ==
+  ====
+C
+  ==
+  - Williams' _ch(a/)p(9) ts(a/)p(9)_ (閘？):
+    Not sure which _ts(a/)p(9)_ [[zaap6]] means "guard-house".
   ==
 $$
--- radicals/8.cmd
+++ radicals/8.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 8-stroke radicals
-* %date-modified --> 2024-01-21
+* %date-modified --> 2024-01-23
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 8-stroke radicals
 
@@ -152,6 +152,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $閘zaap6
+      ==
+      </nav>
   //
     , 8
     ,
