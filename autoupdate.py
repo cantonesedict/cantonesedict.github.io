ef2b86357b753dd31159ade124838727209510c4
Conway <yawnoc@pm.me>
2024-01-24 21:48:08 +0800

    Add check for bad Williams diphthongs

--- autoupdate.py
+++ autoupdate.py
@@ -136,6 +136,18 @@ class Updater:
             )
             sys.exit(1)
 
+        bad_williams_diphthong_runs = re.findall(
+            pattern=r'\S+ (?: \(i/\)u | u\(i/\) ) \S+',
+            string=cmd_content,
+            flags=re.IGNORECASE | re.VERBOSE,
+        )
+        if bad_williams_diphthong_runs:
+            print(
+                f'Error in `{entry_cmd_name}`: bad Williams diphthong in {bad_williams_diphthong_runs}',
+                file=sys.stderr,
+            )
+            sys.exit(1)
+
     @staticmethod
     def _check_jyutping_heuristic(entry_cmd_name, cmd_content):
         bad_jyutping_runs = [
