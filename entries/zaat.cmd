b2d9a68432d4bda2d461852190e8b3ae5b8578ee
Conway <yawnoc@pm.me>
2024-01-29 21:44:03 +0800

    Add 甴 Tsát꜇ (zaat6)

--- entries/zaat.cmd
+++ entries/zaat.cmd
@@ -50,6 +50,7 @@ $$
 <nav class="sideways">
 =={.modern}
 - $曱6
+- $甴6
 ==
 </nav>
 <## /tone-6-characters ##>
@@ -67,7 +68,7 @@ W
   ====
   - [[Page~560 曱(9)]]
     --
-    [[Alternative form of 甴. **See $甴zaat6 for the canonical entry.**]]
+    [[Alternative form of 甴 where 曱甴 is reversed. **See $甴zaat6 for the canonical entry.**]]
     --
   ====
 C
@@ -77,6 +78,45 @@ C
   ==
 S
   ==
-  - $甴zaat6 (TODO)
+  - $曱gaat6 (TODO)
+  ==
+$$
+
+###+ 甴6 | _Ts(a/)t(9)_ [[zaat6]]
+
+$$
+R
+  ⽥ + 0
+U
+  U+7534
+A
+  ==
+  - $曱6
+  ==
+V
+  ==
+  - zaat6-2 (vernacular rising-tone noun)
+  ==
+F
+  [[Not present]]
+W
+  ====
+  - [[Page~560 曱(9); here normalised to 甴]]
+    --
+    A colloquial word.
+    --
+    ==
+    - _[K(a/)t(9)] ts(a/)t(9)_
+        [[gaat6 zaat6 曱甴]]
+        a cockroach.
+    ==
+  ====
+C
+  ==
+  - Williams' 曱甴: Changed _(1)K(a/)_ [[gaa1]] to _K(a/)t(9)_ [[gaat6]].
+  ==
+S
+  ==
+  - $甴gaat6 (TODO)
   ==
 $$
