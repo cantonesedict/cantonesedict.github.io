d87322c1c057935d686ac94e5521bad3a4c276a4
Conway <yawnoc@pm.me>
2024-01-30 23:53:44 +0800

    Add 察 Chʻát꜆ (caat3)

--- entries/caat.cmd
+++ entries/caat.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [caat3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -40,4 +41,54 @@ MP
   ==
   - caat = [tsʰaːt̚], [tɕʰaːt̚]
   ==
+$$
+
+
+##{#3 .williams} Ch(')(a/)t(8) Ts(')(a/)t(8) [[caat3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $察3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 察3 | _Ch(')(a/)t(8)_ [[caat3]]
+
+$$
+R
+  ⼧ + 11
+U
+  U+5BDF
+A
+  ==
+  -{.modern} 詧
+  ==
+F
+  ==
+  - [[F2.72 察]] 審也考也省也又察察潔淨貌
+  - [[F2.72 詧]] 同上
+  ==
+W
+  ====
+  - [[Page~24|n67 察(8)]]
+    ==
+    - To examine, to judge, to search out the truth;
+    - to observe closely, to inquire into all particulars;
+    - _(4)ch(')(a/) ch(')(a/)t(8)_
+        [[caa4 caat3 查察]]
+        to scrutinize;
+    - _k(a/)m(3) ch(')(a/)t(8)_
+        [[gaam3 caat3 鑒察]]
+        beg you to look at, said by suppliants;
+    - (1)king ch(')(a/)t(8)_
+        [[ging1 caat3 京察]],
+        in the censorate;
+    - _ch(')(a/)t(8) t(u")t(9)_
+        [[caat3 dyut6 察奪]]
+        examine and decide [respecting the petition.]
+        [[these square brackets appear in the original text]]
+    ==
+  ====
 $$
--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-01-27
+* %date-modified --> 2024-01-30
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -403,6 +403,7 @@ For all radicals, see [Index of characters by radical](./).
     ,
       <nav class="sideways">
       ==
+      - $察caat3
       - $寢cam2
       - $寨zaai6
       ==
