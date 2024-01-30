855d84be12d3a032e9cf75e160d87f401996fe14
Conway <yawnoc@pm.me>
2024-01-30 22:35:38 +0800

    Fix 岔 caa1 entry missing full stop

--- entries/caa.cmd
+++ entries/caa.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caa
-* %date-modified --> 2024-01-07
+* %date-modified --> 2024-01-30
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -127,7 +127,7 @@ W
     - _(1)s(a/)m (1)ch(')(a/) l(o\)(6) (2)hau_
         [[saam1 caa1 lou6 hau2 三岔路口]],
         a trivium;
-    - [[see $汊caa1 for "branching water" sense]]
+    - [[see $汊caa1 for "branching water" sense]].
     ==
   ====
 C
