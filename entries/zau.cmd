d5010961514ca4ba4731ec1c0a5ccea459997efd
Conway <yawnoc@pm.me>
2024-02-02 22:41:33 +0800

    Add 周 ꜀Tsau (zau1)

--- entries/zau.cmd
+++ entries/zau.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zau1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,77 @@ WP
 MP
   ==
   - zau = [tsɐu], [tɕɐu]
+  ==
+$$
+
+
+##{#1 .williams} (1)Chau (1)Tsau [[zau1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $周1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 周1 | _(1)Chau_ [[zau1]]
+
+$$
+R
+  ⼝ + 5
+U
+  U+5468
+F
+  ==
+  - [[F1.49 周]] 周密周濟周備周遍又姓
+  ==
+W
+  ====
+  - [[Page~25|n68 (1)周]]
+    ==
+    - Universal, complete, entire;
+    - to supply, to provide, to assist;
+    - plenty, enough;
+    - to make a circuit, to environ;
+    - a famous dynasty, B.~C.~1122--255;
+    - secret, fine, subtle;
+    - close together, fine;
+    - a curve, a bend;
+    - to extend everywhere;
+    - honest, trustworthy;
+    - to the end, entirely;
+    - _(2)h(o\) (1)chau t(o\)(3)_
+        [[hou2 zau1 dou3 好周到]]
+        all things prepared;
+    - _(1)chau (2)ch(i/)_
+        [[zau1 zi1 周知]],
+        let all know;
+    - _h(u")(3) k(i/)n(3) (1)chau (1)kung_
+        [[heoi3 gin3 zau1 gung1 去見周公]],
+        he's gone to see Duke~Chau---asleep;
+    - _(1)chau mat(9)_
+        [[zau1 mat6 周密]]
+        numerous, close together;
+    - _(1)chau p(i/)(6)_
+        [[zau1 bei6 周備]]
+        everything ready;
+    - _(1)chau (4)wai_
+        [[zau1 wai4 周圍]],
+        everywhere, all around;
+    - _(1)chau (4)s(u")n_
+        [[zau1 syun4 周旋]]^
+        to circulate, to treat friends well;
+    - _(1)chau (4)ts(')(u")n_
+        [[zau1 cyun4 周全]],
+        to bring a thing about, to remove ill feeling, to explain;
+    - _(2)h(o\) (1)chau ch(i/)[(3)]_
+        [[hou2 zau1 zi3 好周置]],
+        it is excellent.
+    ==
+  ====
+C
+  ==
+  - Williams' 好周置: Corrected _(1)ch(i/)_ [[zi1]] to _ch(i/)(3)_ [[zi3]].
   ==
 $$
--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-01-30
+* %date-modified --> 2024-02-02
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -106,6 +106,7 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $咋zaa3
+      - $周zau1
       ==
       </nav>
   //
