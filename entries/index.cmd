7cacb196083c7a494e68137de2062f26abe78434
Conway <yawnoc@pm.me>
2024-02-17 23:06:43 +0800

    Mark zaau done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2024-02-11
+* %date-modified --> 2024-02-17
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -813,7 +813,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $zaang
 - $zaap
 - $zaat
-- $zaau (work in progress)
+- $zaau
 - $zai
 - $zak
 - $zam
--- entries/zaau.cmd
+++ entries/zaau.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ch(a/)u. [[zaau]]
 
-<nav class="sideways">
-=={.modern}
-- [[Work in progress]]
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
