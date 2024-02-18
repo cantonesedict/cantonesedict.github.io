24070f1cdac83d2c7ef665bd5031977cb47e7e56
Conway <yawnoc@pm.me>
2024-02-18 21:59:28 +0800

    Add 抄 ꜀Chʻáu (caau1)

--- entries/caau.cmd
+++ entries/caau.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [caau1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +39,57 @@ MP
   ==
   - caau = [tsʰaːu] (preferred), [tɕʰaːu]
   ==
+$$
+
+
+##{#1 .williams} (1)Ch(')(a/)u [[caau1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $抄1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 抄1 | _(1)Ch(')(a/)u_ [[caau1]]
+
+$$
+R
+  ⼿ + 4
+U
+  U+6284
+F
+  ==
+  - [[F2.41 抄]] 抄寫
+  ==
+W
+  ====
+  - [[Page~29|n72 (1)抄]]
+    ==
+    - To take, to seize;
+    - to search;
+    - to engross, to transcribe;
+    - to lade out, to spoon out;
+    - to confiscate, to escheat, to sequestrate, to resume;
+    - _(2)shau (1)ch(')(a/)u_
+        [[sau2 caau1 手抄]],
+        a MS. copy;
+    - _(1)ch(')(a/)u (1)ts(')ing (1)k(a/)_
+        [[caau1 cing1 gaa1 抄清家]],
+        all his property is taken;
+    - _chi(u/)(3) (2)p(u/)n (1)ch(')(a/)u (4)t(')ang_
+        [[ziu3 bun2 caau1 tang4 照本抄謄]],
+        copied exactly;
+    - _(1)ch(')(a/)u (2)s(e/)_
+        [[caau1 se2 抄寫]],
+        to copy;
+    - _(1)ch(')(a/)u p(a/)k(9)_
+        [[caau1 baak6 抄白]]
+        it is copied clearly;
+    - _(1)ch(')(a/)u s(a/)i(3)_
+        [[caau1 saai3 抄嗮]]
+        it is all copied.
+    ==
+  ====
 $$
