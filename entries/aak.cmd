c9ca80b0e625bd307f322a8021f3f88aaa6048fb
Conway <yawnoc@pm.me>
2024-02-25 17:50:33 +0800

    Apply automatic tone number superscript to entry pages

--- entries/aak.cmd
+++ entries/aak.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> aak
-* %date-modified --> 2024-01-07
+* %date-modified --> 2024-02-25
 * %copyright-prior-years --> 2023--
 
 %%%
