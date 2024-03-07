7c69db0ceb7b1c5331ec507409cbb21372de660c
Conway <yawnoc@pm.me>
2024-03-07 22:27:59 +0800

    Create zeng

---
+++ entries/zeng.cmd
@@ -0,0 +1,32 @@
+< /res/rules/boilerplate.cmdr
+< /res/rules/syntax.cmdr
+
+OrdinaryDictionaryReplacement: #.properties-override
+- queue_position: AFTER #.boilerplate.properties-override
+- apply_mode: SEQUENTIAL
+* %title --> zeng
+* %date-modified --> 2024-03-07
+* %copyright-prior-years -->
+
+%%%
+
+#{.williams} Tseng. [[zeng]]---_See_ [Tsing. [[zing]]^](zing)
+
+$$
+WH
+  ==
+  - Tseng. [[Page~565]]
+  ==
+WP
+  ==
+  - Tseng <span class="modern">= [tsɛːŋ]</span>
+  ==
+MP
+  ==
+  - zeng = [tsɛːŋ] ((preferred)), [tɕɛːŋ]
+  ==
+S
+  ==
+  - $zing
+  ==
+$$
