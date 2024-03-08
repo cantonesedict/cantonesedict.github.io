b75fb62cae1ef33d2fff996f3db5cc1f5185a3fb
Conway <yawnoc@pm.me>
2024-03-08 22:38:25 +0800

    Do zoek lead

--- entries/zoek.cmd
+++ entries/zoek.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zoek
-* %date-modified --> 2023-08-02
-* %copyright-prior-years -->
+* %date-modified --> 2024-03-08
+* %copyright-prior-years --> 2023--
 
 %%%
-
---{.modern}
-[[From Page~32|n75 (Ch(e/)uk) and Page~565 (Ts(e/)uk) of the original onwards.]]
---
 
 #{.williams} Ch(e/)uk. Ts(e/)uk. [[zoek]]
 
@@ -21,3 +17,49 @@ OrdinaryDictionaryReplacement: #.properties-override
 - [[Work in progress]]
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+- [zoek3](#3)
+- [zoek6](#6)
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ch(e/)uk. [[Page~32|n75]]
+  - Ts(e/)uk. [[Page~565]]
+  ==
+WP
+  ==
+  - Ch(e/)uk <span class="modern">= [tɕɛok̚]</span>
+  - Ts(e/)uk <span class="modern">= [tsɛok̚]</span>
+  ==
+MP
+  ==
+  - zoek = [tsœːk̚], [tɕœːk̚] ((preferred))
+  ==
+$$
+
+
+##{#3 .williams} Ch(e/)uk(8) Ts(e/)uk(8) [[zoek3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tone-3-characters ##>
+
+
+##{#6 .williams} <ins class="modern">Ch(e/)uk(9)</ins> Ts(e/)uk(9) [[zoek6 陽入]]
+
+<## tone-6-characters ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tone-6-characters ##>
