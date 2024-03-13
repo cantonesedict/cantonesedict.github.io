f1dbb289ef8239397fa4fce2f8e42b436f31308f
Conway <yawnoc@pm.me>
2024-03-13 21:52:54 +0800

    Do coek lead

--- entries/coek.cmd
+++ entries/coek.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> coek
-* %date-modified --> 2023-08-02
+* %date-modified --> 2024-03-13
 * %copyright-prior-years -->
 
 %%%
-
---{.modern}
-[[From Page~33|n76 (Ch(')(e/)uk) and Page~565 (Ts(')(e/)uk) of the original onwards.]]
---
 
 #{.williams} Ch(')(e/)uk. Ts(')(e/)uk. [[coek]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - [[Work in progress]]
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ch(')(e/)uk. [[Page~33|n76]]
+  - Ts(')(e/)uk. [[Page~565]]
+  ==
+WP
+  ==
+  - Ch(')(e/)uk <span class="modern">= [tɕʰɛok̚]</span>
+  - Ts(')(e/)uk <span class="modern">= [tsʰɛok̚]</span>
+  ==
+MP
+  ==
+  - coek = [tsʰœːk̚], [tɕʰœːk̚] ((preferred))
+  ==
+$$
