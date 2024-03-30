15832b2c2a2e1e199c997bafb48cfb3b016e4498
Conway <yawnoc@pm.me>
2024-03-30 22:39:22 +0800

    Add 牕 ꜀Chʻéung (coeng1)

--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2024-03-29
+* %date-modified --> 2024-03-30
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -1196,6 +1196,23 @@ For all radicals, see [Index of characters by radical](./).
 ## ⽚ (slice)
 
 <## radical-⽚-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 11
+    ,
+      <nav class="sideways">
+      ==
+      - $牕coeng1
+      ==
+      </nav>
+''
+||||
 <## /radical-⽚-characters ##>
 
 
