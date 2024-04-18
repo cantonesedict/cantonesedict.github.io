afd04ceb4ba3e6f1157b3708df3bc1cf11f8664a
Conway <yawnoc@pm.me>
2024-04-18 23:38:04 +0800

    Add 牸 Tsz'꜅ (zi6)

--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2024-04-17
+* %date-modified --> 2024-04-18
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -1378,6 +1378,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 6
+    ,
+      <nav class="sideways">
+      ==
+      - $牸zi6
+      ==
+      </nav>
   //
     , 16
     ,
