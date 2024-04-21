ef333a2248853a515cb1fb70bc6efc18be390b25
Conway <yawnoc@pm.me>
2024-04-21 23:25:13 +0800

    Mark zi done

--- entries/zi.cmd
+++ entries/zi.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ch(i/). Tsz'. [[zi]]
 
-<nav class="sideways">
-=={.modern}
-- [[Work in progress]]
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
