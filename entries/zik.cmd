d5230d3be0f45b945778f463c050f89dfa4b8f4d
Conway <yawnoc@pm.me>
2024-05-05 21:21:04 +0800

    Do zik lead

--- entries/zik.cmd
+++ entries/zik.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zik
-* %date-modified --> 2023-08-05
-* %copyright-prior-years -->
+* %date-modified --> 2024-05-05
+* %copyright-prior-years --> 2023--
 
 %%%
-
---{.modern}
-(From Page~13* & Page~713 (Chik) and Page~568 (Tsik) of the original onwards.)
---
 
 #{.williams} Chik. Tsik. [[zik]]
 
@@ -21,3 +17,20 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+$$
+WH
+  ==
+  - Chik. [[Page~13*, Page~713]]
+  - Tsik. [[Page~568]]
+  ==
+WP
+  ==
+  - Chik <span class="modern">= [tɕek̚]</span>
+  - Tsik <span class="modern">= [tsek̚]</span>
+  ==
+MP
+  ==
+  - zik = [tsek̚] ((preferred)), [tɕek̚]
+  ==
+$$
