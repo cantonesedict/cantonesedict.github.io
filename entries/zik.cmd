b29926117cd973c745e646f0dc5fa780d09c1da5
Conway <yawnoc@pm.me>
2024-05-07 23:57:52 +0800

    Copy Williams misled remarks to zek3

--- entries/zik.cmd
+++ entries/zik.cmd
@@ -234,8 +234,7 @@ for 直値殖植稙湜埴 (_chik(9)_) and 食蝕 (_shik(9)_),
 it appears that its authors have attempted to distinguish
 the vernacular rime _ek_ from the literary rime _ik_,
 notwithstanding all series appear under 第七英影應益.
-I have corrected Williams by placing 隻, 蹠跖, 炙, and 摭拓
-under $zek3.]]
+I have corrected Williams by placing 隻, 蹠跖, 炙, and 摭拓 under $zek3.]]
 --
 
 ### 値6 | _Chik(9)_ [[zik6]]
