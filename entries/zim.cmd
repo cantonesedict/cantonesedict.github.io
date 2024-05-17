d4fc838b6c86ba58fcb8adbc5338fa537c8268a6
Conway <yawnoc@pm.me>
2024-05-18 00:16:56 +0800

    Add 占 ꜀Chím (zim1)

--- entries/zim.cmd
+++ entries/zim.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zim1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,67 @@ WP
 MP
   ==
   - zim = [tsiːm] ((preferred)), [tɕiːm]
+  ==
+$$
+
+
+##{#1 .williams} (1)Ch(i/)m (1)Ts(i/)m [[zim1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $占1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 占1 | _(1)Ch(i/)m_ [[zim1]]
+
+$$
+R
+  ⼘ + 3
+U
+  U+5360
+F
+  ==
+  - [[F2.47 占]] 騐也測也候也
+  ==
+W
+  ====
+  - [[Page~15* (1)占]]
+    ==
+    - To divine, to cast lots;
+    - divination, casting lots;
+    - to observe, to wait for a verification;
+    - to look towards;
+    - _(1)ch(i/)m pat(7) (4)ling_
+        [[zim1 bat1 ling4 占不靈]],
+        a false prediction;
+    - _(1)ch(i/)m s(u")n(3) (5)h(a/)_
+        [[zim1 syun3 haa5 占算吓]],
+        cast a lot about it;
+    - _(1)ch(i/)m kw(a/)(3)_
+        [[zim1 gwaa3 占卦]]
+        to prognosticate;
+    - _(1)ch(i/)m (i/)m(6)_
+        [[zim1 jim6 占驗]]
+        a verification of lots;
+    - _(1)ch(i/)m hau(6)_
+        [[zim1 hau6 占候]]
+        to foretell seasons;
+    - _(1)ch(i/)m [shi(u/)](6)_
+        [[zim1 siu6 占兆]]
+        to see signs, to foretell by signs.
+    ==
+  ====
+C
+  ==
+  - Williams' 占兆:
+    Changed Middle Chinese _chi(u/)(6)_ (ziu6)
+    to common reading _shi(u/)(6)_ (siu6).
+  ==
+S
+  ==
+  - $占zim3 (TODO)
   ==
 $$
--- radicals/2.cmd
+++ radicals/2.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 2-stroke radicals
-* %date-modified --> 2024-05-15
+* %date-modified --> 2024-05-17
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 2-stroke radicals
 
@@ -581,6 +581,23 @@ For all radicals, see [Index of characters by radical](./).
 ## ⼘ (divination)
 
 <## radical-⼘-characters ##>
+||||{.wide}
+''{.modern}
+|^
+  //
+    ; Residual strokes
+    ; Character entry links
+|:
+  //
+    , 3
+    ,
+      <nav class="sideways">
+      ==
+      - $占zim1
+      ==
+      </nav>
+''
+||||
 <## /radical-⼘-characters ##>
 
 
