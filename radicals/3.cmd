76a4e24f7dcd759e68d799e036a360fc874ad7d3
Conway <yawnoc@pm.me>
2024-05-21 23:07:30 +0800

    Add 孅 ꜀Tsʻím (cim1)

--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-05-19
+* %date-modified --> 2024-05-21
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -515,6 +515,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $嬙coeng4
+      ==
+      </nav>
+  //
+    , 17
+    ,
+      <nav class="sideways">
+      ==
+      - $孅cim1
       ==
       </nav>
   //
