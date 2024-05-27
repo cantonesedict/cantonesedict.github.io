910e7e13840b035401d2ec2d1e0bd3830250a6f0
Conway <yawnoc@pm.me>
2024-05-27 23:58:33 +0800

    Add 廛 ꜁Chʻín (cin4)

--- entries/cin.cmd
+++ entries/cin.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [cin4](#4)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,47 @@ WP
 MP
   ==
   - cin = [tsʰiːn] ((preferred)), [tɕʰiːn]
+  ==
+$$
+
+
+##{#4 .williams} (4)Ch(')(i/)n (4)Ts(')(i/)n [[cin4 陽平]]
+
+<## tone-4-characters ##>
+<nav class="sideways">
+=={.modern}
+- $廛4
+==
+</nav>
+<## /tone-4-characters ##>
+
+### 廛4 | _(4)Ch(')(i/)n_ [[cin4]]
+
+$$
+R
+  ⼴ + 12
+U
+  U+5EDB
+F
+  ==
+  - [[F1.21 ::[廛]::]] 市宅
+  ==
+W
+  ====
+  - [[Page~17* (4)廛]]
+    ==
+    - Ground allotted a serf;
+    - a shop, a village market, a bazaar;
+    - _(4)ts(')un (4)hang (4)ch(')(i/)n sz'(3)_
+        [[ceon4 hang4 cin4 si3 巡行廛肆]]
+        to inspect the markets;
+    - _(4)ch(')(i/)n (5)sh(i/)_
+        [[cin4 si5 廛市]],
+        a market.
+    ==
+  ====
+C
+  ==
+  - Fan Wan entry: Normalised headword (with 厂 and 黑-like interior) to 廛.
   ==
 $$
--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-05-26
+* %date-modified --> 2024-05-27
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -1076,6 +1076,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $廁ci3
+      ==
+      </nav>
+  //
+    , 12
+    ,
+      <nav class="sideways">
+      ==
+      - $廛cin4
       ==
       </nav>
 ''
