4f4b22d8b591dc4b312944d56dcc28040715cf71
Conway <yawnoc@pm.me>
2024-07-14 22:04:52 +0800

    Consistentise caa variations

--- entries/caa.cmd
+++ entries/caa.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caa
-* %date-modified --> 2024-07-12
+* %date-modified --> 2024-07-14
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -436,7 +436,7 @@ U
   U+55CF
 V
   ==
-  - caa1 (expected from 《字彙》初加切)
+  - caa1 (per 《字彙》初加切)
   ==
 F
   ==
