1e0f8457b0bbc9484a31b03ac294c9cbf3da0bef
Conway <yawnoc@pm.me>
2024-07-22 22:02:20 +0800

    Do zo lead

--- entries/zo.cmd
+++ entries/zo.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zo
-* %date-modified --> 2023-08-02
-* %copyright-prior-years -->
+* %date-modified --> 2024-07-22
+* %copyright-prior-years --> 2023--
 
 %%%
-
---{.modern}
-(From Page~25* (Cho) and Page~582 (Tso) of the original onwards.)
---
 
 #{.williams} Cho. Tso. [[zo]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Cho. [[Page~25*]]
+  - Tso. [[Page~582]]
+  ==
+WP
+  ==
+  - Cho <span class="modern">= [tɕɔː]</span>
+  - Tso <span class="modern">= [tsɔː]</span>
+  ==
+MP
+  ==
+  - zo = [tsɔː], [tɕɔː] ((preferred))
+  ==
+$$
