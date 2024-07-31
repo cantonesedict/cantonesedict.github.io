a71c44f1817850f0735ed08342b46720a0b0c48f
Conway <yawnoc@pm.me>
2024-07-31 15:06:10 +0800

    Add 租 ꜀Tsò (zou1)

--- entries/zou.cmd
+++ entries/zou.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zou1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +39,62 @@ MP
   ==
   - zou = [tsou], [tɕou] ((preferred))
   ==
+$$
+
+
+##{#1 .williams} (1)Ts(o\) [[zou1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $租1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 租1 | _(1)Ts(o\)_ [[zou1]]
+
+$$
+R
+  ⽲ + 5
+U
+  U+79DF
+F
+  ==
+  - [[F2.9 租 _(1)Ts(u/)_]] 田賦也納谷納屋錢地稅俱曰租又聚也詩子所蓄租
+  ==
+W
+  ====
+  - [[Page~584 (1)租]]
+    ==
+    - Rent in kind;
+    - rental, rent;
+    - tribute, taxes;
+    - to rent;
+    - _(1)ts(o\) (4)ngan_
+        [[zou1 ngan4 租銀]],
+        the rent;
+    - _(1)shau (1)ts(o\)_
+        [[sau1 zou1 收租]],
+        to collect rents;
+    - _kot(8) (1)ts(o\)_
+        [[got3 zou1 割租]],
+        to deduct from the rent;
+    - _(1)ts(o\) yam(6)_
+        [[zou1 jam6 租賃]]
+        to lease;
+    - _(1)ts(o\) uk(7) ch(u")(6)_
+        [[zou1 uk1 zyu6 租屋住]]
+        to live in a hired honse;
+    - _sh(e/)ung(6) (4)k(')(i/) (1)ts(o\)_
+        [[soeng6 kei4 zou1 上期租]],
+        to prepay the rent;
+    - _(1)t(a/)m (1)ts(o\)_
+        [[daam1 zou1 擔租]],
+        security for the rent;
+    - _(2)h(i/) (1)ts(o\)_
+        [[hei2 zou1 起租]],
+        to raise the rent.
+    ==
+  ====
 $$
--- radicals/5.cmd
+++ radicals/5.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 5-stroke radicals
-* %date-modified --> 2024-07-29
+* %date-modified --> 2024-07-31
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 5-stroke radicals
 
@@ -849,6 +849,7 @@ For all radicals, see [Index of characters by radical](./).
       - $秤cing1
       - $秤cing3
       - $秭zi2
+      - $租zou1
       ==
       </nav>
   //
