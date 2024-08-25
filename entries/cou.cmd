5bf35e0cb0f222745990a927cd34649f2df37f5e
Conway <yawnoc@pm.me>
2024-08-25 23:30:20 +0800

    Add 操 ꜀Tsò (cou1)

--- entries/cou.cmd
+++ entries/cou.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [cou1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,66 @@ WP
 MP
   ==
   - cou = [tsʰou], [tɕʰou] ((preferred))
+  ==
+$$
+
+
+##{#1 .williams} (1)Ts(')(o\) [[cou1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $操1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 操1 | _(1)Ts(o\)_ [[cou1]]
+
+$$
+R
+  ⼿ + 13
+U
+  U+64CD
+F
+  ==
+  - [[F2.9 操 _(1)Ts(')(u/)_]] 操持
+  ==
+W
+  ====
+  - [[Page~586 (1)操]]
+    ==
+    - To take, to hold, to grasp;
+    - to manage;
+    - to exercise, to drill;
+    - expert, used to;
+    - to maintain, to restrain one's self, to moderate one's desires;
+    - a purpose, a design, self-imposed rule;
+    - consistent, principled;
+    - _(1)ts(')(o\) (2)(i/)n_ [[cou1 jin2 操演]]^
+        or
+      _h(a/)[(6)] (1)ts(')(o\)_ [[haa6 cou1 下操]],
+        to drill troops;
+    - _(2)t(')ai (1)ts(')(o\)_
+        [[tai2 cou1 睇操]],
+        to see a review;
+    - _t(a/)i(6) (1)ts(')(o\)_
+        [[daai6 cou1 大操]],
+        the triennial review;
+    - _(1)ts(')(o\) (4)ch(')(i/)_
+        [[cou1 ci4 操持]],
+        resolute, adhering to right;
+    - _(1)ts(')(o\) (1)sam_
+        [[cou1 sam1 操心]],
+        anxious, careworn.
+    ==
+  ====
+C
+  ==
+  - Williams' 下操: Corrected _(5)h(a/)_ (haa5) to _h(a/)(6)_ (haa6).
+  ==
+S
+  ==
+  - $操cou3 (TODO)
   ==
 $$
--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2024-08-18
+* %date-modified --> 2024-08-25
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -452,6 +452,7 @@ For all radicals, see [Index of characters by radical](./).
     ,
       <nav class="sideways">
       ==
+      - $操cou1
       - $撾zaa1
       - $擇zaak6
       - $擳zit1
