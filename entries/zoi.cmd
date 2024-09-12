717c4dd93d81c9bf6abb5a09290ce8568bbf9dc2
Conway <yawnoc@pm.me>
2024-09-12 22:48:20 +0800

    Add 哉 ꜀Tsoi (zoi1)

--- entries/zoi.cmd
+++ entries/zoi.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zoi1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,81 @@ WP
 MP
   ==
   - zoi = [tsɔːy], [tɕɔːy] ((preferred))
+  ==
+$$
+
+
+##{#1 .williams} (1)Tsoi [[zoi1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $哉1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 哉1 | _(1)Tsoi_ [[zoi1]]
+
+$$
+R
+  ⼝ + 6
+U
+  U+54C9
+F
+  ==
+  - [[F2.43 哉]] 語助詞又始也
+  ==
+W
+  ====
+  - [[Page~588 (1)哉]]
+    ==
+    - An interjection of praise or surprise;
+    - an interrogative particle, intimating doubt or conviction, according to the scope;
+    - a superlative;
+    - a particle placed after important words to draw attention,
+      but the regimen alters its meaning much;
+    - to begin;
+    - to burst forth, as plants in spring;
+    - _(4)k(')(i/) k(u/)(3) (4)ho (1)tsoi_
+        [[kei4 gu3 ho4 zoi1 其故何哉]],
+        on what account, pray?
+    - _(4)ho (5)yau (1)(u") (5)ngo (1)tsoi_
+        [[ho4 jau5 jyu1 ngo5 zoi1 何有於我哉]],
+        what is this to me?
+    - _(2)h(i/) (2)ho sik(7) (1)tsoi_
+        [[hei2 ho2 sik1 zoi1 豈可惜哉]],
+        would you pity him!
+    - _(4)yan (5)(u")n (4)[f(u/)] (1)tsoi_
+        [[jan4 jyun5 fu4 zoi1 仁遠乎哉]],
+        has humanity so far gone!
+    - _(1)on pat(7) (1)p(i/) (1)tsoi_
+        [[on1 bat1 bei1 zoi1 安不悲哉]],
+        how could I forbear pity?
+    - _sik(7) (1)tsoi_
+        [[sik1 zoi1 惜哉]],
+        sad indeed!
+    - _t(a/)i(6) (1)tsoi man(6)_
+        [[daai6 zoi1 man6 大哉問]]
+        ah! truly an important question;
+    - _t(a/)i(6) (1)tsoi (2)Hung (2)tsz'_
+        [[daai6 zoi1 hung2 zi2 大哉孔子]]
+        Confucius, oh, how great!
+    - _(1)(u/) (1)f(u/) (1)oi (1)tsoi_
+        [[wu1 fu1 oi1 zoi1 嗚呼哀哉]],
+        alas, sad indeed!
+    - _(5)y(e/) (1)tsoi (4)Yau [(5)]y(a/)_
+        [[je5 zoi1 jau4 jaa5 野哉由也]]^
+        what a clown is Yau!
+    - _(1)k(u/) (1)ch(i/) (1)tsoi, (1)k(u/) (1)ch(i/) (1)tsoi_
+        [[gu1 zi1 zoi1 gu1 zi1 zoi1 沽之哉沽之哉]],
+        sell it! sell it!
+    ==
+  ====
+C
+  ==
+  - Williams' 仁遠乎哉: Changed Middle Chinese _(4)(u/)_ (wu4)
+    to common reading _(4)f(u/)_ (fu4).
+  - Williams' 野哉由也: Corrected _y(a/)(6)_ (jaa6) to _(5)y(a/)_ (jaa5).
   ==
 $$
--- radicals/3.cmd
+++ radicals/3.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 3-stroke radicals
-* %date-modified --> 2024-09-10
+* %date-modified --> 2024-09-12
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 3-stroke radicals
 
@@ -135,6 +135,7 @@ For all radicals, see [Index of characters by radical](./).
       - $咨zi1
       - $呰zi2
       - $咫zi2
+      - $哉zoi1
       ==
       </nav>
   //
