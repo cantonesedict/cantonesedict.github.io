93a4ac01c92965ee9a74ec27b23baf2378266e4e
Conway <yawnoc@pm.me>
2024-09-14 01:05:22 +0800

    Mark zoi done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2024-09-12
+* %date-modified --> 2024-09-13
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -841,7 +841,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $zo
 - $zoek
 - $zoeng
-- $zoi (work in progress)
+- $zoi
 - $zok (work in progress)
 - $zong (work in progress)
 - $zou
