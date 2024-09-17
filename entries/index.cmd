f3fff74dca0b30a0dedf285d0e8e331c98926e7f
Conway <yawnoc@pm.me>
2024-09-17 22:11:18 +0800

    Mark coi done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2024-09-13
+* %date-modified --> 2024-09-17
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -155,7 +155,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $coe (work in progress)
 - $coek
 - $coeng
-- $coi (work in progress)
+- $coi
 - $cok (work in progress)
 - $cong (work in progress)
 - $cou
