8cb758a1b9a205e182e027eb711000cb13bd394f
Conway <yawnoc@pm.me>
2024-09-23 17:51:37 +0800

    Add 創 ꜀Chʻong (cong1)

--- entries/cong.cmd
+++ entries/cong.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [cong1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,62 @@ WP
 MP
   ==
   - cong = [tsʰɔːŋ], [tɕʰɔːŋ] ((preferred))
+  ==
+$$
+
+
+##{#1 .williams} (1)Ch(')ong (1)Ts(')ong [[cong1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $創1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 創1 | _(1)Ch(')ong_ [[cong1]]
+
+$$
+R
+  ⼑ + 10
+U
+  U+5275
+A
+  ==
+  - $刅1
+  -{.modern} 戧
+  ==
+V
+  ==
+  - cong3 (common)
+  ==
+F
+  ==
+  - [[F1.89 戧]] ::[傷]也::
+  ==
+W
+  ====
+  - [[Page~28* (1)創(1)刅]]
+    ==
+    - To wound, to cut and injure;
+    - to butt, to gore;
+    - a wound, a cut;
+    - wounded, gashed;
+    - _(1)shan p(i/)(6) (1)to (1)ch(')ong_
+        [[san1 bei6 do1 cong1 身被多創]]^
+        he received many wounds.
+    ==
+  ====
+C
+  ==
+  - Fan Wan entry: Corrected 楊 to 傷.
+  - Whilst the classical reading for the "wound" sense is level-tone _(1)ch(')ong_ (cong1),
+    the common reading departing-tone _ch(')ong(3)_ (cong3) is not unattested;
+    Kangxi has ::又《唐韻》初亮切瘡去聲始也造也[...]又傷也《前漢匈奴傳》人民創艾戰鬭::.
+  ==
+S
+  ==
+  - $創cong3 (TODO)
   ==
 $$
--- radicals/2.cmd
+++ radicals/2.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 2-stroke radicals
-* %date-modified --> 2024-09-18
+* %date-modified --> 2024-09-23
 * %copyright-prior-years -->
 * %meta-description --> index of characters with 2-stroke radicals
 
@@ -486,6 +486,14 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - $剪zin2
+      ==
+      </nav>
+  //
+    , 10
+    ,
+      <nav class="sideways">
+      ==
+      - $創cong1
       ==
       </nav>
   //
