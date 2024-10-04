d346e3bf25686920ca2cd0f80cc57486ea4ba895
Conway <yawnoc@pm.me>
2024-10-05 03:59:55 +0800

    Revise zap variations and canonicals

--- entries/zap.cmd
+++ entries/zap.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zap
-* %date-modified --> 2024-07-12
+* %date-modified --> 2024-10-04
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -54,7 +54,7 @@ $$
 
 ### 𠽃1 [[⿰口執]] | _Tsap(7)_ [[zap1]]
 
-$$
+$$.
 R
   ⼝ + 11
 U
