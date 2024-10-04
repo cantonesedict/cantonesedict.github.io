b2309f4c6d4437bc7e3d9983367b82c64936b8bb
Conway <yawnoc@pm.me>
2024-10-05 01:17:08 +0800

    Revise caau variations and canonicals

--- entries/caau.cmd
+++ entries/caau.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> caau
-* %date-modified --> 2024-09-22
+* %date-modified --> 2024-10-04
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -302,7 +302,7 @@ $$
 
 ### 鈔3 | _Ch(')(a/)u(3)_ [[caau3]]
 
-$$
+$$.
 R
   ⾦ + 4
 U
@@ -447,7 +447,7 @@ $$
 
 ### 窠4 | _(4)Ch(')(a/)u_ [[caau4]]
 
-$$
+$$.
 R
   ⽳ + 8
 U
