bd392884a1fcd5e7edd6351fc1d3a7991e6247fd
Conway <yawnoc@pm.me>
2024-10-05 03:16:36 +0800

    Revise ngaa variations and canonicals

--- entries/ngaa.cmd
+++ entries/ngaa.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> ngaa
-* %date-modified --> 2024-05-05
+* %date-modified --> 2024-10-04
 * %copyright-prior-years --> 2023--
 
 %%%
