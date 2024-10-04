e6d15b9aed94b93bc415978f06a7a7fcf565207e
Conway <yawnoc@pm.me>
2024-10-05 02:34:02 +0800

    Revise cip canonicals

--- entries/cip.cmd
+++ entries/cip.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> cip
-* %date-modified --> 2024-07-07
+* %date-modified --> 2024-10-04
 * %copyright-prior-years --> 2023--
 
 %%%
