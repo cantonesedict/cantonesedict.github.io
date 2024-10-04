b96e4ad7e6fc06c2f087702e480afd5d94e796ff
Conway <yawnoc@pm.me>
2024-10-05 02:57:55 +0800

    Revise coi variations and canonicals

--- entries/coi.cmd
+++ entries/coi.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> coi
-* %date-modified --> 2024-09-17
+* %date-modified --> 2024-10-05
 * %copyright-prior-years --> 2023--
 
 %%%
