47cf73fa164d7bc655dd29f65a6e806e7993eafb
Conway <yawnoc@pm.me>
2024-10-05 11:47:17 +0800

    Fix cak, zaap date modified

--- entries/zaap.cmd
+++ entries/zaap.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zaap
-* %date-modified --> 2024-09-28
+* %date-modified --> 2024-10-05
 * %copyright-prior-years --> 2023--
 
 %%%
