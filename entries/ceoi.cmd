f2bf398657d358c1dd2fe1d0cf581d43d5006efb
Conway <yawnoc@pm.me>
2024-10-09 13:20:51 +0800

    Do ceoi lead

--- entries/ceoi.cmd
+++ entries/ceoi.cmd
@@ -5,16 +5,12 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> ceoi
-* %date-modified --> 2023-08-02
-* %copyright-prior-years -->
+* %date-modified --> 2024-10-09
+* %copyright-prior-years --> 2023--
 
 %%%
 
---{.modern}
-(From Page~34* (Ch(')ui), Page~595 (Ts(')(u")), Page~597 (Ts(')ui), and Page~598 (Ts(')(u/)i) of the original onwards.)
---
-
-#{.williams} Ch(')ui. Ts(')(u"). Ts(')ui. Ts(')(u/)i. [[ceoi]]
+#{.williams} Ch(')(u") Ch(')ui. Ts(')(u"). Ts(')ui. Ts(')(u/)i. [[ceoi]]
 
 <## TODO: Prepend Ch(')(u") ##>
 
@@ -23,3 +19,37 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ch(')(u"). [[Page~32*]]
+  - Ch(')ui. [[Page~34*]]
+  - Ts(')(u"). [[Page~595]]
+  - Ts(')ui. [[Page~597]]
+  - Ts(')(u/)i. [[Page~598]]
+  ==
+WP
+  ==
+  - Ch(')(u") <span class="modern">= [tɕʰyː]</span>
+  - Ch(')ui <span class="modern">= [tɕʰui]</span>
+  - Ts(')(u") <span class="modern">= [tsʰyː]</span>
+  - Ts(')ui <span class="modern">= [tsʰui]</span>
+  - Ts(')(u/)i <span class="modern">= [tsʰuːi]</span>
+  ==
+MP
+  ==
+  - ceoi = [tsʰɵy], [tɕʰɵy] ((preferred))
+  ==
+S
+  ==
+  - $cyu
+  ==
+$$
