ebec8c0110aaf4f8ff4c836de5f036dde58c044a
Conway <yawnoc@pm.me>
2024-11-07 22:18:52 +0800

    Add 竹 Chuk꜆ (zuk1)

--- entries/zuk.cmd
+++ entries/zuk.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zuk1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -40,4 +41,56 @@ MP
   ==
   - zuk = [tsok̚], [tɕok̚] ((preferred))
   ==
+$$
+
+
+##{#1 .williams} Chuk(7) [[zuk1 高陰入]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $竹1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 竹1 | _Chuk(7)_ [[zuk1]]
+
+$$
+R
+  ⽵ + 0
+U
+  U+7AF9
+F
+  ==
+  - [[F1.63 竹]] 竹木
+  ==
+W
+  ====
+  - [[Page~34* 竹(7)]]
+    ==
+    - The bamboo;
+    - _chuk(7) (2)k(')(e/)ung_
+        [[zuk1 koeng2 竹蔃]],
+        whangees, bamboo roots;
+    - _chuk(7) p(o\)(3)_
+        [[zuk1 bou3 竹布]]
+        linen;
+    - _chuk(7) (2)sun_
+        [[zuk1 seon2 竹筍]],
+        bamboo sprouts;
+    - _chuk(7) ts(i/)t(8) y(e/)ung(6)_
+        [[zuk1 zit3 joeng6 竹節樣]]
+        ridged;
+    - _chuk(7) (4)wong_
+        [[zuk1 wong4 竹黃]],
+        tabasheer;
+    - _chuk(7) shat(9)_
+        [[zuk1 sat6 竹實]]
+        seeds of the bamboo;
+    - _chuk(7) p(')(a/)u(3)_
+        [[zuk1 paau3 竹炮]]
+        a kind of large fireworks.
+    ==
+  ====
 $$
