213b89f5d0819f3f8b96ea7d3c25b31f5fb0410b
Conway <yawnoc@pm.me>
2024-11-13 23:39:54 +0800

    Do zeon lead

--- entries/zeon.cmd
+++ entries/zeon.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zeon
-* %date-modified --> 2023-08-02
-* %copyright-prior-years -->
+* %date-modified --> 2024-11-13
+* %copyright-prior-years --> 2023--
 
 %%%
-
---{.modern}
-(From Page~36* (Chun) and Page~600 (Tsun) of the original onwards.)
---
 
 #{.williams} Chun. Tsun. [[zeon]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Chun. [[Page~36*]]
+  - Tsun. [[Page~600]]
+  ==
+WP
+  ==
+  - Chun <span class="modern">= [tɕun]</span>
+  - Tsun <span class="modern">= [tsun]</span>
+  ==
+MP
+  ==
+  - zeon = [tsɵn], [tɕɵn] ((preferred))
+  ==
+$$
