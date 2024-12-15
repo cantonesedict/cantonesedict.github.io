0472c88789664c29a2272a8f9bd3d172e9a9a78f
Conway <yawnoc@pm.me>
2024-12-15 20:38:41 +0800

    Revise Kangxi 漆

--- entries/cat.cmd
+++ entries/cat.cmd
@@ -170,9 +170,9 @@ W
   - [[Page~559 ::[漆](7)::]]
     ==
     - Varnish, lacquer;
-    - viscid or resinous juices which can be used in painting;
+    - viscid or resinous juices which can be used in painting [[Kangxi: 木汁可髤物]];
     - to paint, to varnish;
-    - black, as lacquer;
+    - black, as lacquer [[Kangxi: 物之黑者曰漆]];
     - adhesive;
     - exceedingly friendly, doating on;
     - _(4)yau ts(')at(7)_
