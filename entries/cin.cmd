a4d81d8d215b6df5a21adbbf9175b111c09d3856
Conway <yawnoc@pm.me>
2025-01-05 15:03:31 +0800

    Revise 躔

--- entries/cin.cmd
+++ entries/cin.cmd
@@ -499,7 +499,7 @@ W
   - [[Page~575 (4)前]]
     ==
     - Before, in front of, in presence of [[Kangxi: 前後之對]];
-    - in advance [[Kangxi: 又進也]];
+    - in advance [[Kangxi: 進也]];
     - previously [[Kangxi: 先也, 猶故也]];
     - formerly, anciently;
     - the former;
@@ -681,10 +681,10 @@ W
   ====
   - [[Page~18* (4)躔]]
     ==
-    - To tread;
-    - to follow in order;
-    - to turn in an orbit;
-    - a trodden path, a rut, a footstep;
+    - To tread [[Kangxi: 踐也]];
+    - to follow in order [[Kangxi: 星之躔次星所履行也]];
+    - to turn in an orbit [[Kangxi: 歷行也日運爲躔]];
+    - a trodden path [[Kangxi: 躔徑也]], a rut [[Kangxi: 行徑轍迹也]], a footstep [[Kangxi: 脚所踐處]];
     - a course, an orbit;
     - _tsik(7) (4)ch(')(i/)n_
         [[zik1 cin4 跡躔]],
