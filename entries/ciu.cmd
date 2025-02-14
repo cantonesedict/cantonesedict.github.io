483d62a08de609adcad0dc6e14c777a0ef9fd438
Conway <yawnoc@pm.me>
2025-02-15 01:44:17 +0800

    Date fix before push

--- entries/ciu.cmd
+++ entries/ciu.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> ciu
-* %date-modified --> 2025-02-15
+* %date-modified --> 2025-02-14
 * %copyright-prior-years --> 2023--
 
 %%%
