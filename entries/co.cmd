102067248d99ad9dd56ae42a94eea5da70d2250d
Conway <yawnoc@pm.me>
2025-02-16 22:48:31 +0800

    Revise 坐 again

--- entries/co.cmd
+++ entries/co.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> co
-* %date-modified --> 2025-02-10
+* %date-modified --> 2025-02-16
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -1034,7 +1034,7 @@ W
   [[Not present]]
 P
   ++
-  1. Reading variation (verb-like vernacular). See $坐zo6.
+  1. Reading variation (verb-like). See $坐zo6.
   ++
 S
   ==
--- entries/zo.cmd
+++ entries/zo.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zo
-* %date-modified --> 2025-01-20
+* %date-modified --> 2025-02-16
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -494,7 +494,7 @@ U
   U+5750
 V
   ==
-  - co5 (verb-like vernacular)
+  - co5 (verb-like)
   ==
 F
   ==
