e2e314c2c054c9c359d6d498119d733bd5b76ab7
Conway <yawnoc@pm.me>
2025-03-12 02:22:09 +0800

    Bump date to 2025-03-12 before push

--- entries/ci.cmd
+++ entries/ci.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> ci
-* %date-modified --> 2025-03-11
+* %date-modified --> 2025-03-12
 * %copyright-prior-years --> 2023--
 
 %%%
--- terms/index.cmd
+++ terms/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of Cantonese terms by Jyutping
-* %date-modified --> 2025-03-11
+* %date-modified --> 2025-03-12
 * %copyright-prior-years --> 2023--
 * %meta-description --> searchable index of Cantonese terms by Jyutping
 
