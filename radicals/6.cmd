5c7a3bd6662895515749ce58d52274a14e3b6f6c
Conway <yawnoc@pm.me>
2025-03-14 22:41:25 +0800

    Comment out 葠 cam1

--- radicals/6.cmd
+++ radicals/6.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 6-stroke radicals
-* %date-modified --> 2025-03-12
+* %date-modified --> 2025-03-14
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 6-stroke radicals
 
@@ -1083,7 +1083,6 @@ For all radicals, see [Index of characters by radical](./).
     ,
       <nav class="sideways">
       ==
-      - $葠cam1
       - $葺cap1
       - $葬zong3
       ==
