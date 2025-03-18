5a108c35ffa4d1fa4c47597181d808b91b8184ee
Conway <yawnoc@pm.me>
2025-03-18 22:05:26 +0800

    Revise 灒 again

--- entries/zaan.cmd
+++ entries/zaan.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zaan
-* %date-modified --> 2025-03-12
+* %date-modified --> 2025-03-18
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -513,6 +513,7 @@ W
 C
   ==
   - Fan Wan entry: Corrected 酒 to 洒.
+  - Williams' "stir up": Better rendered "spatter".
   ==
 $$
 
