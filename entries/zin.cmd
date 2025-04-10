8718cb02bff34b134a28269644d7013491c3c6ab
Conway <yawnoc@pm.me>
2025-04-11 00:09:15 +0800

    Revise 墊

--- entries/zin.cmd
+++ entries/zin.cmd
@@ -1197,10 +1197,10 @@ F
   [[Not present]]
 W
   [[Not present]]
-C
-  ==
-  - Alternative form of 薦. **See $薦zin3 for the canonical entry.**
-  ==
+P
+  ++
+  1. Alternative form of 薦. **See $薦zin3 for the canonical entry.**
+  ++
 S
   ==
   - $墊dim3 (TODO)
