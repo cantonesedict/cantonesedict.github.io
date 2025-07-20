2c7c1e9f92d07e2591f54bb32466942dafd4fac0
Conway <yawnoc@pm.me>
2025-07-20 15:40:16 +0800

    Fix 唧 missing Kangxi redirect indicator

--- entries/zik.cmd
+++ entries/zik.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zik
-* %date-modified --> 2025-07-08
+* %date-modified --> 2025-07-20
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -223,7 +223,7 @@ W
   ====
 P
   ++
-  1. (_onomatopoeia_) A clicking (or perhaps chirping) sound (Kangxi: 喞聲也).
+  1. (_onomatopoeia_) A clicking (or perhaps chirping) sound (Kangxi 喞: 喞聲也).
   ++
 E
   ====
