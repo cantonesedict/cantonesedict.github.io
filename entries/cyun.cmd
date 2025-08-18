723402c5431848ba742ed9ab6675979612080d88
Conway <yawnoc@pm.me>
2025-08-19 00:40:07 +0800

    Mark cyun done

--- entries/cyun.cmd
+++ entries/cyun.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ch(')(u:)n. Ts(')(u:)n. [[cyun]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2025-08-12
+* %date-modified --> 2025-08-18
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -161,7 +161,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $cuk
 - $cung (work in progress)
 - $cyu
-- $cyun (work in progress)
+- $cyun
 - $cyut (work in progress)
 ==
 </nav>
