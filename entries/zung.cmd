de4b60db3aa010cba76190d65a2b3861e6ec676e
Conway <yawnoc@pm.me>
2025-08-19 17:37:47 +0800

    Do zung lead

--- entries/zung.cmd
+++ entries/zung.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zung
-* %date-modified --> 2023-08-02
-* %copyright-prior-years -->
+* %date-modified --> 2025-08-19
+* %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~38* (Chung) and Page~606 (Tsung) of the original onwards.)
---
 
 #{.williams} Chung. Tsung. [[zung]]
 
@@ -21,3 +17,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Chung. [[Page~38*]]
+  - Tsung. [[Page~606]]
+  ==
+WP
+  ==
+  - Chung <span class="modern">= *[tɕoŋ]</span>
+  - Tsung <span class="modern">= *[tsoŋ]</span>
+  ==
+MP
+  ==
+  - zung = [tsoŋ], [tɕoŋ] ((preferred))
+  ==
+$$
