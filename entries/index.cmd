665d9734520b2b9ad3e442aab3ab52a573970397
Conway <yawnoc@pm.me>
2025-08-25 23:31:10 +0800

    Mark zung done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2025-08-18
+* %date-modified --> 2025-08-25
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -845,7 +845,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $zong
 - $zou
 - $zuk
-- $zung (work in progress)
+- $zung
 - $zyu
 - $zyun
 - $zyut (work in progress)
