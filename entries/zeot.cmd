05edcfbf42b7f46de12c4a4a9dc0f6383ac83249
Conway <yawnoc@pm.me>
2025-09-04 22:18:57 +0800

    Add 黜 ꜀Chut (zeot1)

--- entries/zeot.cmd
+++ entries/zeot.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [zeot1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -39,5 +40,71 @@ WP
 MP
   ==
   - zeot = [tsɵt̚], [tɕɵt̚] ((preferred))
+  ==
+$$
+
+
+##{#1 .williams} (1)Chut [[zeot1 高陰入]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $黜1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 黜1 | _(1)Chut_ [[zeot1]]
+
+$$
+R
+  ⿊ + 5
+U
+  U+9EDC
+A
+  ==
+  - 絀
+  ==
+V
+  ==
+  - ceot1 (per 《廣韻》丑律切)
+  - zyut3 (common)
+  ==
+F
+  ==
+  - [[F2.55 黜]] 諄入聲貶斥也
+  ==
+W
+  ====
+  - [[Page~41 黜(7)]]
+    ==
+    - ~~To blame;~~
+    - to degrade, to dismiss, to put out of office [[Kangxi: 貶下也, 退也貶也下也去也放絕也減也]];
+    - to expel;
+    - _chut(7) t(')(u/)i(3)_
+        [[zeot1 teoi3 黜退]]
+        to cashier;
+    - _s(a/)m[(3)] chut(7)_
+        [[saam3 zeot1 三黜]]
+        ~~a third reprimand~~;
+    - _yuk(9) chut(7)_
+        [[juk6 zeot1 辱黜]]
+        degraded and ashamed.
+    ==
+  ====
+C
+  ==
+  - Williams' 三黜: Corrected _(1)s(a/)m_ (saam1) to _s(a/)m(3)_ (saam3)
+    per 《論語集注》三去聲. Not a third reprimand, but thrice dismissed.
+  ==
+P
+  ++
+  1. To reject.
+  1. To abolish.
+  1. To rebuke.
+  ++
+S
+  ==
+  - $黜ceot1 (TODO)
   ==
 $$
--- radicals/12.cmd
+++ radicals/12.cmd
@@ -5,8 +5,8 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 12-stroke radicals
-* %date-modified --> 2024-05-19
-* %copyright-prior-years -->
+* %date-modified --> 2025-09-04
+* %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 12-stroke radicals
 
 OrdinaryDictionaryReplacement: #.no-black-serif-note
@@ -78,6 +78,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 5
+    ,
+      <nav class="sideways">
+      ==
+      - $黜zeot1
+      ==
+      </nav>
   //
     , 9
     ,
