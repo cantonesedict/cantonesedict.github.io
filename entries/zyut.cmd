880e48d4c5c401deb1d8d1b7294048becf098e36
Conway <yawnoc@pm.me>
2025-09-07 00:48:22 +0800

    Mark zyut done

--- entries/zyut.cmd
+++ entries/zyut.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ch(u:)t. Ts(u:)t. [[zyut]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
