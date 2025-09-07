f6fcbbe9e6d10d3e5a7d3f5dc6e8cddfd0dee9f3
Conway <yawnoc@pm.me>
2025-09-07 17:19:49 +0800

    Mark cyut done

--- entries/cyut.cmd
+++ entries/cyut.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} Ts(')(u:)t. [[cyut]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2025-09-06
+* %date-modified --> 2025-09-07
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -162,7 +162,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 - $cung
 - $cyu
 - $cyun
-- $cyut (work in progress)
+- $cyut
 ==
 </nav>
 
