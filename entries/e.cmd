10d50d3aaa0ba9371a50c8ff4820e13d2f728590
Conway <yawnoc@pm.me>
2025-09-07 23:06:22 +0800

    Do e lead

--- entries/e.cmd
+++ entries/e.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> e
-* %date-modified --> 2023-08-05
-* %copyright-prior-years -->
+* %date-modified --> 2025-09-07
+* %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~42 & Page~713 of the original onwards.)
---
 
 #{.williams} (E/). [[e]]
 
@@ -21,3 +17,29 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - (E/). [[Page~42, Page~713]]
+  ==
+WP
+  ==
+  - (E/) <span class="modern">= *[eɪ]</span>
+  ==
+MP
+  ==
+  - e = [ɛː]
+  ==
+S
+  ==
+  - $nge
+  ==
+$$
