85f178c74598437b2009481d21f4c5eb9875a9a3
Conway <yawnoc@pm.me>
2025-09-07 23:47:02 +0800

    Do nge lead

--- entries/nge.cmd
+++ entries/nge.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> nge
-* %date-modified --> 2023-08-05
-* %copyright-prior-years -->
+* %date-modified --> 2025-09-07
+* %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~724 of the original onwards.)
---
 
 #{.williams} Ng(e/). [[nge]]
 
@@ -21,3 +17,29 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Ng(e/). [[Page~724]]
+  ==
+WP
+  ==
+  - Ng(e/) <span class="modern">= *[ŋeɪ]</span>
+  ==
+MP
+  ==
+  - nge = [ŋɛː]
+  ==
+S
+  ==
+  - $e
+  ==
+$$
