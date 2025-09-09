13bdd4897c3780aa88f9d412376c9bcc090712a2
Conway <yawnoc@pm.me>
2025-09-09 21:01:27 +0800

    Do fai lead

--- entries/fai.cmd
+++ entries/fai.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fai
-* %date-modified --> 2023-08-05
-* %copyright-prior-years -->
+* %date-modified --> 2025-09-09
+* %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~43 & Page~713 of the original onwards.)
---
 
 #{.williams} Fai. [[fai]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Fai. [[Page~43, Page~713]]
+  ==
+WP
+  ==
+  - Fai <span class="modern">= *[fɐi]</span>
+  ==
+MP
+  ==
+  - fai = [fɐi]
+  ==
+$$
