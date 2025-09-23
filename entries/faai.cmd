eeeb805bac17ead36209c4996919d73510729af7
Conway <yawnoc@pm.me>
2025-09-23 23:34:14 +0800

    Mark faai done

--- entries/faai.cmd
+++ entries/faai.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} F(a/)i. [[faai]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2025-09-21
+* %date-modified --> 2025-09-23
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -230,7 +230,7 @@ See also: [Index of Cantonese terms by Jyutping](/terms/).
 <nav>
 =={.modern}
 - $faa
-- $faai (work in progress)
+- $faai
 - $faak (work in progress)
 - $faan (work in progress)
 - $faat (work in progress)
