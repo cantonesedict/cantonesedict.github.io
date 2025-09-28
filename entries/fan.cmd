86f07c50c409588ec3fdfd117e291aaeb40ab8d3
Conway <yawnoc@pm.me>
2025-09-28 19:38:21 +0800

    Do fan lead

--- entries/fan.cmd
+++ entries/fan.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fan
-* %date-modified --> 2023-08-05
-* %copyright-prior-years -->
+* %date-modified --> 2025-09-29
+* %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~45 & Page~713 of the original onwards.)
---
 
 #{.williams} Fan. [[fan]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Fan. [[Page~45, Page~713]]
+  ==
+WP
+  ==
+  - Fan <span class="modern">= *[fɐn]</span>
+  ==
+MP
+  ==
+  - fan = [fɐn]
+  ==
+$$
