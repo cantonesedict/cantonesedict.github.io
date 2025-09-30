feadb1f0565003d9f18ee12157f454458097f7ea
Conway <yawnoc@pm.me>
2025-09-30 23:29:11 +0800

    Add 熏 ꜀Fan (fan1)

--- entries/fan.cmd
+++ entries/fan.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fan
-* %date-modified --> 2025-09-29
+* %date-modified --> 2025-09-30
 * %copyright-prior-years --> "2023, "
 
 %%%
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [fan1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +39,74 @@ MP
   ==
   - fan = [fɐn]
   ==
+$$
+
+
+##{#1 .williams} (1)Fan [[fan1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways">
+=={.modern}
+- $熏1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 熏1 | _(1)Fan_ [[fan1]]
+
+$$
+R
+  ⽕ + 10
+U
+  U+718F
+A
+  ==
+  - 燻 <## TODO: $燻fan1 ##>
+  ==
+F
+  ==
+  - [[F1.80 熏]] ::烟也又[熱]氣蒸也::
+  ==
+W
+  ====
+  - [[Page~45 (1)熏]]
+    ==
+    - Vapor, exhalations, steam [[Kangxi: 火氣盛貌]];
+    - smoke rising [[Kangxi: 火煙上出也]];
+    - to smoke, to fumigate, to heat [[Kangxi: 熱也]], to parch [[Kangxi: 熏灼也]];
+    - to offend;
+    - to becloud;
+    - ~~evening;~~
+    - _(1)fan (1)fung_
+        [[fan1 fung1 熏風]],
+        warm wind [[Kangxi: 東南曰熏風]];
+    - _(1)fan tsik(9)_
+        [[fan1 zik6 熏夕]] <## TODO: $曛fan1 ##>
+        twilight;
+    - _(1)fan (2)fo (2)t(')(u/)i_
+        [[fan1 fo2 teoi2 熏火腿]],
+        to smoke hams;
+    - _(1)fan (1)fan_
+        [[fan1 fan1 熏熏]],
+        ~~uneasy, fidgetty~~;
+    - _(1)fan (1)kon_
+        [[fan1 gon1 熏乾]],
+        to dry at the fire;
+    - _(1)fan chek[(8)]_
+        [[fan1 zek3 熏炙]]
+        to cauterize.
+    ==
+  ====
+C
+  ==
+  - Fan Wan entry: Normalised {𤍠=⿱執灬} to 熱.
+  - Williams' "uneasy, fidgetty": This is a rendering of Kangxi 熏熏坐不安之貌,
+    which is one of the explanations for 《詩大雅》公尸來止熏熏
+    (not a very good one, in my opinion).
+  - Williams' 熏炙: Corrected _chek(9)_ (zek6) to _chek(8)_ (zek3).
+  ==
+P
+  ++
+  1. Amiable and cheerful (Kangxi: 熏熏和悅也).
+  ++
 $$
--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2025-09-23
+* %date-modified --> 2025-09-30
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -1685,6 +1685,7 @@ For all radicals, see [Index of characters by radical](./).
     ,
       <nav class="sideways">
       ==
+      - $熏fan1
       - {$蒸zing1, $蒸zing3}
       ==
       </nav>
