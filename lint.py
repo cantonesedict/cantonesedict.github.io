91d9a5b9bdaff736d3a2b91e310cdbcf141b1dac
Conway <yawnoc@pm.me>
2025-11-03 21:18:50 +0800

    Write lint.py logic to be done

--- lint.py
+++ lint.py
@@ -14,17 +14,28 @@ class CmdSource:
         with open(file_name, 'r', encoding='utf-8') as cmd_file:
             content = cmd_file.read()
 
+        # TODO: generic linting checks
+
         self.file_name = file_name
         self.content = content
+        self.entry_page = EntryPage(file_name, content)
 
     def __repr__(self):
         return f'CmdSource({self.file_name!r})'
 
 
-class Gatherer:
+class EntryPage:
+    def __init__(self, file_name: str, content: str):
+        if file_name.startswith('entries/'):
+            pass  # TODO: do entry extraction
+        else:
+            pass  # TODO: do nothing
+
+
+class Parser:
     def __init__(self):
         cmd_file_names = [
-            os.path.normpath(os.path.join(path, file_name))
+            os.path.normpath(os.path.join(path, file_name)).replace('\\', '/')
             for path, _, file_names in os.walk(os.curdir)
             for file_name in file_names
             if file_name.endswith('.cmd')
@@ -34,8 +45,8 @@ class Gatherer:
 
 
 def main():
-    gatherer = Gatherer()
-    print(gatherer.cmd_sources)
+    parser = Parser()
+    print(parser.cmd_sources)
 
 
 if __name__ == '__main__':
