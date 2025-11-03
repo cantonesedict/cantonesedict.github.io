d805262a7c6b1cb8e2533b4ed75d0c1e74e0fffe
Conway <yawnoc@pm.me>
2025-11-03 21:10:43 +0800

    Write gathering of CMD sources

--- lint.py
+++ lint.py
@@ -3,12 +3,39 @@
 """
 # lint.py
 
-Lint all Conway-Markdown (CMD) files, with some automatic correction.
+Lint all Conway-Markdown (CMD) source files, with some automatic correction.
 """
+
+import os
+
+
+class CmdSource:
+    def __init__(self, file_name: str):
+        with open(file_name, 'r', encoding='utf-8') as cmd_file:
+            content = cmd_file.read()
+
+        self.file_name = file_name
+        self.content = content
+
+    def __repr__(self):
+        return f'CmdSource({self.file_name!r})'
+
+
+class Gatherer:
+    def __init__(self):
+        cmd_file_names = [
+            os.path.join(path, file_name)
+            for path, _, file_names in os.walk(os.curdir)
+            for file_name in file_names
+            if file_name.endswith('.cmd')
+        ]
+
+        self.cmd_sources = [CmdSource(file_name) for file_name in sorted(cmd_file_names)]
 
 
 def main():
-    pass
+    gatherer = Gatherer()
+    print(gatherer.cmd_sources)
 
 
 if __name__ == '__main__':
