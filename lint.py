bc5c508b347b8b851fc45950b9011fa23df000af
Conway <yawnoc@pm.me>
2025-11-03 20:50:17 +0800

    Create lint.py

---
+++ lint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+# lint.py
+
+Lint all Conway-Markdown (CMD) files, with some automatic correction.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
