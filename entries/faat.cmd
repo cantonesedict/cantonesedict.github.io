2b6d47471f94ada2adf7989d8e6ca0bd38fd5cad
Conway <yawnoc@pm.me>
2025-11-11 00:42:16 +0800

    Mark faat done
    All entries under faat6 are to be normalised to fat6.

--- entries/faat.cmd
+++ entries/faat.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} F(a/)t. [[faat]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
