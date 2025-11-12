68eef5d1e04745448664b73719883d5b941453eb
Conway <yawnoc@pm.me>
2025-11-12 19:55:28 +0800

    Do fau lead

--- entries/fau.cmd
+++ entries/fau.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fau
-* %date-modified --> 2025-11-02
+* %date-modified --> 2025-11-12
 * %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~51 & Page~714 of the original onwards.)
---
 
 #{.williams} Fau. [[fau]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Fau. [[Page~51, Page~714]]
+  ==
+WP
+  ==
+  - Fau <span class="modern">= *[fɐu]</span>
+  ==
+MP
+  ==
+  - fau = [fɐu]
+  ==
+$$
