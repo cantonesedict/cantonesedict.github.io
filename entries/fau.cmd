61575df6475196febea2072283fe98d158129b95
Conway <yawnoc@pm.me>
2025-11-12 21:07:25 +0800

    Add 缶 ꜂Fau (fau2)

--- entries/fau.cmd
+++ entries/fau.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [fau2](#2)
 ==
 </nav>
 <## /tones ##>
@@ -38,4 +39,45 @@ MP
   ==
   - fau = [fɐu]
   ==
+$$
+
+
+##{#2 .williams} (2)Fau [[fau2 陰上]]
+
+<## tone-2-characters ##>
+<nav class="sideways">
+=={.modern}
+- $缶2
+==
+</nav>
+<## /tone-2-characters ##>
+
+###+ 缶2 | _(2)Fau_ [[fau2]]
+
+$$
+R
+  ⽸ + 0
+U
+  U+7F36
+F
+  ==
+  - (F1.50 缶 _(1)Fau_ (fau1))
+  ==
+W
+  ====
+  - [[Page~51 (1)缶 _(1)Fau_ (fau1); here corrected to _(2)Fau_ (fau2)]]
+    ==
+    - Earthenware in general [[Kangxi: 瓦缶之器]];
+    - a vessel used to contain wine [[Kangxi: 瓦器所以盛酒漿]];
+    - one on which time was ~~noted~~ ``beaten`` [[Kangxi: 秦人鼓之以節歌]];
+    - a jar [[Kangxi: 盎謂之缶, 缶卽盎也大腹而斂口]];
+    - _(5)ng(a/) ~~(1)fau~~ ``(2)fau``_
+        [[ngaa5 fau2 瓦缶]],
+        earthenware.
+    ==
+  ====
+P
+  ++
+  1. The 121st~radical ("jar").
+  ++
 $$
--- radicals/6.cmd
+++ radicals/6.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 6-stroke radicals
-* %date-modified --> 2025-11-11
+* %date-modified --> 2025-11-12
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 6-stroke radicals
 
@@ -497,6 +497,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 0
+    ,
+      <nav class="sideways">
+      ==
+      - $缶fau2
+      ==
+      </nav>
   //
     , 10
     ,
