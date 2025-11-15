b51e0a8cd62ba9a01ec36135f86b400cb10908ba
Conway <yawnoc@pm.me>
2025-11-15 21:40:43 +0800

    Add TODO for 覆 fuk1

--- entries/fuk.cmd
+++ entries/fuk.cmd
@@ -21,3 +21,54 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## TODO: $覆fuk1
+R
+  ⾑ + 12
+U
+  U+8986
+V
+  ==
+  - fau3 (per 《廣韻》敷救切 for "cover" sense)
+  - fau6 (per 《廣韻》扶富切 for "soldiers lying in wait" sense)
+  ==
+F
+  ==
+  - (F1.56 覆 _Fau(6)_ (fau6)) 天覆地載葢也
+  ==
+W
+  ====
+  - [[Page~52 覆(6) _Fau(6)_ (fau6); here corrected to _Fau(3)_ (fau3) and normalised to _Fuk(7)_ (fuk1)]]
+    ==
+    - To cover, to overshadow;
+    - to brood, as a hen;
+    - to overspread;
+    - _(1)t(')(i/)n ~~fau(6)~~ ``fuk(7)`` t(i/)(6) tsoi(3)_
+        [[tin1 fuk1 dei6 zoi3 天覆地載]]
+        the heaven covers and earth ~~contains~~ ``carries``;
+    - _(1)kai (2)n(a/) ~~fau(6)~~ ``fuk(7)`` (2)tsai_
+        [[gai1 naa2 fuk1 zai2 雞乸覆仔]],
+        the hen covers her chickens.
+    ==
+  ====
+C
+  ==
+  - 《廣韻》 has four headline readings:
+    ++
+    1. 敷救切 _fau(3)_ (fau3) for the "cover" sense;
+    2. 扶富切 _fau(6)_ (fau6) for the "soldiers lying in wait" sense;
+    3. 芳福切 _fuk(7)_ (fuk1) for the "overturn" sense; and
+    4. 匹北切 _p(')ak(7)_ (pak1) without explanation.
+    ++
+    The explanations for the first and third readings
+    (i.e. _fau(3)_ (fau3) and _fuk(7)_ (fuk1)) refer to each other.
+    It is unclear whether these are intended as mere cross-references
+    or as genuine alternatives for those two senses.
+  - In any case, Fan Wan _fau(6)_ (fau6) for the "cover" sense is incorrect,
+    and should be _fau(3)_ (fau3) if one insists on the departing tone.
+  - According to 段玉裁's annotations of 《說文》, the "cover" and "overturn" senses
+    have the same olden reading 芳福切 _fuk(7)_ (fuk1),
+    and distinguishing 敷救切 _fau(3)_ (fau3) for the "cover" sense
+    is a southern reading (南音).
+  ==
+##>
