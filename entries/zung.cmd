760c9db895727eb4ea3e3eaf0e889d38298b5ef6
Conway <yawnoc@pm.me>
2025-11-23 22:55:45 +0800

    Add surname sense of 宗

--- entries/zung.cmd
+++ entries/zung.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zung
-* %date-modified --> 2025-11-22
+* %date-modified --> 2025-11-23
 * %copyright-prior-years --> "2023, "
 
 %%%
@@ -255,6 +255,7 @@ C
 P
   ++
   1. An ancestor.
+  1. A surname (Kangxi: 姓).
   ++
 $$
 
