c12dc749a46da85f681f321630e3525d0a1abb8b
Conway <yawnoc@pm.me>
2025-11-23 23:57:39 +0800

    Add surname sense of 晏

--- entries/aan.cmd
+++ entries/aan.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> aan
-* %date-modified --> 2025-11-16
+* %date-modified --> 2025-11-23
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -109,6 +109,10 @@ C
   - Williams' "tiffin": British for "lunch".
   - Williams' 上晏: Now rarely heard; 上晝 is more common.
   ==
+P
+  ++
+  1. A surname (Kangxi: 姓).
+  ++
 E
   ====
   - 【晏】 (aan3):
