29b7fd662b2acc3154be992b6ca8c316f2f0e5b3
Conway <yawnoc@pm.me>
2025-11-24 21:15:11 +0800

    Add 捹 Fing꜅ (fing6)

--- entries/fing.cmd
+++ entries/fing.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [fing6](#6)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,44 @@ WP
 MP
   ==
   - fing = [feŋ]
+  ==
+$$
+
+
+##{#6 .williams} Fing(6) [[fing6 陽去]]
+
+<## tone-6-characters ##>
+<nav class="sideways">
+=={.modern}
+- $捹6
+==
+</nav>
+<## /tone-6-characters ##>
+
+### 捹6 | _Fing(6)_ [[fing6]]
+
+$$.
+R
+  ⼿ + 8
+U
+  U+6379
+H
+  May appear as either ⿰扌⿱大卉 (+8 assumed here) or ⿰扌⿱大𠦄 (+9),
+  depending on the font.
+F
+  [[Not present]]
+W
+  ====
+  - [[Page~53 捹(6)]]
+    --
+    [[Alternative form of 揈. **See $揈fing6 for the canonical entry.**]] [[TODO]]
+    --
+  ====
+S
+  ==
+  - $捹ban3 (TODO) <## alternative form of 擯 (to braid) alongside 鬢 ##>
+  - $捹bun6 (TODO) <## authorised sense corrected from Williams ban3 ##>
+  - $捹fing1 (TODO)
+  - $捹fing3 (TODO)
   ==
 $$
