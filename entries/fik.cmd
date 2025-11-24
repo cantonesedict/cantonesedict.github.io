a2847e5ecbf3ac25465a6e8262502ed9a999b8ce
Conway <yawnoc@pm.me>
2025-11-24 20:43:45 +0800

    Do fik lead

--- entries/fik.cmd
+++ entries/fik.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fik
-* %date-modified --> 2025-11-02
+* %date-modified --> 2025-11-24
 * %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~53 & Page~714 of the original onwards.)
---
 
 #{.williams} Fik. [[fik]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Fik. [[Page~53, Page~714]]
+  ==
+WP
+  ==
+  - Fik <span class="modern">= *[fɪk̚]</span>
+  ==
+MP
+  ==
+  - fik = [fek̚]
+  ==
+$$
