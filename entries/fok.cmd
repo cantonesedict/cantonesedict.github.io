93cc348b9047b1861e98239c90a31b8d716e9bef
Conway <yawnoc@pm.me>
2025-12-08 00:05:27 +0800

    Add 躩 Fok꜆ (fok3)

--- entries/fok.cmd
+++ entries/fok.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [fok3](#3)
 ==
 </nav>
 <## /tones ##>
@@ -42,3 +43,52 @@ $$
 
 <## TODO: $擴kwok3 per 《集韻》闊鑊切 `06047209.cn_0025.png`, kwong3 common, gwok3 ##>
 <## TODO: $廓kwok3, gwok3 uncommon ##>
+
+
+##{#3 .williams} Fok(8) [[fok3 低陰入]]
+
+<## tone-3-characters ##>
+<nav class="sideways">
+=={.modern}
+- $躩3
+==
+</nav>
+<## /tone-3-characters ##>
+
+### 躩3 | _Fok(8)_ [[fok3]]
+
+$$
+R
+  ⾜ + 20
+U
+  U+8EA9
+V
+  ==
+  - gwok3 (per 《廣韻》居縛切)
+  - kwok3 (per 《廣韻》丘縛切)
+  ==
+F
+  ==
+  - (F1.103 躩) 盤辟貌
+  ==
+W
+  ====
+  - [[Page~54 躩(8)]]
+    ==
+    - A short, quick, stately step,
+      as if ~~going up stairs~~ ``turning on the spot``,
+      deemed respectful to superiors [[Kangxi: 足盤辟而爲敬也]];
+    - _fok(8) ~~p(o/)(6)~~ ``p(o\)(6)``_
+        [[fok3 bou6 躩步]]
+        a quick, regular walk [[Kangxi: 躩步疾行]].
+    ==
+  ====
+C
+  ==
+  - Williams' 躩步: Corrected _p(o/)(6)_ to _p(o\)(6)_.
+  ==
+P
+  ++
+  1. To jump (Kangxi: 躩跳也).
+  ++
+$$
--- radicals/7.cmd
+++ radicals/7.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 7-stroke radicals
-* %date-modified --> 2025-12-04
+* %date-modified --> 2025-12-07
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 7-stroke radicals
 
@@ -828,6 +828,14 @@ For all radicals, see [Index of characters by radical](./).
       - $躔cin4
       - $躑zaak6
       - $躓zi3
+      ==
+      </nav>
+  //
+    , 20
+    ,
+      <nav class="sideways">
+      ==
+      - $躩fok3
       ==
       </nav>
 ''
