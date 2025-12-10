71138e813778cd54e6720828746bc568e8394089
Conway <yawnoc@pm.me>
2025-12-10 22:23:17 +0800

    Do fong lead

--- entries/fong.cmd
+++ entries/fong.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fong
-* %date-modified --> 2025-11-02
+* %date-modified --> 2025-12-10
 * %copyright-prior-years --> "2023, "
 
 %%%
-
---{.modern}
-(From Page~55 & Page~714 of the original onwards.)
---
 
 #{.williams} Fong. [[fong]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Fong. [[Page~55, Page~714]]
+  ==
+WP
+  ==
+  - Fong <span class="modern">= *[fɔːŋ]</span>
+  ==
+MP
+  ==
+  - fong = [fɔːŋ]
+  ==
+$$
