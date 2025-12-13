1f38ee2bed95e845a8f67685b66c69f2eb5b0315
Conway <yawnoc@pm.me>
2025-12-13 23:47:21 +0800

    Add Baxter transcription to literary translation

--- entries/cat.cmd
+++ entries/cat.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> cat
-* %date-modified --> 2025-11-16
+* %date-modified --> 2025-12-13
 * %copyright-prior-years --> 2023--
 
 %%%
@@ -121,7 +121,7 @@ C
   ==
 L
   ====
-  * 七
+  * 七 (`tshit`)
     ==
     - (_noun-like_) seven
     - (_adjective-like_) seventh
