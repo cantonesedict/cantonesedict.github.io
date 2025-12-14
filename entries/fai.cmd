a453b03b22eb7baef80d17d8af0add59a97d0235
Conway <yawnoc@pm.me>
2025-12-14 17:56:09 +0800

    Remove trailing colon from Cantonese entries (HTML)

--- entries/fai.cmd
+++ entries/fai.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fai
-* %date-modified --> 2025-12-13
+* %date-modified --> 2025-12-14
 * %copyright-prior-years --> "2023, "
 
 %%%
--- entries/zou.cmd
+++ entries/zou.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zou
-* %date-modified --> 2025-11-23
+* %date-modified --> 2025-12-14
 * %copyright-prior-years --> 2023--
 
 %%%
