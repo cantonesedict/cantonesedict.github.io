4dd6bd70676e8f094258b762f4a11dbbe034d3f7
Conway <yawnoc@pm.me>
2025-12-16 23:03:42 +0800

    Fix au date modified

--- entries/au.cmd
+++ entries/au.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> au
-* %date-modified --> 2025-12-14
+* %date-modified --> 2025-12-16
 * %copyright-prior-years --> 2023--
 
 %%%
