c09748b34a0cfe9426d8954777f2a3eafb54da3f
Conway <yawnoc@pm.me>
2025-12-19 00:05:21 +0800

    Allow ⼽ + 9 for 賊 (per 从戈則聲)

--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2025-12-10
+* %date-modified --> 2025-12-18
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -278,6 +278,7 @@ For all radicals, see [Index of characters by radical](./).
     ,
       <nav class="sideways">
       ==
+      - $賊caak6
       - $戢cap1
       ==
       </nav>
