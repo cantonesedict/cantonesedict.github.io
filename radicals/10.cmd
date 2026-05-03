b8a49cd5e01792a911b3aac426a1a850ed69904a
Conway <yawnoc@pm.me>
2026-05-03 16:16:44 +0800

    Add 髣 ꜂Fong (fong2)

--- radicals/10.cmd
+++ radicals/10.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 10-stroke radicals
-* %date-modified --> 2026-03-09
+* %date-modified --> 2026-05-03
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 10-stroke radicals
 
@@ -168,6 +168,14 @@ For all radicals, see [Index of characters by radical](./).
     ; Residual strokes
     ; Character entry links
 |:
+  //
+    , 4
+    ,
+      <nav class="sideways">
+      ==
+      - $髣fong2
+      ==
+      </nav>
   //
     , 5
     ,
