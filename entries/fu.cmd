beeefa0847faf83d855a1651d28e6d347d2da804
Conway <yawnoc@pm.me>
2026-05-16 11:43:29 +0800

    Do fu lead

--- entries/fu.cmd
+++ entries/fu.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fu
-* %date-modified --> 2026-02-18
+* %date-modified --> 2026-05-16
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
-
---{.modern}
-(From Page~57 & Page~715 of the original onwards.)
---
 
 #{.williams} F(u/). [[fu]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - F(u/). [[Page~57, Page~715]]
+  ==
+WP
+  ==
+  - F(u/) <span class="modern">= *[uː]</span>
+  ==
+MP
+  ==
+  - fu = [uː]
+  ==
+$$
