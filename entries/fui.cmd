5cf0f4171e633ea82973ce70487130ef8690a121
Conway <yawnoc@pm.me>
2026-06-20 21:59:09 +0800

    Do fui lead

--- entries/fui.cmd
+++ entries/fui.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fui
-* %date-modified --> 2026-02-18
+* %date-modified --> 2026-06-20
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
-
---{.modern}
-(From Page~62 & Page~715 of the original onwards.)
---
 
 #{.williams} F(u/)i. [[fui]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - F(u/)i. [[Page~62, Page~715]]
+  ==
+WP
+  ==
+  - F(u/)i <span class="modern">= *[fuːɪ]</span>
+  ==
+MP
+  ==
+  - fui = [fuːy]
+  ==
+$$
