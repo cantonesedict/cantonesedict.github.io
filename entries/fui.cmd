c7daa5d695be8ffacdddfc561d82c3137bbf4562
Conway <yawnoc@pm.me>
2026-06-27 23:50:40 +0800

    Mark fui done

--- entries/fui.cmd
+++ entries/fui.cmd
@@ -12,12 +12,6 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 #{.williams} F(u/)i. [[fui]]
 
-<nav class="sideways">
-=={.modern}
-- (Work in progress)
-==
-</nav>
-
 <## tones ##>
 <nav class="sideways">
 =={.modern}
--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2026-06-19
+* %date-modified --> 2026-06-27
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -243,7 +243,7 @@ OrdinaryDictionaryReplacement: #.no-black-serif-note
 - $fok
 - $fong
 - $fu
-- $fui (work in progress)
+- $fui
 - $fuk (work in progress)
 - $fun (work in progress)
 - $fung (work in progress)
