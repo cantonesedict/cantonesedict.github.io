b1e043ebe59b61b13337772a526a2e6a3b276682
Conway <yawnoc@pm.me>
2026-07-09 23:58:36 +0800

    Mark fun done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2026-07-04
+* %date-modified --> 2026-07-09
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -245,7 +245,7 @@ OrdinaryDictionaryReplacement: #.no-black-serif-note
 - $fu
 - $fui
 - $fuk
-- $fun (work in progress)
+- $fun
 - $fung (work in progress)
 - $fut (work in progress)
 ==
