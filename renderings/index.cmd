8e13057b2f37c0cdf45167587781c86af41d60d0
Conway <yawnoc@pm.me>
2026-07-11 13:11:00 +0800

    Fix renderings index not updated for 豐 phjuwng

--- renderings/index.cmd
+++ renderings/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of literary renderings by Baxter notation
-* %date-modified --> 2026-07-10
+* %date-modified --> 2026-07-11
 * %copyright-prior-years --> 2025--
 * %meta-description --> searchable index of links to literary renderings by Baxter notation
 
