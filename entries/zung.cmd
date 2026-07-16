67209c4cd8d65a1bf76a2e42f95a1f62d07188bf
Conway <yawnoc@pm.me>
2026-07-16 21:50:09 +0800

    Fix missing qualifier on 粽 zung2 redirect

--- entries/zung.cmd
+++ entries/zung.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> zung
-* %date-modified --> 2026-07-15
+* %date-modified --> 2026-07-16
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
@@ -1476,7 +1476,7 @@ W
   ====
   - [[Page~607 (2)粽(2)糉]]
     --
-    [[Reading variation. **See $粽zung3 for the canonical entry.**]]
+    [[Reading variation (vernacular rising-tone noun). **See $粽zung3 for the canonical entry.**]]
     --
   ====
 S
