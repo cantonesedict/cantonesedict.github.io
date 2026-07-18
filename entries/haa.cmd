c4681a2a54bbee6b2779e6aba5aeaa3e26cc53f3
Conway <yawnoc@pm.me>
2026-07-19 00:14:35 +0800

    Add 蝦 ꜀Há (haa1)

--- entries/haa.cmd
+++ entries/haa.cmd
@@ -21,6 +21,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 <## tones ##>
 <nav class="sideways">
 =={.modern}
+- [haa1](#1)
 ==
 </nav>
 <## /tones ##>
@@ -37,5 +38,78 @@ WP
 MP
   ==
   - haa = [haː]
+  ==
+$$
+
+
+##{#1 .williams} (1)H(a/) [[haa1 陰平]]
+
+<## tone-1-characters ##>
+<nav class="sideways characters">
+=={.modern}
+- $蝦1
+==
+</nav>
+<## /tone-1-characters ##>
+
+### 蝦1 | _(1)H(a/)_ [[haa1]]
+
+$$
+R
+  ⾍ + 9
+U
+  U+8766
+A
+  ==
+  - 鰕
+  - 虾 (Mainland simplified)
+  ==
+F
+  ==
+  - (F2.74 蝦) 魚蝦
+  - (F2.74 鰕) 同上
+  ==
+W
+  ====
+  - [[Page~67 (1)蝦(1)鰕]]
+    ==
+    - A shrimp, prawn, crawfish [[Kangxi 蝦: 與鰕通 etc.]];
+    - _(1)h(a/) (1)ch(')un_
+        [[haa1 ceon1 蝦膥]],
+        ~~crab's eggs~~ ``shrimp roe``;
+    - _(1)h(a/) (1)ch(')un ch(a/)p(9)_
+        [[haa1 ceon1 zaap6 蝦膥鈒]]
+        a skinflint;
+    - _(4)lung (1)h(a/)_
+        [[lung4 haa1 龍蝦]],
+        the sea crawfish _(Palinurus)_;
+    - _(4)ngan (1)h(a/)_
+        [[ngan4 haa1 銀蝦]],
+        sea shrimps;
+    - _(4)ming (1)h(a/)_
+        [[ming4 haa1 明蝦]],
+        large yellow prawns;
+    - _(1)h(a/) k(o\)m(3) (2)ts(a/)n_
+        [[haa1 gam3 zaan2 蝦咁趲]],
+        ~~to~~ throw``ing`` the arms about passionately;
+    - _~~(1)shang~~ ``(1)sh(a/)ng`` (1)h(a/) k(o\)m(3) t(')i(u/)(3)_
+        [[saang1 haa1 gam3 tiu3 生蝦咁跳]]
+        lively, playful.
+    ==
+    --
+    [[...]]
+    --
+    --
+    [[Otherwise, an alternative form of 蛤, and reading variation. **See $蛤haa4 for the canonical entry.**]] [[TODO]]
+    --
+  ====
+C
+  ==
+  - Williams' 生蝦咁跳: Changed _(1)shang_ (sang1)
+    to vernacular reading _(1)sh(a/)ng_ (saang1).
+  ==
+S
+  ==
+  - ($蝦haa4) (TODO)
   ==
 $$
--- radicals/6.cmd
+++ radicals/6.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 6-stroke radicals
-* %date-modified --> 2026-07-17
+* %date-modified --> 2026-07-18
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 6-stroke radicals
 
@@ -1600,6 +1600,7 @@ For all radicals, see [Index of characters by radical](./).
       - $蝌fo1
       - $蝠fuk1
       - $蝮fuk1
+      - $蝦haa1
       ==
       </nav>
   //
