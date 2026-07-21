34be85030734111722f3a55f0f11749faa157d67
Conway <yawnoc@pm.me>
2026-07-21 20:58:38 +0800

    Add 厦 Há꜅ (haa6)

--- radicals/2.cmd
+++ radicals/2.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 2-stroke radicals
-* %date-modified --> 2026-07-17
+* %date-modified --> 2026-07-21
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 2-stroke radicals
 
@@ -992,6 +992,7 @@ For all radicals, see [Index of characters by radical](./).
       <nav class="sideways">
       ==
       - {$厨ceoi4, $厨cyu4}
+      - $厦haa6
       ==
       </nav>
   //
