14a7c38b72f582279961cb4956280fe843a069fc
Conway <yawnoc@pm.me>
2026-07-25 20:23:17 +0800

    Fix missing 'Fan Wan entry:' under 輻

--- entries/fuk.cmd
+++ entries/fuk.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fuk
-* %date-modified --> 2026-07-15
+* %date-modified --> 2026-07-25
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
@@ -664,7 +664,7 @@ W
   ====
 C
   ==
-  - Corrected malformed headword 輻.
+  - Fan Wan entry: Corrected malformed headword 輻.
   - In modern usage, used in 輻射 (radiation).
   ==
 L
