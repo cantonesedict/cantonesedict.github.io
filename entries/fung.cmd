c50c61fb5d6a797b49613e602b38f020bb55b96d
Conway <yawnoc@pm.me>
2026-07-30 21:16:00 +0800

    Fix missing simplification 沨 under 渢

--- entries/fung.cmd
+++ entries/fung.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fung
-* %date-modified --> 2026-07-17
+* %date-modified --> 2026-07-30
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
@@ -1145,6 +1145,10 @@ R
   ⽔ + 9
 U
   U+6E22
+A
+  ==
+  - 沨 (Mainland simplified)
+  ==
 V
   ==
   - faan3 (per 《集韻》孚梵切 for "neutral/even-sounding" sense)
