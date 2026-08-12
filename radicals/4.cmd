4e6e35331a7b4285c18fbf6fc0845af950b01623
Conway <yawnoc@pm.me>
2026-08-12 23:47:59 +0800

    Allow ⽊ + 12 for 築 zuk1
    Per 从木筑聲

--- radicals/4.cmd
+++ radicals/4.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of characters with 4-stroke radicals
-* %date-modified --> 2026-08-11
+* %date-modified --> 2026-08-12
 * %copyright-prior-years --> 2024--
 * %meta-description --> index of characters with 4-stroke radicals
 
@@ -1319,6 +1319,7 @@ For all radicals, see [Index of characters by radical](./).
       - $橃fat6
       - $樽zeon1
       - $橡zoeng6
+      - $築zuk1
       ==
       </nav>
   //
