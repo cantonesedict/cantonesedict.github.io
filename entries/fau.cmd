2464e57ae8d8e27844280ef66646cfdae4cf5728
Conway <yawnoc@pm.me>
2026-08-17 23:23:38 +0800

    Fix missing alternative form 𦈢 under 缶

--- entries/fau.cmd
+++ entries/fau.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> fau
-* %date-modified --> 2026-08-07
+* %date-modified --> 2026-08-17
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
@@ -195,6 +195,7 @@ U
   U+7F36
 A
   ==
+  - 𦈢
   - 缻
   ==
 F
