db1ce34d02b51fb8440e99705bff563d73635822
Conway <yawnoc@pm.me>
2026-08-19 20:57:06 +0800

    Do han lead

--- entries/han.cmd
+++ entries/han.cmd
@@ -5,14 +5,10 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> han
-* %date-modified --> 2026-02-18
+* %date-modified --> 2026-08-19
 * %copyright-prior-years --> "2023, 2025--"
 
 %%%
-
---{.modern}
-(From Page~71 of the original onwards.)
---
 
 #{.williams} Han. [[han]]
 
@@ -21,3 +17,25 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<## tones ##>
+<nav class="sideways">
+=={.modern}
+==
+</nav>
+<## /tones ##>
+
+$$
+WH
+  ==
+  - Han. [[Page~71]]
+  ==
+WP
+  ==
+  - Han <span class="modern">= *[hɐn]</span>
+  ==
+MP
+  ==
+  - han = [hɐn]
+  ==
+$$
