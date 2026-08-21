29235fa1f01837673ad1bb45c5234487f0b1d5a9
Conway <yawnoc@pm.me>
2026-08-21 23:17:57 +0800

    Mark han done

--- entries/index.cmd
+++ entries/index.cmd
@@ -5,7 +5,7 @@ OrdinaryDictionaryReplacement: #.properties-override
 - queue_position: AFTER #.boilerplate.properties-override
 - apply_mode: SEQUENTIAL
 * %title --> Index of entry pages by Jyutping
-* %date-modified --> 2026-08-18
+* %date-modified --> 2026-08-21
 * %copyright-prior-years --> 2023--
 * %meta-description --> index of entry pages by Jyutping
 
@@ -335,7 +335,7 @@ OrdinaryDictionaryReplacement: #.no-black-serif-note
 - $hai
 - $hak
 - $ham (work in progress)
-- $han (work in progress)
+- $han
 - $hang (work in progress)
 - $hap (work in progress)
 - $hat (work in progress)
