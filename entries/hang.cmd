c1e7e45ac0a6d36a62a06a1db2aef29ac9e46016
Conway <yawnoc@pm.me>
2026-08-21 19:35:55 +0800

    Prepare [[Page~73 (4)行]]

--- entries/hang.cmd
+++ entries/hang.cmd
@@ -93,3 +93,97 @@ W
   ====
 $$
 ##>
+
+<##
+$$
+W
+  ====
+  - [[Page~73 before (4)行]]
+    --
+    (This character [[行]] is often pronounced like the next syllable [[haang]].)
+    --
+  - [[Page~73 (4)行]]
+    ==
+    - To go, to walk, to proceed;
+    - to act, to do, to direct;
+    - imports the action of the following verb;
+    - to appeal, or transfer a case;
+    - a step;
+    - a road, a way, a path;
+    - one of the elements;
+    - a manner;
+    - motion;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` l(o\)(6)_
+        [[haang4 lou6]]
+        to walk in the way;
+    - _p(o\)(6) (4)hang_
+        [[bou6 hang4]],
+        to travel afoot;
+    - _(4)hang (4)yan_
+        [[hang4 jan4]],
+        a traveler;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (5)lai_
+        [[haang4 lai5]],
+        to visit, to attend to prescribed rites;
+    - _(4)hang (5)(u:)_
+        [[hang4 jyu5]],
+        to tell to;
+    - _(4)hang (5)l(i/)_
+        [[hang4 lei5]],
+        baggage;
+    - _(5)'ng (4)hang_
+        [[ng5 hang4]],
+        the 5~elements;
+    - _(4)hang k(a/)u(3)_
+        [[hang4 gaau3]]
+        to promulgate;
+    - _t(a/)i(6) (4)hang_
+        [[daai6 hang4]],
+        the recently deceased emperor or empress;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (4)t(')au_
+        [[haang4 tau4]],
+        head-gear, masks, &c., used by actors;
+    - _(4)hang (1)h(e/)ung_
+        [[hang4 hoeng1]],
+        to go in procession;
+    - _(4)hang (1)ts(')ing_
+        [[hang4 cing1]],
+        to worship at the tombs in spring;
+    - _(4)hang (4)ying_
+        [[hang4 jing4]],
+        to punish;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (1)ts(')(i/)n p(o\)(6)_
+        [[haang4 cin1 bou6]]
+        to 'walk a mile;'
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (4)sh(u:)n_
+        [[haang4 syun4]],
+        to weigh anchor, to follow the sea, to act as a sailor;
+    - _~~(1)hang~~ ``(4)h(a/)ng`` (4)m(a/)i tik(7)_
+        [[haang4 maai4 dik1]]
+        step aside a little!
+    - _(4)hang (2)h(i/)m_
+        [[hang4 him2]],
+        to follow dangerous pursuits;
+    - _(4)hang (1)ping_
+        [[hang4 bing1]],
+        to direct troops;
+    - _(4)hang (4)wai_
+        [[hang4 wai4]],
+        conduct, manner of acting;
+    - _(4)hang ling(6)_
+        [[hang4 ling6]]
+        to send an order;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (5)h(a/) (2)ch(e/)_
+        [[haang4 haa5 ze2]],
+        wait, or do;
+        a little presently;
+    - _(4)hang (1)sh(u:)_
+        [[hang4 syu1]],
+        to write a running hand~~,~~``;``
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (4)l(u/)i_
+        [[haang4 leoi4]],
+        to thunder.
+    ==
+  ====
+$$
+##>
