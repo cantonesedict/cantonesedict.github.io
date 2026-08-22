2e96929062a366e126da68d78c2b8acbbea0d9fc
Conway <yawnoc@pm.me>
2026-08-22 21:27:21 +0800

    Prepare [[Page~74 (1)坑]]

--- entries/haang.cmd
+++ entries/haang.cmd
@@ -21,3 +21,35 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~74 (1)坑]]
+    ==
+    - A ditch, trench, hollow, hole, cave, or pit, natural or artificial;
+    - the furrows in a roof;
+    - a pit to entrap wild beasts;
+    - to throw into a pit;
+    - to excavate;
+    - _(1)h(a/)ng (4)k(')(u:)_
+        [[haang1 keoi4]],
+        a sewer or drain;
+    - _(1)h(a/)ng (2)h(o\)m_
+        [[haang1 ham2]],
+        a natural gully, a ravine;
+    - _(2)L(o\) (1)h(a/)ng_
+        [[lou2 haang1]],
+        a quarry in Sh(a/)uking f(u/) [[]]
+        which furnishes good inkstones;
+    - _(2)shui (1)h(a/)ng (4)m(u/)n_
+        [[seoi2 haang1 mun4]],
+        a sluice;
+    - _(4)m(u/)i (1)h(a/)ng_
+        [[mui4 haang1]],
+        a coal pit.
+    ==
+  ====
+$$
+##>
