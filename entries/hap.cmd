d4e7e481dfb4c84be03b79c3bbe681962a4fd1b4
Conway <yawnoc@pm.me>
2026-08-22 21:41:20 +0800

    Prepare [[Page~74 恰(7)]]

--- entries/hap.cmd
+++ entries/hap.cmd
@@ -21,3 +21,35 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~74 恰(7)]]
+    ==
+    - To apply the mind to;
+    - seasonably, fortunately, in good time, to the purpose, fitly;
+    - exactly;
+    - _hap(7) (u:)(6)_
+        [[hap1 jyu6]]
+        luckily met;
+    - _hap(7) (2)h(o\)_
+        [[hap1 hou2]],
+        suitable, just the thing;
+    - _hap(7) (5)ts(')z'_
+        [[hap1 ci5]]
+        very much alike;
+    - _hap(7) hap(7)_
+        [[hap1 hap1]]
+        chirping of birds;
+    - _hap(7) ~~(4)ng(a/)m~~ ``(1)ng(a/)m``_
+        [[hap1 ngaam1]],
+        fits well;
+    - _hap(7) (2)hau (5)yau_
+        [[hap1 hau2 jau5]],
+        had it just in season.
+    ==
+  ====
+$$
+##>
