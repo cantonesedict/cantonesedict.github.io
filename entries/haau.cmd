1ee79707fb7210f17c5c0bc2143ff75e777187bc
Conway <yawnoc@pm.me>
2026-09-06 21:29:08 +0800

    Prepare [[Page~77 (1)𫾣敲]]

--- entries/haau.cmd
+++ entries/haau.cmd
@@ -49,3 +49,35 @@ W
   ====
 $$
 ##>
+
+<##
+$$
+W
+  ====
+  - [[Page~77 ::(1)~~{𫾣=⿰高支}~~``敲``::]]
+    ==
+    - A baton or club;
+    - to beat or tap horizontally with a club;
+    - to rap;
+    - _(1)h(a/)u puk(7) (4)(u:)_
+        [[haau1 buk1 jyu4]],
+        to rap the block when chanting;
+    - _(1)h(a/)u (4)m(u/)n_
+        [[haau1 mun4]],
+        to knock at a door;
+    - _(1)h(a/)u (1)king n(i/)m(6) Fat(9)_
+        [[haau1 ging1 nim6 fat6]]
+        to recite prayers and meditate on Budha;
+    - _(1)h(a/)u l(a/)n(6)_
+        [[haau1 laan6]]
+        to beat to pieces, or to a jelly.
+    ==
+  ====
+C
+  ==
+  - Williams misprints 敲 as {𫾣=⿰高支}; here corrected.
+    (Despite the incorrect right-hand component,
+    the headword {𫾣=⿰高支} appears on Page~768 under the radical ⽁.)
+  ==
+$$
+##>
