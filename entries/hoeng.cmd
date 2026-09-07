413c517c02461150e97bac3285c62e2fdf809614
Conway <yawnoc@pm.me>
2026-09-08 00:09:50 +0800

    Prepare [[Page~79 (2)珦]]

--- entries/hoeng.cmd
+++ entries/hoeng.cmd
@@ -275,3 +275,15 @@ W
   ====
 $$
 ##>
+
+<##
+$$
+W
+  ====
+  - [[Page~79 (2)珦 _(2)H(e/)ung_ (hoeng2); here corrected _H(e/)ung(3)_ (hoeng3)]]
+    ==
+    - An ornament worn on the girdle by women.
+    ==
+  ====
+$$
+##>
