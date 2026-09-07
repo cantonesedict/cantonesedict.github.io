442c1408ec03a6262915606858c81909072e35f2
Conway <yawnoc@pm.me>
2026-09-07 23:05:48 +0800

    Prepare [[Page~78 (1)香]]

--- entries/hoeng.cmd
+++ entries/hoeng.cmd
@@ -21,3 +21,52 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~78 (1)香]]
+    ==
+    - Fragrant, odoriferous, aromatic, sweet;
+    - incense;
+    - effluvia or aroma;
+    - reputable, renowned, a 'fragrant' name;
+    - _(1)h(e/)ung h``(i/)``(3)_
+        [[hoeng1 hei3]]
+        fragrance;
+    - _(1)h(e/)ung li(u/)(6)_
+        [[hoeng1 liu6-2]]
+        spicery;
+    - _k(e/)uk(8) (1)h(e/)ung_
+        [[goek3 hoeng1]], incense sticks;
+    - _(1)sh(u:) (1)h(e/)ung_
+        [[syu1 hoeng1]]
+        literary reputation of a family;
+    - _(1)h(e/)ung (1)ch(u:)_
+        [[hoeng1 zyu1]],
+        fragrant beads;
+    - _(1)h(e/)ung ~~(5)l(o\)~~ ``(4)l(o\)``_
+        [[hoeng1 lou4 香爐]],
+        a burner of incense in a temple;
+    - _(4)hang (1)h(e/)ung_
+        [[hang4 hoeng1]],
+        to worship;
+    - _m(u/)n(6) (1)h(e/)ung_
+        [[mun6 hoeng1]],
+        drugs burned by thieves to stupefy people;
+    - _(1)h(e/)ung sun(3)_
+        [[hoeng1 seon3]]
+        mushrooms;
+    - _(1)H(e/)ung (2)kong_
+        [[hoeng1 gong2]],
+        Hongkong.
+    ==
+  ====
+C
+  ==
+  - Williams' 香氣: Inferred incompletely printed letter _(i/)_ in _h(i/)(3)_.
+  - Williams' 香爐: Corrected _(5)l(o\)_ (lou5) to _(4)l(o\)_ (lou4).
+  ==
+$$
+##>
