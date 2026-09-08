37c32a1d7c9a4ea7c138543602965c1baff5e75c
Conway <yawnoc@pm.me>
2026-09-09 01:05:19 +0800

    Prepare [[Page~83 讞(3) etc.]]

--- entries/jin.cmd
+++ entries/jin.cmd
@@ -22,4 +22,30 @@ OrdinaryDictionaryReplacement: #.properties-override
 ==
 </nav>
 
+<##
+$$
+W
+  ====
+  - [[Page~83 讞(3) _H(i/)n(3)_ (hin3); here normalised to _(I/)n(6)_ (jin6)]]
+    ==
+    - To consult on criminal causes;
+    - to judge or decide a case;
+    - to adjudge;
+    - _~~h(i/)n(3)~~ ``(i/)n(6)`` yuk(9)_
+        [[jin6 juk6]]
+        to sentence;
+    - _~~h(i/)n(3)~~ ``(i/)n(6)`` (2)t(i/)n_
+        [[jin6 din2]],
+        a legal decision;
+    - _(1)ts(')au ~~h(i/)n(3)~~ ``(i/)n(6)``_
+        [[cau1 jin6]]
+        the autumnal assize;
+    - _(5)yau sun(3) ~~h(i/)n(3)~~ ``(i/)n(6)``_
+        [[jau5 seon3 jin6]]
+        a good decision, a true judgment.
+    ==
+  ====
+$$
+##>
+
 <## TODO: 言 jin4-2 (vernacular rising-tone noun in Cantonese opera senses) ##>
