5d093327e2a7f4bea6899a2f947b605aabb90d06
Conway <yawnoc@pm.me>
2026-09-09 00:10:00 +0800

    Prepare [[Page~82 (1)牽]]

--- entries/hin.cmd
+++ entries/hin.cmd
@@ -21,3 +21,35 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~82 (1)牽]]
+    ==
+    - To drag, to pull, as an ox does;
+    - to lead or pull along with a cord;
+    - to bring about;
+    - to induce, to guide;
+    - to connect with, to deduce;
+    - dragged into, held;
+    - _(1)h(i/)n (4)l(i/)n_
+        [[hin1 lin4]],
+        to connect, to implicate or compromise;
+    - _(1)h(i/)n l(a/)m(6)_
+        [[hin1 laam6]]
+        to track a boat;
+    - _(1)h(i/)n (4)t(')au (4)p(')o_
+        [[hin1 tau4 po4]]
+        a bawd;
+    - _(1)h(i/)n kw(a/)(3)_
+        [[hin1 gwaa3]]
+        in suspense;
+    - _(1)h(i/)n h(i/)(3)_
+        [[hin1 hei3]]
+        gasping, dying.
+    ==
+  ====
+$$
+##>
