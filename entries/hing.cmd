f69cd882cd2e55f27331064017d2305a38f41082
Conway <yawnoc@pm.me>
2026-09-09 01:14:04 +0800

    Prepare [[Page~83 (1)興]]

--- entries/hing.cmd
+++ entries/hing.cmd
@@ -21,3 +21,48 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+H
+  May appear with interior of the top-middle component as
+  ::⿱一コ :: (+8) or ⿱一口 (+9 assumed here),
+  depending on the font.
+W
+  ====
+  - [[Page~83 (1)興]]
+    ==
+    - To rise, to elevate, to get up;
+    - to flourish;
+    - flourishing, prosperous;
+    - promoted;
+    - to be in demand, fashionable;
+    - to move, to put in motion;
+    - _(1)hing (2)h(i/)_
+        [[hing1 hei2]],
+        to arise;
+    - _(1)hing (1)k(u:) n(a/)p(9) fuk(7)_
+        [[hing1 geoi1 naap6 fuk1]]
+        hope you are in all respects well;
+    - _(1)hing (1)kung_
+        [[hing1 gung1]],
+        to commence a work;
+    - _~~(4)n(i/)~~ ``(1)n(i/)`` ch(')(u:)(3) (4)'m (1)hing (5)k(')(u:)_
+        [[ni1 cyu3 m4 hing1 keoi5]],
+        no demand for it here;
+    - _(4)sh(i/) (1)hing_
+        [[si4 hing1]],
+        fashionable;
+    - _(1)hing wong(6)_
+        [[hing1 wong6]]
+        successful, prosperous;
+    - _tsok(8) (1)hing_
+        [[zok3 hing1]],
+        pleased with, complacent;
+    - _(1)hing (4)'m (1)hing_
+        [[hing1 m4 hing1]],
+        would you like it?
+    ==
+  ====
+$$
+##>
