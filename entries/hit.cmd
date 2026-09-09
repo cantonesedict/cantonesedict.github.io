6b541675a11a9101489c9cca3564eccf6f64bd8f
Conway <yawnoc@pm.me>
2026-09-09 23:41:51 +0800

    Prepare [[Page~85 歇(8)]]

--- entries/hit.cmd
+++ entries/hit.cmd
@@ -21,3 +21,44 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~85 歇(8)]]
+    ==
+    - To stop, to rest, to desist;
+    - to keep silence;
+    - to terminate, to discontinue;
+    - to appease;
+    - _h(i/)t(8) ch(u:)(6) (2)shau_
+        [[hit3 zyu6 sau2]],
+        to rest from work, to hold up;
+    - _h(i/)t(8) ch(u:)(6) (2)hau_ [[hit3 zyu6 hau2]],
+        or
+      _h(i/)t(8) (2)tsui_ [[hit3 zeoi2]],
+        to be still, to stop eating;
+    - _h(i/)t(8) t(i/)m(3)_
+        [[hit3 dim3]]
+        a tavern, a lodging-place;
+    - _h(i/)t(8) (5)h(a/) k(e/)uk(8)_
+        [[hit3 haa5 goek3]]
+        stop walking awhile;
+    - _h(i/)t(8) t(a/)m(3)_
+        [[hit3 daam3]]
+        put the load down;
+    - _h(i/)t(8) yat(7) h(i/)t(8)_
+        [[hit3 jat1 hit3]]
+        rest a while;
+    - _pat(7) h(i/)t(8)_
+        [[bat1 hit3]]
+        uninterrupted, incessant;
+    - _h(i/)t(8) h(a/)k(8)_
+        [[hit3 haak3]]
+        a guest;
+        to detain or lodge a friend.
+    ==
+  ====
+$$
+##>
