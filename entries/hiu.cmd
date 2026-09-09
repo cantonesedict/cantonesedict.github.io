a4df01f6c898a003ca6e01eed6db572c33908188
Conway <yawnoc@pm.me>
2026-09-09 23:52:52 +0800

    Prepare [[Page~85 (1)囂]]

--- entries/hiu.cmd
+++ entries/hiu.cmd
@@ -21,3 +21,24 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~85 (1)囂]]
+    ==
+    - To vociferate, to make a noise;
+    - clamor, hum, as of a market;
+    - to vilify, to murmur;
+    - _(1)hi(u/) pok(9)_
+        [[hiu1 bok6]]
+        diminishing, worse, impoverished;
+    - _(1)hi(u/) (1)hi(u/)_ [[hiu1 hiu1]],
+        or
+      _(1)hi(u/) ng(o\)(6)_ [[hiu1 ngou6]]
+        self-complacent, pluming one's self.
+    ==
+  ====
+$$
+##>
