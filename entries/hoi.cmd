b1afbef63d4f7cdffef7d656fe84993970bf6b00
Conway <yawnoc@pm.me>
2026-09-11 00:23:44 +0800

    Prepare [[Page~89 (1)開]]

--- entries/hoi.cmd
+++ entries/hoi.cmd
@@ -21,3 +21,75 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~89 (1)開]]
+    ==
+    - To open, to unfold, to spread out;
+    - to institute, to begin;
+    - to commence, to start;
+    - to lay out, to explain, to separate;
+    - to reveal, to disclose;
+    - _(2)(a/)u (1)hoi_
+        [[aau2 hoi1]],
+        to break open, to split the difference;
+    - _(1)hoi (4)t(')oi_
+        [[hoi1 toi4-2]],
+        to set the table;
+        to open the play;
+    - _(1)hoi k(a/)(3)_
+        [[hoi1 gaa3]]
+        to state the price;
+    - _(4)'m (1)hoi tak(7) (1)k(a/)u_
+        [[m4 hoi1 dak1 gaau1]],
+        it must be so, impracticable;
+        a strong superlative;
+    - _(1)hoi (1)ch(e/)ung_
+        [[hoi1 zoeng1]],
+        to open a shop;
+    - _(1)hoi (1)shan_
+        [[hoi1 san1]],
+        to weigh anchor;
+    - _(1)hoi ch(')(a/)k(8) kau(6) ch(e/)ung(3)_
+        [[hoi1 caak3 gau6 zoeng3]]
+        to liquidate old debts;
+    - _(1)hoi (4)sh(u:)n_
+        [[hoi1 syun4]],
+        to go aboard;
+    - _(1)hoi (1)kwong_
+        [[hoi1 gwong1]],
+        to vivify an idol;
+    - _(1)hoi (4)n(i/)n_
+        [[hoi1 nin4]],
+        newyear's day;
+    - _~~(4)hang~~ ``(4)h(a/)ng`` (1)hoi tik(7)_
+        [[haang4 hoi1 dik1]]
+        step aside a little!
+    - _(1)hoi (1)sam_
+        [[hoi1 sam1]],
+        to amuse one, to dissipate sorrow;
+    - _(1)hoi l(o\)(6)_
+        [[hoi1 lou6]]
+        to clear the road for the ghost;
+    - _(1)hoi (1)fong_
+        [[hoi1 fong1]],
+        to clear new land;
+    - _(1)hoi kok(8)_
+        [[hoi1 gok3]]
+        to borrow money;
+    - _(1)hoi (2)kong_
+        [[hoi1 gong2]],
+        to explain the meaning of the classics;
+    - _(1)hoi (4)ming_
+        [[hoi1 ming4]]
+        state the items;
+    - _(1)hoi (4)loi_
+        [[hoi1 loi4]],
+        come nearer to me.
+    ==
+  ====
+$$
+##>
