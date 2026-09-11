e8e670301d6f591a13024b15b12922819ab5649e
Conway <yawnoc@pm.me>
2026-09-11 23:11:30 +0800

    Prepare [[Page~90 殼(8)壳(8)]]

--- entries/hok.cmd
+++ entries/hok.cmd
@@ -21,3 +21,38 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~90 殼(8)壳(8)]]
+    ==
+    - The skin, shells, or covering of fruits or eggs;
+    - shell or scale of mollusks, reptiles;
+    - exuviæ of eggs, snakes, or chrysalides;
+    - a hollow old tree;
+    - bark, crust, or what covers;
+    - a ladle;
+    - _(1)kwai hok(8)_
+        [[gwai1 hok3]]
+        a divining turtle shell;
+    - _(1)t(')ong hok(8)_
+        [[tong1 hok3]]
+        soup-ladle;
+    - _(2)shui hok(8)_
+        [[seoi2 hok3]]
+        a dipper;
+    - _(4)t(')au hok(8)_
+        [[tau4 hok3]]
+        the skull;
+    - _m(o\)(6) hok(8)_
+        [[mou6 hok3]]
+        a summer hat, without a fringe;
+    - _si(u/)(3) m(i/)n(6) hok(8)_
+        [[siu3 min6 hok3]]
+        a mask.
+    ==
+  ====
+$$
+##>
