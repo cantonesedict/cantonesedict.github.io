90ba386b6f79fcf5241e378a53a62e33181e1a50
Conway <yawnoc@pm.me>
2026-09-12 00:20:52 +0800

    Prepare [[Page~91 (1)堪]]

--- entries/ham.cmd
+++ entries/ham.cmd
@@ -21,3 +21,36 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~91 (1)堪]]
+    ==
+    - To sustain or bear;
+    - able for, adequate to, capable of;
+    - worthy of, fit, worthy for---in a moral or physical sense;
+    - covering over a hollow;
+    - _pat(7) (1)h(o\)m_
+        [[bat1 ham1]],
+        incompetent, unworthy;
+    - _(1)h(o\)m yung(6)_
+        [[ham1 jung6]]
+        useful, capable, serviceable;
+    - _(1)h(o\)m ~~shing(3)~~ ``(1)shing``_
+        [[ham1 sing1]]
+        able to succeed;
+    - _(1)h(o\)m yam(6)_
+        [[ham1 jam6]]
+        able to sustain;
+    - _(1)h(o\)m (4)(u:) (1)s(i/)n ~~(1)shang~~ ``(1)sh(a/)ng``_
+        [[ham1 jyu4 sin1 saang1]],
+        a geomancer;
+    - _(1)h(o\)m (4)(i/)_
+        [[ham1 ji4]],
+        very proper, satisfactory, suitable.
+    ==
+  ====
+$$
+##>
