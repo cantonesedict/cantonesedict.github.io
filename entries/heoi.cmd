14b4c5fc62099d34a3582d8492a0dcc785182bb6
Conway <yawnoc@pm.me>
2026-09-13 18:29:36 +0800

    Prepare [[Page~96 (1)虛]]

--- entries/heoi.cmd
+++ entries/heoi.cmd
@@ -21,3 +21,48 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~96 (1)虛]]
+    ==
+    - Empty, vacant;
+    - unsubstantial, unsatisfactory, void;
+    - vain, inane;
+    - humble, pure;
+    - abstract contemplation, as understood by the Budhists;
+    - space, the void of the firmament;
+    - the constellation Aquarius;
+    - _(1)hung (1)h(u:)_
+        [[hung1 heoi1]],
+        empty;
+    - _(1)h(u:) (4)fau_
+        [[heoi1 fau4]],
+        frothy, nonsensical;
+    - _(1)h(u:) tsz'(6)_
+        [[heoi1 zi6]]
+        particles, adverbs;
+    - _(1)h(u:) w(a/)(6)_
+        [[heoi1 waa6-2]]
+        unfounded, idle prate;
+    - _(1)h(u:) (1)sam h(a/)(6) h(i/)(3)_
+        [[heoi1 sam1 haa6 hei3]]
+        to put up with, unprejudiced, indifferent to;
+    - _t(')(a/)i(3) (1)h(u:)_
+        [[taai3 heoi1]],
+        the heavens;
+    - _(1)h(u:) t(o\)(6)_
+        [[heoi1 dou6]]
+        vainly spent [his days];
+    - _(1)h(u:) (4)ch(')(u:)n_
+        [[heoi1 cyun4]],
+        a false rumor;
+    - _(1)h(u:) ~~y(e/)uk(8)~~ ``y(e/)uk(9)``_
+        [[heoi1 joek6]]
+        weak.
+    ==
+  ====
+$$
+##>
