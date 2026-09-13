231766bc67296b040b1cb6c6ada688703ca2e142
Conway <yawnoc@pm.me>
2026-09-13 18:13:54 +0800

    Prepare [[Page~96 渴(8)]]

--- entries/hot.cmd
+++ entries/hot.cmd
@@ -35,3 +35,29 @@ W
   ====
 $$
 ##>
+
+<##
+$$
+W
+  ====
+  - [[Page~96 渴(8)]]
+    ==
+    - Dry, thirsty;
+    - to thirst;
+    - desirous of, anxious, longing (in a good sense);
+    - _(4)hau hot(8)_
+        [[hau4 hot3]]
+        thirsty;
+    - _hot(8) (2)s(e/)ung_
+        [[hot3 soeng2]],
+        desirous of;
+    - _w(a/)(6) (4)m(u/)i~~,~~ hot(8) (2)ch(i/)_
+        [[waa6 mui4 hot3 zi2]],
+        he mentioned plums and their thirst stopped;
+    - _(2)ch(i/) hot(8)_
+        [[zi2 hot3]]
+        to quench thirst.
+    ==
+  ====
+$$
+##>
