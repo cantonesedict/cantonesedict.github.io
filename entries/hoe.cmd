b84ca8ee9ac18ee4e8e820ff6daca0c3c43cafbc
Conway <yawnoc@pm.me>
2026-09-13 22:35:30 +0800

    Prepare [[Page~97 (1)靴]]

--- entries/hoe.cmd
+++ entries/hoe.cmd
@@ -21,3 +21,27 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~97 (1)靴]]
+    ==
+    - Boots;
+    - _yat(7) t(u/)i(3) (1)h(u\)_
+        [[jat1 deoi3 hoe1]],
+        a pair of boots;
+    - _(1)h(u\) m(o\)(6)_
+        [[hoe1 mou6-2]]
+        boots and caps, sent as presents;
+    - _(1)fong (4)t(')au (1)h(u\)_
+        [[fong1 tau4 hoe1]],
+        square-toed boots;
+    - _(5)ng(a/) (1)h(u\) k(o\)m(3) ng(a/)ng(6)_
+        [[ngaa5 hoe1 gam3 ngaang6]]
+        as stiff as earthen boots, unchangeable, obstinate.
+    ==
+  ====
+$$
+##>
