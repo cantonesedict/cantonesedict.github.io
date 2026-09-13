c9aebd7ed6bfbe84f758fb3230c329c9517f9146
Conway <yawnoc@pm.me>
2026-09-13 22:55:34 +0800

    Prepare [[Page~98 哭(7)]]

--- entries/huk.cmd
+++ entries/huk.cmd
@@ -21,3 +21,34 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~98 哭(7)]]
+    ==
+    - To cry aloud, the noise of grief or pain;
+    - to groan, to wail;
+    - _huk(7) yap(7)_
+        [[huk1 jap1]]
+        crying and sobbing;
+    - _(4)t(')ai huk(7)_
+        [[tai4 huk1]]
+        groaning loudly;
+    - _(1)k(')am tak(7) huk(7)_
+        [[kam1 dak1 huk1]]
+        unceasing crying;
+    - _huk(7) (1)song_
+        [[huk1 song1]],
+        wailing for the dead;
+    - _t(')ung(3) huk(7)_
+        [[tung3 huk1]]
+        heartfelt grief;
+    - _huk(7) (1)song ch(e/)ung(6)_
+        [[huk1 song1 zoeng6]]
+        the staff carried by a son at a funeral.
+    ==
+  ====
+$$
+##>
