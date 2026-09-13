dfa6bc8b98cc6a16c2dfcd7ea3bb869e83fcdb68
Conway <yawnoc@pm.me>
2026-09-14 00:35:49 +0800

    Prepare [[Page~99 (1)空]]

--- entries/hung.cmd
+++ entries/hung.cmd
@@ -26,6 +26,69 @@ OrdinaryDictionaryReplacement: #.properties-override
 $$
 W
   ====
+  - [[Page~99 (1)空]]
+    ==
+    - Empty, vacant, void;
+    - at leisure;
+    - an opening or crevice;
+    - great, wide;
+    - the firmament, sky;
+    - poor, broken;
+    - unprejudiced, able to perceive clearly;
+    - abstraction as understood by the Budhists.
+    ==
+    --
+    Read _(2)hung_ [[hung2]],
+    a hole, a tunnel or opening in the ground.
+    --
+    ==
+    - _(1)Hung uk(7)_
+        [[hung1 uk1]]
+        an empty house;
+    - _t(')(a/)i(3) (1)hung_ [[taai3 hung1]],
+        or
+      _(1)hung (1)chung_ [[hung1 zung1]],
+        heaven, sky;
+    - _(5)n(i/) k(o\)m(3) (2)hau (1)hung_
+        [[nei5 gam3 hau2 hung1]],
+        so impudent;
+        do n't you put in your tongue;
+    - _(1)hung ~~(2)(')(o\)~~ ``(5)t(')(o\)``_
+        [[hung1 tou5]],
+        an empty stomach;
+    - _(1)hung (2)shau_
+        [[hung1 sau2]],
+        empty-handed;
+    - _(4)nong (1)hung_
+        [[nong4 hung1]]
+        no coin by one, moneyless;
+    - _(1)hung (4)h(a/)n_
+        [[hung1 haan4]],
+        unoccupied;
+    - _(1)hung (5)lung_
+        [[hung1 lung5]],
+        hollow;
+    - _(1)hung t(i/)(6)_
+        [[hung1 dei6-2]]
+        a vacant spot;
+    - _(1)hung (4)l(o\) (4)m(o\) yik(7)_
+        [[hung1 lou4 mou4 jik1]]
+        lost all my trouble;
+    - _(1)hung (1)hung h(u:)(3)_
+        [[hung1 hung1 heoi3]]
+        went away empty;
+    - _yat(7) (4)ch(')(e/)ung (1)hung_
+        [[jat1 coeng4 hung1]],
+        all gone, everything is lost.
+    ==
+  ====
+$$
+##>
+
+<##
+$$
+W
+  ====
   - [[Page~94 (1)箜 _(1)Hong_ (hong1); here normalised to _(1)Hung_ (hung1)]]
     ==
     - A flat lute with 25~strings,
