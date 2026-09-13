1946c37cc9adf2c55bc4b93d1913ee368dc2ad82
Conway <yawnoc@pm.me>
2026-09-13 22:49:01 +0800

    Prepare [[Page~98 曲(7)]]

--- entries/kuk.cmd
+++ entries/kuk.cmd
@@ -21,3 +21,35 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~98 曲(7) _Huk(7)_ (huk1); here normalised to _K(')uk(7)_ (kuk1)]]
+    ==
+    - Crooked, bent curved, distorted;
+    - scheming, tortuous, false;
+    - to oppress, to wrong;
+    - to force, or oblige one to do, necessitated;
+    - songs, sonnets, ditties;
+    - a carpenter's square;
+    - _(1)w(a/)n ~~huk(7)~~ ``k(')uk(7)``_
+        [[waan1 kuk1]]
+        devious, meandering;
+    - _~~huk(7)~~ ``k(')uk(7)`` ~~chik(7)~~ ``chik(9)``_
+        [[kuk1 zik6]]
+        crooked and straight, the right and wrong;
+    - _(1)sz' ~~huk(7)~~ ``k(')uk(7)``_
+        [[si1 kuk1]]
+        tricky, underhand;
+    - _~~ch(e/)ung(3)~~ ``ch(')(e/)ung(3)`` ~~huk(7)~~ ``k(')uk(7)``_
+        [[coeng3 kuk1]]
+        to sing ditties with a fife;
+    - _wat(7) ~~huk(7)~~ ``k(')uk(7)``_
+        [[wat1 kuk1]]
+        to bend.
+    ==
+  ====
+$$
+##>
