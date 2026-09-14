4d2825c891e72e903974d581df870f05f2bfeedb
Conway <yawnoc@pm.me>
2026-09-15 01:02:55 +0800

    Prepare [[Page~101 血(8)]]

--- entries/hyut.cmd
+++ entries/hyut.cmd
@@ -21,3 +21,49 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~101 血(8)]]
+    ==
+    - Blood;
+    - ``the 143d``~radical [[U+2F8E ⾎ "blood"]] of blood or bloody things;
+    - _h(u:)t(8) h(i/)(3)_
+        [[hyut3 hei3]]
+        the bodily stamina, the constitution;
+    - _h(u:)t(8) (2)p(u/)n_
+        [[hyut3 bun2]],
+        capital in trade;
+    - _ch(')ut(7) h(u:)t(8)_
+        [[ceot1 hyut3]]
+        bleeding;
+    - _(2)p(o\) h(u:)t(8)_
+        [[bou2 hyut3]]
+        to strengthen the system;
+    - _~~(u:)(3)~~ ``(2)(u:)`` h(u:)t(8)_
+        [[jyu2 hyut3]]
+        blood settled in a bruise;
+    - _kuk(9) h(u:)t(8)_
+        [[guk6 hyut3]]
+        coagulated blond;
+    - _h(u:)t(8) (1)sh(u:)_
+        [[hyut3 syu1]],
+        a letter written in blood (at the last extremity);
+    - _h(u:)t(8) sing(3)_
+        [[hyut3 sing3]]
+        careful memory of, attention;
+    - _shat(7) h(u:)t(8)_
+        [[sat1 hyut3]]
+        pale, white-livered;
+    - _h(u:)t(8) (4)(u:)_
+        [[hyut3 jyu4]],
+        hair of the head;
+    - _ch(')ut(7) p(a/)k(9) h(u:)t(8)_
+        [[ceot1 baak6 hyut3]]
+        to pay another's defalcation.
+    ==
+  ====
+$$
+##>
