a6fbd79ca329813d3ddbd786f0bf93d064be626d
Conway <yawnoc@pm.me>
2026-09-15 01:09:05 +0800

    Prepare [[Page~102 闕(8)]]

--- entries/kyut.cmd
+++ entries/kyut.cmd
@@ -21,3 +21,37 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~102 闕(8) _H(u:)t(8)_ (hyut3); here normalised to _K(')(u:)t(8)_ (kyut3)]]
+    ==
+    - Looking out a door, a lookout above a gate;
+    - imperial;
+    - defective, lost;
+    - not enough;
+    - faults, deficiencies;
+    - to miss, to err;
+    - disrespectful, wanting in;
+    - to dig;
+    - _~~h(u:)t(8)~~ ``k(')(u:)t(8)`` (4)m(u/)n_
+        [[kyut3 mun4]],
+        his Majesty's palace;
+    - _(1)kam ~~h(u:)t(8)~~ ``k(')(u:)t(8)``_
+        [[gam1 kyut3]]
+        the golden gate of Olympus;
+    - _(u:)t(9) ~~h(u:)t(8)~~ ``k(')(u:)t(8)``_
+        [[jyut6 kyut3]]
+        the waning moon;
+    - _~~hut(8)~~ ``k(')(u:)t(8)`` shat(7)_
+        [[kyut3 sat1]]
+        lost, missing;
+    - _(4)h(o\) (4)m(o\) (2)shi(u/) ~~h(u:)t(8)~~ ``k(')(u:)t(8)``_
+        [[hou4 mou4 siu2 kyut3]]
+        not the least thing missing.
+    ==
+  ====
+$$
+##>
