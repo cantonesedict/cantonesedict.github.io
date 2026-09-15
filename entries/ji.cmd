060f31b6362ba98093cb330bc4b37c9b911e8090
Conway <yawnoc@pm.me>
2026-09-15 22:26:51 +0800

    Prepare [[Page~102 (1)衣]]

--- entries/ji.cmd
+++ entries/ji.cmd
@@ -21,3 +21,43 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~102 (1)衣]]
+    ==
+    - Clothes, specially those for the body;
+    - ``the 145th``~radical [[U+2F90 ⾐ "clothes"]] of garments;
+    - a cover, case, or wrapper;
+    - the husk of cocoanuts;
+    - _(1)(i/) fuk(9)_ [[ji1 fuk6]]
+        or
+      _(1)(i/) (4)sh(e/)ung_ [[ji1 soeng4]],
+        clothes;
+    - _p(a/)k(9) (1)(i/)_
+        [[baak6 ji1]],
+        plebeians;
+    - _(1)ts(')ing (1)(i/)_
+        [[cing1 ji1]],
+        si(u/)ts(a/)i [[秀才]] graduates;
+    - _p(i/)n(6) (1)(i/)_
+        [[bin6 ji1]],
+        undress, common dress;
+    - _(5)m(o\) (1)(i/) shik(9)_
+        [[mou5 ji1 sik6]]
+        improper, lewdly;
+    - _ch(e/)uk(8) (1)(i/) fuk(9)_
+        [[zoek3 ji1 fuk6]]
+        to dress;
+    - _(4)shing (1)(i/) t(i/)m(3)_
+        [[sing4 ji1 dim3]]
+        a slop-shop;
+    - _shui(6) (1)(i/)_
+        [[seoi6 ji1]],
+        a sleeping dress.
+    ==
+  ====
+$$
+##>
