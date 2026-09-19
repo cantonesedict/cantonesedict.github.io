ac9e770d8aa4761f0cd41694c1f7c67d9d5956b1
Conway <yawnoc@pm.me>
2026-09-20 01:02:07 +0800

    Prepare [[Page~110 (1)煙(1)烟]]

--- entries/jin.cmd
+++ entries/jin.cmd
@@ -24,6 +24,68 @@ OrdinaryDictionaryReplacement: #.properties-override
 
 <##
 $$
+H
+  May appear as either ⿰火⿱西土 (assumed here) or ⿰火⿱覀土,
+  depending on the font.
+W
+  ====
+  - [[Page~110 (1)煙(1)烟]]
+    ==
+    - Smoke, misty vapor;
+    - tobacco or opium, because they are smoked;
+    - _(1)(u/) (1)(i/)n_
+        [[wu1 jin1]],
+        lamp-black;
+    - _(4)ming (1)(i/)n_
+        [[ming4 jin1]],
+        excellent tobacco;
+    - _(1)(i/)n (1)ts(')(e/)ung_
+        [[jin1 coeng1]],
+        an opium-pipe;
+    - _(1)m(a/) (1)k(u/) (1)(i/)n_
+        [[maa1 gu1 jin1]],
+        cigars;
+    - _(2)shui (1)(i/)n_
+        [[seoi2 jin1]],
+        tobacco prepared for the hooka;
+    - _(1)shang (1)(i/)n_
+        [[sang1 jin1]],
+        smoking tobacco;
+    - _(1)(i/)n (i/)p(9)_
+        [[jin1 jip6]]
+        tobacco-leaf;
+    - _(1)shi(u/) (1)(i/)n (2)fo_
+        [[siu1 jin1 fo2]],
+        to let off fireworks;
+    - _(1)(i/)n (1)f(a/) long(6) (2)tsz'_
+        [[jin1 faa1 long6 zi2]]
+        a brothel-goer;
+    - _(1)(i/)n ~~(4)^chan~~ ``(4)ch(')an``_
+        [[jin1 can4]],
+        light dust or motes in the air;
+        a term applied to banditti;
+    - _t(')(a/)n(3) (1)(i/)n_
+        [[taan3 jin1]],
+        to smoke opium;
+    - _(1)(i/)n (5)yan (2)h(i/)_
+        [[jin1 jan5 hei2]],
+        the longing of victimized opium-smokers;
+    - _(1)(i/)n (5)m(o\) chan(6)_
+        [[jin1 mou5 zan6]]
+        'smoke has no rules,' said of great brags;
+    - _(1)hoi (1)tang (1)(i/)n (2)k(u/)n_
+        [[hoi1 dang1 jin1 gun2]],
+        an opium-smoking shop;
+    - _(4)yan (1)(i/)n (4)ch(')au ~~mat(7)~~ ``mat(9)``_
+        [[jan4 jin1 cau4 mat6]]
+        a place thickly settled.
+    ==
+  ====
+$$
+##>
+
+<##
+$$
 W
   ====
   - [[Page~83 讞(3) _H(i/)n(3)_ (hin3); here normalised to _(I/)n(6)_ (jin6)]]
