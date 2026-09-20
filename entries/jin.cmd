0e0d25c1990557cd2d0a82353013867e5ba089e6
Conway <yawnoc@pm.me>
2026-09-20 16:45:18 +0800

    Fix placement of [[Page~83 讞(3)]]

--- entries/jin.cmd
+++ entries/jin.cmd
@@ -168,32 +168,6 @@ $$
 <##
 $$
 W
-  ====
-  - [[Page~83 讞(3) _H(i/)n(3)_ (hin3); here normalised to _(I/)n(6)_ (jin6)]]
-    ==
-    - To consult on criminal causes;
-    - to judge or decide a case;
-    - to adjudge;
-    - _~~h(i/)n(3)~~ ``(i/)n(6)`` yuk(9)_
-        [[jin6 juk6]]
-        to sentence;
-    - _~~h(i/)n(3)~~ ``(i/)n(6)`` (2)t(i/)n_
-        [[jin6 din2]],
-        a legal decision;
-    - _(1)ts(')au ~~h(i/)n(3)~~ ``(i/)n(6)``_
-        [[cau1 jin6]]
-        the autumnal assize;
-    - _(5)yau sun(3) ~~h(i/)n(3)~~ ``(i/)n(6)``_
-        [[jau5 seon3 jin6]]
-        a good decision, a true judgment.
-    ==
-  ====
-$$
-##>
-
-<##
-$$
-W
   ======
   - [[Page~110 (4)焉]]
     ====
@@ -347,6 +321,32 @@ W
     - _shik(9) (4)(i/)n_
         [[sik6 jin4]],
         to retract, to break a promise.
+    ==
+  ====
+$$
+##>
+
+<##
+$$
+W
+  ====
+  - [[Page~83 讞(3) _H(i/)n(3)_ (hin3); here normalised to _(I/)n(6)_ (jin6)]]
+    ==
+    - To consult on criminal causes;
+    - to judge or decide a case;
+    - to adjudge;
+    - _~~h(i/)n(3)~~ ``(i/)n(6)`` yuk(9)_
+        [[jin6 juk6]]
+        to sentence;
+    - _~~h(i/)n(3)~~ ``(i/)n(6)`` (2)t(i/)n_
+        [[jin6 din2]],
+        a legal decision;
+    - _(1)ts(')au ~~h(i/)n(3)~~ ``(i/)n(6)``_
+        [[cau1 jin6]]
+        the autumnal assize;
+    - _(5)yau sun(3) ~~h(i/)n(3)~~ ``(i/)n(6)``_
+        [[jau5 seon3 jin6]]
+        a good decision, a true judgment.
     ==
   ====
 $$
