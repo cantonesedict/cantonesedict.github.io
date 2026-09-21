46a07897d97bffb85a2cec721ec3212bb2dbe306
Conway <yawnoc@pm.me>
2026-09-22 00:23:58 +0800

    Prepare [[Page~116 (1)家]]

--- entries/gaa.cmd
+++ entries/gaa.cmd
@@ -21,3 +21,91 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~116 (1)家]]
+    ==
+    - What is within doors;
+    - a household, a family;
+    - home, a dwelling;
+    - domestic, domesticated;
+    - title of a husband or master of a family;
+    - term applied to some dignitaries;
+    - the country, the government;
+    - to dwell, to live at;
+    - a sect, profession, or class;
+    - a region of the body,
+      as _fai(3) (1)k(a/)_
+        [[fai3 gaa1]],
+        the lungs;
+    - a suffix of nouns to denote persons,
+      as _t(a/)i(6) (1)k(a/)_
+        [[daai6 gaa1]],
+        all of us, the whole, all at once, together;
+    - _tsz'(6) (1)k(a/)_
+        [[zi6 gaa1]],
+        I, myself;
+    - _f(u/)(3) (1)k(a/)_
+        [[fu3 gaa1]],
+        the rich;
+    - _(1)k(a/) (4)yan_
+        [[gaa1 jan4]],
+        domestics, official attendants;
+    - _t(a/)i(6) (1)k(a/) (1)s(e/)ung ts(u:)(6)_
+        [[daai6 gaa1 soeng1 zeoi6]]
+        all met together;
+    - _(1)k(a/) shat(7)_
+        [[gaa1 sat1]]
+        a husband and wife, household;
+    - _h(o\)p(9) (1)k(a/)_
+        [[hap6 gaa1]],
+        the whole family;
+    - _(2)k(u/)n (1)k(a/)_
+        [[gun2 gaa1]],
+        your servants, your assistants;
+    - _(1)k(a/) f(u/)(6)_
+        [[gaa1 fu6]]
+        my father;
+    - _~~(1)ts(')an~~ ``ts(')an(3)`` (1)k(a/)_
+        [[can3 gaa1]],
+        term by which the parents of a married couple call each other;
+    - _(2)p(u/)n (1)k(a/)_
+        [[bun2 gaa1]],
+        a namesake, one of the same clan name;
+    - _ch(')ut(7) (1)k(a/)_
+        [[ceot1 gaa1]],
+        to become a bonze or nun;
+    - _(1)k(a/) (2)ch(e/)ung_
+        [[gaa1 zoeng2]],
+        our clan elder;
+    - _(1)k(a/) (5)h(a/)_
+        [[gaa1 haa5]],
+        this instant, just now;
+    - _(1)k(a/) (5)lai_
+        [[gaa1 lai5]],
+        family rites, usages pertaining to marriage and burial;
+    - _(1)k(a/) yung(6)_
+        [[gaa1 jung6]]
+        for 'family use,' superior quality of goods;
+    - _(1)k(a/) (4)shan_
+        [[gaa1 san4]],
+        courtiers;
+    - _tai(6) yat(7) (1)k(a/)_
+        [[dai6 jat1 gaa1]],
+        excellent, "first chop," skillful;
+    - _(1)f(a/) (1)k(a/) (2)tsai_
+        [[faa1 gaa1 zai2]],
+        you little scamp;
+    - _(5)yau (1)k(a/) ~~(5)l(i/)~~ ``(5)l(u:)``_
+        [[jau5 gaa1 leoi5]],
+        I am married;
+    - _(1)k(a/) kwok(8) sz'(6)_
+        [[gaa1 gwok3 si6]]
+        national affairs.
+    ==
+  ====
+$$
+##>
