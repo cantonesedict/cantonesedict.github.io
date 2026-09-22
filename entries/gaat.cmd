8f73bae39b1991c0c70cbf67b08f665c2a72fb90
Conway <yawnoc@pm.me>
2026-09-23 00:28:32 +0800

    Prepare [[Page~117 (1)甴]]

--- entries/gaat.cmd
+++ entries/gaat.cmd
@@ -21,3 +21,31 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+A
+  ==
+  - 甴 (in 曱甴 spelt 甴曱)
+  ==
+V
+  ==
+  - gaa1 (per Williams)
+  ==
+W
+  ====
+  - [[Page~117 (1)甴 _(1)K(a/)_ (gaa1); here normalised to 曱 _K(a/)t(9)_ (gaat6)]]
+    ==
+    - _~~(1)K(a/)~~ ``K(a/)t(9)`` ~~ts(a/)t(8)~~ ``ts(a/)t(9)``_
+        [[gaat6 zaat6 曱甴]]
+        the common name of the cockroach.
+    ==
+  ====
+C
+  ==
+  - Williams' 曱甴: Corrected _ts(a/)t(8)_ (zaat3) to _ts(a/)t(9)_ (zaat6).
+  - See [曱甴](/entries/zaat#cantonese-曱甴)
+    for discussion on the spelling (曱甴 versus 甴曱).
+  ==
+$$
+##>
