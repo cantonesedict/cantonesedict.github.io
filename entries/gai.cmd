1610903412adf9e94ab15dd631b146731a9bc281
Conway <yawnoc@pm.me>
2026-09-23 23:21:18 +0800

    Prepare [[Page~119 (1)鷄(1)雞]]

--- entries/gai.cmd
+++ entries/gai.cmd
@@ -21,3 +21,62 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~119 (1)鷄(1)雞]]
+    ==
+    - The cock;
+    - the gallinæ;
+    - _(1)kai (1)kung_
+        [[gai1 gung1]]
+        a cock;
+    - _(1)kai ~~(5)n(a/)~~ ``(2)n(a/)``_
+        [[gai1 naa2]],
+        a hen;
+    - _(1)kai hong(6)_
+        [[gai1 hong6]]
+        a pullet;
+    - _(1)sh(a/)n (1)kai_
+        [[saan1 gai1]],
+        a pheasant;
+    - _(1)kam (1)kai_
+        [[gam1 gai1]],
+        the golden pheasant;
+    - _(1)kai (2)tsai_
+        [[gai1 zai2]],
+        a chicken;
+    - _(1)kai (4)ming_ [[gai1 ming4]],
+        or
+      _(1)kai (4)t(')ai_ [[gai1 tai4]],
+        cockcrowing;
+    - _(4)ngan (1)kai_
+        [[ngan4 gai1]],
+        a boatswain's whistle;
+    - _(1)kai (5)ng(a/)n_
+        [[gai1 ngaan5]],
+        corns;
+    - _(4)t(')(i/)n (1)kai_
+        [[tin4 gai1]],
+        a frog;
+    - _(2)fo (1)kai_
+        [[fo2 gai1]],
+        a turkey;
+    - _(2)shui (1)kai_
+        [[seoi2 gai1]],
+        'water hens,' a term for prostitutes in boats;
+    - _(1)kai (1)k(a/)n_
+        [[gai1 gaan1]],
+        sodomy;
+    - _tau(3) (1)kai_
+        [[dau3 gai1]],
+        cockfighting;
+    - _(1)kai k(')at(7) k(o\)m(3) ~~(5)ts(')(o\)~~ ``(2)ts(o\)``_
+        [[gai1 kat1 gam3 zou2]],
+        early as the cockcrow.
+    ==
+  ====
+$$
+##>
