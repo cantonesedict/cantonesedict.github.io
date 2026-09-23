adf057502ba43850ed65936d8b77ecec5b900359
Conway <yawnoc@pm.me>
2026-09-23 23:13:44 +0800

    Prepare [[Page~119 卡(3)]]

--- entries/kaa.cmd
+++ entries/kaa.cmd
@@ -21,3 +21,34 @@ OrdinaryDictionaryReplacement: #.properties-override
 - (Work in progress)
 ==
 </nav>
+
+<##
+$$
+W
+  ====
+  - [[Page~119 卡(3) _K(a/)(3)_ (gaa3); here normalised to _(1)K(')(a/)_ (kaa1)]]
+    ==
+    - A pass or station, where guards are posted;
+    - _~~k(a/)(3)~~ ``(1)k(')(a/)`` l(o\)(6)_
+        [[kaa1 lou6]]
+        a guardhouse;
+    - _sun(3) ~~k(a/)(3)~~ ``(1)k(')(a/)``_
+        [[seon3 kaa1]]
+        post-house;
+    - _(1)k(a/)i ~~k(a/)(3)~~ ``(1)k(')(a/)``_
+        [[gaai1 kaa1]]
+        a street guard, a police-station;
+    - ~~_k(a/)(3) l(a/)(3)_
+        [[gaa3 laa3 ？？]]
+        middling, obsequious, undecided.~~
+    ==
+  ====
+C
+  ==
+  - I could not find any other source that concurs with Williams' reading
+    of _k(a/)(3)_ (gaa3) for the "checkpoint" sense.
+  - Williams' _k(a/)(3) l(a/)(3)_ (gaa3 laa3):
+    It is unclear if this is related to 卡啦 kaa3 laa1 or kaa3 laa2.
+  ==
+$$
+##>
